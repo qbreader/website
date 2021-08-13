e73cbe4b5be8e3a43b6f9da12c9abef1122154da
Geoffrey Wu <geoffrey.feifei@gmail.com>
2021-08-13 13:07:22 -0500

    bug fixes
    Fixed a bug where pressing next while a question was being read caused the question to be read more quickly

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # qb-packet-reader
  
 useful packet parser: https://github.com/alopezlago/YetAnotherPacketParser
+
+TODO: 
+
+add more packets
+packet number selector should support dashes
--- index.js
+++ index.js
@@ -1,68 +1,20 @@
-currentlyBuzzing = false
-currentQuestionNumber = 0
+currentlyBuzzing = false;
+currentQuestionNumber = 0;
+intervalId = -1;
+packetName = '';
+packetNumbers = [];
+packetNumber = -1;
+questions = [{}];
 
-questions = 0;
-packetnumbers = -1;
-packetnumber = -1;
 
-document.getElementById('start').addEventListener('click', async () => {
-    packetname = document.getElementById('year-select').value + '-' + document.getElementById('name-select').value;
-    if (packetname.length == 1) return;
-    packetname = packetname.toLowerCase();
-    packetnumbers = document.getElementById('packet-select').value;
-    if (packetnumbers.length == 1) return;
-    // process string
-    packetnumbers = packetnumbers.split(',');
-    for (let i = 0; i < packetnumbers.length; i++) {
-        packetnumbers[i] = packetnumbers[i].trim();
-        packetnumbers[i] = parseInt(packetnumbers[i]);
+function buzz() {
+    if (currentlyBuzzing) {
+        displayRestOfQuestion();
+        document.getElementById('buzz').innerHTML = 'Buzz';
+    } else {
+        currentlyBuzzing = true;
+        document.getElementById('buzz').innerHTML = 'Reveal';
     }
-
-    packetnumber = packetnumbers.shift();
-    questions = await getquestions(packetname, packetnumber);
-
-    currentQuestionNumber = document.getElementById('question-select').value;
-    if (currentQuestionNumber == '') currentQuestionNumber = '1';
-    currentQuestionNumber = parseInt(currentQuestionNumber) - 1;
-    readQuestion();
-});
-
-
-async function getquestions(set, packet) {
-    return await fetch(`/getpacket?directory=${encodeURI(set)}&packetnumber=${encodeURI(packet)}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data['tossups']);
-        return data['tossups'];
-    });
-}
-
-async function next() {
-    document.getElementById('question').innerHTML = '';
-    document.getElementById('answer').innerHTML = '';
-    currentQuestionNumber++;
-    currentlyBuzzing = false;
-    if (currentQuestionNumber >= questions.length) {
-        packetnumber = packetnumbers.shift();
-        questions = await getquestions('2018-nasat', packetnumber);
-        currentQuestionNumber = 0;
-    }
-    readQuestion();
-}
-
-function readQuestion() {
-    document.getElementById('question-info').innerHTML = `${packetname} Packet ${packetnumber} Question ${currentQuestionNumber+1}`
-
-    questiontext = questions[currentQuestionNumber]['question_sanitized'];
-    console.log(questiontext);
-    questiontextsplit = questiontext.split(' ');
-    readingspeed = document.getElementById('reading-speed').value;
-    var intervalId = window.setInterval(() => {
-        document.getElementById('question').innerHTML += questiontextsplit.shift() + ' ';
-        if (currentlyBuzzing || questiontext.length == 0) {
-            clearInterval(intervalId);
-        }
-    }, readingspeed);
 }
 
 function displayRestOfQuestion() {
@@ -70,19 +22,99 @@ function displayRestOfQuestion() {
     document.getElementById('answer').innerHTML = 'ANSWER: ' + questions[currentQuestionNumber]['answer_sanitized'];
 }
 
+async function getQuestions(set, packet) {
+    return await fetch(`/getpacket?directory=${encodeURI(set)}&packetNumber=${encodeURI(packet)}`)
+        .then(response => response.json())
+        .then(data => {
+            return data['tossups'];
+        });
+}
+
+async function nextQuestion() {
+    clearInterval(intervalId);
+    currentlyBuzzing = false;
+
+    document.getElementById('question').innerHTML = '';
+    document.getElementById('answer').innerHTML = '';
+    document.getElementById('buzz').innerHTML = 'Buzz';
+    document.getElementById('info-text').innerHTML = 'Press space to buzz';
+    
+    currentQuestionNumber++;
+    if (currentQuestionNumber >= questions.length) {
+        packetNumber = packetNumbers.shift();
+        questions = await getQuestions(packetName, packetNumber);
+        currentQuestionNumber = 0;
+    }
+    
+    readQuestion();
+}
+
+function readQuestion() {
+    document.getElementById('question-info').innerHTML = `${packetName} Packet ${packetNumber} Question ${currentQuestionNumber + 1}`
+
+    questiontext = questions[currentQuestionNumber]['question_sanitized'];
+    questionTextSplit = questiontext.split(' ');
+    intervalId = window.setInterval(() => {
+        document.getElementById('question').innerHTML += questionTextSplit.shift() + ' ';
+        if (currentlyBuzzing || questionTextSplit.length == 0) {
+            clearInterval(intervalId);
+            document.getElementById('info-text').innerHTML = 'Press space to reveal';
+        }
+    }, document.getElementById('reading-speed').value);
+}
+
+
+document.getElementById('start').addEventListener('click', async () => {
+    clearInterval(intervalId);
+    currentlyBuzzing = false;
+
+    document.getElementById('question').innerHTML = '';
+    document.getElementById('answer').innerHTML = '';
+    document.getElementById('info-text').innerHTML = 'Press space to buzz';
+
+    packetyear = document.getElementById('year-select').value;
+    if (packetyear.length == 0) {
+        window.alert('Enter a packet year.');
+        return;
+    }
+    packetabbreviation = document.getElementById('name-select').value;
+    if (packetabbreviation.length == 0) {
+        window.alert('Enter a packet name.');
+        return;
+    }
+    packetName = packetyear + '-' + packetabbreviation;
+    packetName = packetName.toLowerCase();
+
+    packetNumbers = document.getElementById('packet-select').value;
+    if (packetNumbers.length == 0) {
+        window.alert('Enter packet numbers.');
+        return;
+    }
+    packetNumbers = packetNumbers.split(',');
+    for (let i = 0; i < packetNumbers.length; i++) {
+        packetNumbers[i] = packetNumbers[i].trim();
+    }
+    packetNumber = packetNumbers.shift();
+
+    questions = await getQuestions(packetName, packetNumber);
+
+    currentQuestionNumber = document.getElementById('question-select').value;
+    if (currentQuestionNumber == '') currentQuestionNumber = '1';  // default = 1
+    currentQuestionNumber = parseInt(currentQuestionNumber) - 1;
+
+    readQuestion();
+});
+
 document.getElementById('buzz').addEventListener('click', buzz);
-document.getElementById('next').addEventListener('click', next);
+document.getElementById('next').addEventListener('click', nextQuestion);
 
 document.addEventListener('keyup', () => {
-    if (packetnumbers != -1) {
+    if (document.activeElement.tagName != 'BODY') return;
+    if (packetNumbers != -1) {
         if (event.which == 32) {  // spacebar
-            if (currentlyBuzzing) {
-                displayRestOfQuestion();
-            } else {
-                currentlyBuzzing = true;
-            }
+            buzz();
         } else if (event.which == 78) { // pressing 'N'
-            next();
+            nextQuestion();
         }
     }
 });
