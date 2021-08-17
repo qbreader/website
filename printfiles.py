07b56f2f4d3a43fbe5c7c3e696b2117221cb296f
Geoffrey Wu <geoffrey.feifei@gmail.com>
2021-08-16 22:01:52 -0500

    Update printfiles.py

--- printfiles.py
+++ printfiles.py
@@ -1,4 +1,14 @@
 import os
+import re
+
+names = []
 
 for filename in os.listdir('packets'):
+    if '.DS_Store' in filename:
+        continue
+    filename = re.sub('_', ' ', filename)
+    names.append(filename)
+
+names.sort(reverse=True)
+for filename in names:
     print('<li>' + filename + '</li>')
