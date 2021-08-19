5e219e27ffae9b939c37d2f4572e757c5d05fc8f
Geoffrey Wu <geoffrey.feifei@gmail.com>
2021-08-18 21:34:18 -0500

    typing 'all' works for packet numbers

--- tossups.js
+++ tossups.js
@@ -130,7 +130,7 @@ document.getElementById('start').addEventListener('click', async () => {
     packetName = packetName.toLowerCase();
 
     packetNumbers = document.getElementById('packet-select').value.trim();
-    if (packetNumbers.length == 0) {
+    if (packetNumbers.length == 0 || packetNumbers.toLowerCase() == 'all') {
         packetNumbers = '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24';
     }
     packetNumbers = packetNumbers.split(',');
