41d546bebc044692feae970b9c0f5549451a9caa
Geoffrey Wu <42471355+geoffrey-wu@users.noreply.github.com>
2022-07-12 09:10:15 -0400

    add buzz processing

--- static/tossups.js
+++ static/tossups.js
@@ -19,83 +19,6 @@ var toggleCorrectClicked = false;
 var inPower = false;
 
 /**
- * Called when the users buzzes.
- * The first "buzz" pauses the question, and the second "buzz" reveals the rest of the question
- * and updates the score.
- */
-function buzz() {
-    if (currentlyBuzzing) {
-        // Update scoring:
-        inPower = !document.getElementById('question').innerHTML.includes('(*)') && questionText.includes('(*)');
-        if (inPower) {
-            shift('powers', 1);
-            if (setName.toLowerCase().includes('pace')) {
-                shift('points', 20);
-            }
-            else {
-                shift('points', 15);
-            }
-        }
-        else {
-            shift('tens', 1);
-            shift('points', 10);
-        }
-
-        // Update question text and show answer:
-        let characterCount = document.getElementById('question').innerHTML.length;
-        document.getElementById('question').innerHTML += questionTextSplit.join(' ');
-        shift('totalCelerity', 1 - characterCount / document.getElementById('question').innerHTML.length);
-        document.getElementById('answer').innerHTML = 'ANSWER: ' + questions[currentQuestionNumber]['answer'];
-        document.getElementById('buzz').innerHTML = 'Buzz';
-        document.getElementById('next').innerHTML = 'Next';
-
-        document.getElementById('buzz').setAttribute('disabled', 'disabled');
-        document.getElementById('toggle-correct').removeAttribute('disabled');
-        document.getElementById('toggle-correct').innerHTML = 'I was wrong';
-        updateStatDisplay();
-    } else {
-        // Stop the question reading
-        clearTimeout(timeoutID);
-        currentlyBuzzing = true;
-
-        // Include buzzpoint
-        document.getElementById('question').innerHTML += '(#) ';
-
-        document.getElementById('buzz').innerHTML = 'Reveal';
-        document.getElementById('pause').setAttribute('disabled', 'disabled');
-    }
-}
-
-/**
- * Updates the displayed stat line.
- */
-function updateStatDisplay() {
-    let numTossups = parseInt(sessionStorage.powers) + parseInt(sessionStorage.tens) + parseInt(sessionStorage.negs) + parseInt(sessionStorage.dead);
-    let celerity = numTossups != 0 ? parseFloat(sessionStorage.totalCelerity) / numTossups : 0;
-    celerity = Math.round(1000 * celerity) / 1000;
-    let includePlural = (numTossups == 1) ? '' : 's';
-    document.getElementById('statline').innerHTML
-        = `${sessionStorage.powers}/${sessionStorage.tens}/${sessionStorage.negs} with ${numTossups} tossup${includePlural} seen (${sessionStorage.points} pts, celerity: ${celerity})`;
-    if (numTossups === 0) //disable clear stats button if no stats
-        document.getElementById('clear-stats').setAttribute("disabled", "disabled");
-    else
-        document.getElementById('clear-stats').removeAttribute("disabled");
-}
-
-/**
- * Clears user stats.
- */
-function clearStats() {
-    sessionStorage.setItem('powers', 0);
-    sessionStorage.setItem('tens', 0);
-    sessionStorage.setItem('negs', 0);
-    sessionStorage.setItem('dead', 0);
-    sessionStorage.setItem('points', 0);
-    sessionStorage.setItem('totalCelerity', 0);
-    updateStatDisplay();
-}
-
-/**
  * Loads and reads the next question.
  */
 async function readQuestion() {
@@ -257,18 +180,3 @@ document.addEventListener('keyup', () => {
         document.getElementById('start').click();
     }
 });
-
-
-if (sessionStorage.getItem('powers') === null)
-    sessionStorage.setItem('powers', 0);
-if (sessionStorage.getItem('tens') === null)
-    sessionStorage.setItem('tens', 0);
-if (sessionStorage.getItem('negs') === null)
-    sessionStorage.setItem('negs', 0);
-if (sessionStorage.getItem('dead') === null)
-    sessionStorage.setItem('dead', 0);
-if (sessionStorage.getItem('points') === null)
-    sessionStorage.setItem('points', 0);
-if (sessionStorage.getItem('totalCelerity') === null)
-    sessionStorage.setItem('totalCelerity', 0);
-
