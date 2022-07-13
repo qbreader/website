baa5864f1ce027b616cd4339dcf09c4e6b5031be
Geoffrey Wu <42471355+geoffrey-wu@users.noreply.github.com>
2022-07-13 12:39:26 -0400

    fixed issue of getting the wrong packet

--- rooms.js
+++ rooms.js
@@ -8,7 +8,7 @@ function createRoom(roomName) {
         setTitle: '2022 PACE',
         setYear: 2022,
         setName: 'PACE',
-        packetNumbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 178, 19, 20, 21, 22, 23, 24],
+        packetNumbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
         packetNumber: 0,
         currentQuestionNumber: -1,
         readingSpeed: 50,
