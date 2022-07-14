66668ca33553f989555c67420630d161a653dd7b
Geoffrey Wu <42471355+geoffrey-wu@users.noreply.github.com>
2022-07-14 13:38:45 -0400

    update references

--- routes/about.js
+++ routes/about.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile('about.html', { root: './static' });
+    res.sendFile('about.html', { root: './client' });
 });
 
 module.exports = router;
