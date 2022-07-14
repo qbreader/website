dd7b8029998f895ddd2309302375b817cf89820d
Geoffrey Wu <42471355+geoffrey-wu@users.noreply.github.com>
2022-07-14 13:38:45 -0400

    update references

--- routes/multiplayer.js
+++ routes/multiplayer.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile('multiplayer.html', { root: './static' });
+    res.sendFile('index.html', { root: './client/multiplayer' });
 });
 
 router.get('/*', (req, res) => {
-    res.sendFile('room.html', { root: './static' });
+    res.sendFile('room.html', { root: './client/multiplayer' });
 })
 
 module.exports = router;
--- routes/tossups.js
+++ routes/tossups.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile('tossups.html', { root: './static' });
+    res.sendFile('tossups.html', { root: './client/singleplayer/tossups' });
 });
 
 module.exports = router;
