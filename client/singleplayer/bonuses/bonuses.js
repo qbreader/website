dde876af415709a32c31e8a9a91f7265d7f40247
Geoffrey Wu <geoffrey.feifei@gmail.com>
2022-07-16 12:34:59 -0400

    more code refactoring for clarity

--- client/singleplayer/bonuses/bonuses.js
+++ client/singleplayer/bonuses/bonuses.js
@@ -1,12 +1,22 @@
 var setTitle = '';
 var packetNumbers = [];
 var currentPacketNumber = -1;
+var validCategories;
+var validSubcategories;
 
 var questions = [{}];
 var currentQuestionNumber = 0;
 
 var onQuestion = true;
 var currentBonusPart = -1;
+
+/**
+ * Clears user stats.
+ */
+function clearStats() {
+    sessionStorage.setItem('stats', [0, 0, 0, 0]);
+    updateStatDisplay();
+}
 
 function createBonusPart(bonusPartNumber, bonusText) {
     let input = document.createElement('input');
@@ -38,30 +48,7 @@ function createBonusPart(bonusPartNumber, bonusText) {
     document.getElementById('question').appendChild(row);
 }
 
-/**
- * Called when the users wants to reveal the next bonus part.
- */
-function revealBonusPart() {
-    if (currentBonusPart > 2) return;
-
-    if (onQuestion) {
-        createBonusPart(currentBonusPart, questions[currentQuestionNumber]['parts'][currentBonusPart]);
-    } else {
-        let paragraph = document.createElement('p');
-        paragraph.appendChild(document.createTextNode('ANSWER: ' + questions[currentQuestionNumber]['answers'][currentBonusPart]));
-        document.getElementById(`bonus-part-${currentBonusPart + 1}`).appendChild(paragraph);
-        currentBonusPart++;
-    }
-
-    onQuestion = !onQuestion;
-
-    if (currentBonusPart > 2) {
-        document.getElementById('reveal').disabled = true;
-        document.getElementById('next').innerHTML = 'Next';
-    }
-}
-
-function updateStats() {
+function getPointsForCurrentBonus() {
     let statsArray = sessionStorage.stats.split(',');
 
     var pointsOnBonus = 0;
@@ -71,29 +58,7 @@ function updateStats() {
 
     statsArray[3 - Math.round(pointsOnBonus / 10)]++;
     sessionStorage.setItem('stats', statsArray);
-}
-
-/**
- * Calculates that points per bonus and updates the display.
- */
-function updateStatDisplay() {
-    let statsArray = sessionStorage.stats.split(',');
-
-    let numBonuses = parseInt(statsArray[0]) + parseInt(statsArray[1]) + parseInt(statsArray[2]) + parseInt(statsArray[3]);
-    let points = 30 * parseInt(statsArray[0]) + 20 * parseInt(statsArray[1]) + 10 * parseInt(statsArray[2]) || 0;
-    let ppb = Math.round(100 * points / numBonuses) / 100 || 0;
-
-    let includePlural = (numBonuses == 1) ? '' : 'es';
-    document.getElementById('statline').innerHTML
-        = `${ppb} points per bonus with ${numBonuses} bonus${includePlural} seen (${statsArray[0]}/${statsArray[1]}/${statsArray[2]}/${statsArray[3]}, ${points} pts)`;
-}
-
-/**
- * Clears user stats.
- */
-function clearStats() {
-    sessionStorage.setItem('stats', [0, 0, 0, 0]);
-    updateStatDisplay();
+    return pointsOnBonus;
 }
 
 /**
@@ -129,15 +94,160 @@ async function loadAndReadBonus() {
         document.getElementById('set-title-info').innerHTML = setTitle;
         document.getElementById('packet-number-info').innerHTML = currentPacketNumber;
         document.getElementById('question-number-info').innerHTML = currentQuestionNumber + 1;
-    
+
         currentBonusPart = 0;
-    
+
         // Update the question text:
-    
+
         let paragraph = document.createElement('p');
         paragraph.appendChild(document.createTextNode(questions[currentQuestionNumber]['leadin']));
         document.getElementById('question').appendChild(paragraph);
 
         revealBonusPart();
     }
+}
+
+/**
+ * Called when the users wants to reveal the next bonus part.
+ */
+function revealBonusPart() {
+    if (currentBonusPart > 2) return;
+
+    if (onQuestion) {
+        createBonusPart(currentBonusPart, questions[currentQuestionNumber]['parts'][currentBonusPart]);
+    } else {
+        let paragraph = document.createElement('p');
+        paragraph.appendChild(document.createTextNode('ANSWER: ' + questions[currentQuestionNumber]['answers'][currentBonusPart]));
+        document.getElementById(`bonus-part-${currentBonusPart + 1}`).appendChild(paragraph);
+        currentBonusPart++;
+    }
+
+    onQuestion = !onQuestion;
+
+    if (currentBonusPart > 2) {
+        document.getElementById('reveal').disabled = true;
+        document.getElementById('next').innerHTML = 'Next';
+    }
+}
+
+/**
+ * Calculates that points per bonus and updates the display.
+ */
+function updateStatDisplay() {
+    let statsArray = sessionStorage.stats.split(',');
+
+    let numBonuses = parseInt(statsArray[0]) + parseInt(statsArray[1]) + parseInt(statsArray[2]) + parseInt(statsArray[3]);
+    let points = 30 * parseInt(statsArray[0]) + 20 * parseInt(statsArray[1]) + 10 * parseInt(statsArray[2]) || 0;
+    let ppb = Math.round(100 * points / numBonuses) / 100 || 0;
+
+    let includePlural = (numBonuses == 1) ? '' : 'es';
+    document.getElementById('statline').innerHTML
+        = `${ppb} points per bonus with ${numBonuses} bonus${includePlural} seen (${statsArray[0]}/${statsArray[1]}/${statsArray[2]}/${statsArray[3]}, ${points} pts)`;
+}
+
+// Event listeners
+document.getElementById('start').addEventListener('click', async function () {
+    this.blur();
+    onQuestion = true;
+    initialize();
+    let [setYear, setName] = parseSetTitle(setTitle);
+    await getPacket(setYear, setName, currentPacketNumber, 'bonuses').then(async (data) => {
+        questions = data;
+        await loadAndReadBonus();
+    });
+});
+
+document.getElementById('reveal').addEventListener('click', function () {
+    this.blur();
+    revealBonusPart();
+});
+
+document.getElementById('next').addEventListener('click', async function () {
+    this.blur();
+
+    if (this.innerHTML === 'Next') {
+        getPointsForCurrentBonus();
+        updateStatDisplay();
+    }
+
+    onQuestion = true;
+    await loadAndReadBonus();
+});
+
+document.getElementById('clear-stats').addEventListener('click', function () {
+    this.blur();
+    clearStats();
+});
+
+document.querySelectorAll('#categories input').forEach(input => {
+    input.addEventListener('click', function (event) {
+        this.blur();
+        validCategories = JSON.parse(localStorage.getItem('validCategories'));
+        validSubcategories = JSON.parse(localStorage.getItem('validSubcategories'));
+        [validCategories, validSubcategories] = updateCategory(input.id, validCategories, validSubcategories);
+        localStorage.setItem('validCategories', JSON.stringify(validCategories));
+        localStorage.setItem('validSubcategories', JSON.stringify(validSubcategories));
+    });
+});
+
+document.querySelectorAll('#subcategories input').forEach(input => {
+    input.addEventListener('click', function (event) {
+        this.blur();
+        validSubcategories = updateSubcategory(input.id, validSubcategories);
+        localStorage.setItem('validSubcategories', JSON.stringify(validSubcategories));
+    });
+});
+
+document.getElementById('set-title').addEventListener('change', function () {
+    localStorage.setItem('setTitleBonusSave', this.value);
+});
+
+document.getElementById('packet-number').addEventListener('change', function () {
+    localStorage.setItem('packetNumberBonusSave', this.value);
+});
+
+document.getElementById('question-number').addEventListener('change', function () {
+    localStorage.setItem('questionNumberBonusSave', document.getElementById('question-number').value);
+});
+
+window.onload = () => {
+    if (sessionStorage.getItem('stats') === null) {
+        sessionStorage.setItem('stats', [0, 0, 0, 0]);
+    }
+
+    if (localStorage.getItem('validCategories') === null) {
+        localStorage.setItem('validCategories', '[]');
+        validCategories = [];
+    } else {
+        validCategories = JSON.parse(localStorage.getItem('validCategories'));
+    }
+    
+    if (localStorage.getItem('validSubcategories') === null) {
+        localStorage.setItem('validSubcategories', '[]');
+        validSubcategories = [];
+    } else {
+        validSubcategories = JSON.parse(localStorage.getItem('validSubcategories'));
+    }
+    
+    if (localStorage.getItem('setTitleBonusSave')) {
+        setTitle = localStorage.getItem('setTitleBonusSave');
+        document.getElementById('set-title').value = setTitle;
+        let [year, name] = parseSetTitle(setTitle);
+        (async () => {
+            maxPacketNumber = await getNumPackets(year, name);
+            document.getElementById('packet-number').placeholder = `Packet #s (1-${maxPacketNumber})`;
+        })();
+    }
+
+    if (localStorage.getItem('packetNumberBonusSave')) {
+        document.getElementById('packet-number').value = localStorage.getItem('packetNumberBonusSave');
+    }
+
+    if (localStorage.getItem('questionNumberBonusSave')) {
+        document.getElementById('question-number').value = localStorage.getItem('questionNumberBonusSave');
+    }
+
+    loadCategoryModal(validCategories, validSubcategories);
+
+    updateStatDisplay();
 }
