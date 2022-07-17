52c87e9637c366bce55271f7430bba9cb7b7d8aa
Geoffrey Wu <geoffrey.feifei@gmail.com>
2022-07-17 00:48:51 -0400

    code reorganization and directly redirect from herokuapp to https

--- client/redirect.js
+++ client/redirect.js
@@ -5,3 +5,7 @@ if (location.pathname.endsWith('/') && location.pathname.length > 1) {
 if (location.origin === 'http://www.qbreader.org') {
     location.href = 'https://www.qbreader.org' + location.pathname;
 }
+
+if (document.URL.substring(0, 30) === 'https://qbreader.herokuapp.com') {
+    window.location.href = 'https://www.qbreader.org' + document.URL.substring(30);
+}
