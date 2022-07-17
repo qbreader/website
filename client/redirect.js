01a747f50e420b501d8657a8752424e22cc3790e
Geoffrey Wu <geoffrey.feifei@gmail.com>
2022-07-17 00:43:11 -0400

    add redirect to https

--- client/redirect.js
+++ client/redirect.js
@@ -1,3 +1,7 @@
 if (location.pathname.endsWith('/')) {
     location.pathname = location.pathname.substring(0, location.pathname.length - 1);
 }
+
+if (location.origin === 'http://www.qbreader.org') {
+    location.href = 'https://www.qbreader.org' + location.pathname;
+}
