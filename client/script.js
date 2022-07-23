ec8613e22a2a38a3fa3ad79559475658dcef23ed
Geoffrey Wu <geoffrey.feifei@gmail.com>
2022-07-22 23:02:26 -0400

    code formatting

--- client/script.js
+++ client/script.js
@@ -1,12 +1,15 @@
-/*
- * Variables and functions common to both tossups and bonuses
- */
+if (location.pathname.endsWith('/') && location.pathname.length > 1) {
+    location.pathname = location.pathname.substring(0, location.pathname.length - 1);
+}
+
+if (['http://www.qbreader.org', 'http://qbreader.herokuapp.com', 'https://qbreader.herokuapp.com'].includes(location.origin)) {
+    location.href = 'https://www.qbreader.org' + location.pathname;
+}
 
 var questionCounter = 1;
 var maxPacketNumber = 24;
 
 const CATEGORIES = ["Literature", "History", "Science", "Fine Arts", "Religion", "Mythology", "Philosophy", "Social Science", "Current Events", "Geography", "Other Academic", "Trash"]
-
 /**
  * Indexed by their index in the `CATEGORIES` array.
  * Categories that do not have any subcategories are not listed.
@@ -18,29 +21,203 @@ const SUBCATEGORIES = [
     ["Visual Fine Arts", "Auditory Fine Arts", "Other Fine Arts"]
 ]
 
-/** Check if two arrays have the same elements, in any order.
- * @param {Array<String>} arr1
- * @param {Array<String>} arr2
+
+/**
+ * 
+ * @param {String} setName 
+ * @returns
  */
-function arraysEqual(arr1, arr2) {
-    if (arr1.length !== arr2.length) return false;
-    arr1 = arr1.sort();
-    arr2 = arr2.sort();
-    for (let i = 0; i < arr1.length; i++) {
-        if (!arr2.includes(arr1[i])) return false;
-        if (!arr1.includes(arr2[i])) return false;
+async function getNumPackets(setName) {
+    if (setName === undefined) return 0;
+
+    return fetch(`/api/num-packets?setName=${encodeURIComponent(setName)}`)
+        .then(response => response.json())
+        .then(data => {
+            return parseInt(data);
+        });
+}
+
+
+function arrayToRange(array) {
+    if (array.length === 0) return '';
+
+    array = [...new Set(array)];
+    array = array.sort((a, b) => a - b);
+
+    let string = '';
+    let lastRangeStart = array[0];
+    let lastRangeEnd = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] - lastRangeEnd === 1) {
+            lastRangeEnd = array[i];
+        } else {
+            if (lastRangeStart === lastRangeEnd) {
+                string = `${string}, ${lastRangeStart}`;
+            } else {
+                string = `${string}, ${lastRangeStart}-${lastRangeEnd}`;
+            }
+            lastRangeStart = array[i];
+            lastRangeEnd = array[i];
+        }
     }
+
+    if (lastRangeStart === lastRangeEnd) {
+        string = `${string}, ${lastRangeStart}`;
+    } else {
+        string = `${string}, ${lastRangeStart}-${lastRangeEnd}`;
+    }
+
+    return string.slice(2);
+}
+
+
+function createTossupCard(question, packetNumber, questionNumber) {
+    if (!question || Object.keys(question).length === 0) return;
+
+    // append a card containing the question to the history element
+    let card = document.createElement('div');
+    card.className = 'card my-2';
+
+    let cardHeader = document.createElement('div');
+    cardHeader.className = 'card-header';
+    cardHeader.setAttribute('data-bs-toggle', 'collapse');
+    cardHeader.setAttribute('data-bs-target', '#question-' + questionCounter);
+    cardHeader.innerHTML = question.answer;
+    card.appendChild(cardHeader);
+
+    let cardContainer = document.createElement('div');
+    cardContainer.id = 'question-' + questionCounter;
+    cardContainer.className = 'card-container collapse';
+
+    let cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+
+    let cardText = document.createElement('p');
+    cardText.className = 'card-text';
+    cardText.innerHTML = question.question;
+    cardBody.appendChild(cardText);
+
+    let cardFooter = document.createElement('div');
+    cardFooter.className = 'card-footer';
+
+    let cardFooterText = document.createElement('small');
+    cardFooterText.className = 'text-muted';
+    cardFooterText.innerHTML = `${document.getElementById('set-name').value} / ${question.category} / ${question.subcategory}`;
+    cardFooter.appendChild(cardFooterText);
+
+    let cardFooterText2 = document.createElement('small');
+    cardFooterText2.className = 'text-muted float-end';
+    cardFooterText2.innerHTML = `Packet ${packetNumber} / Question ${questionNumber}`;
+    cardFooter.appendChild(cardFooterText2);
+
+    cardContainer.appendChild(cardBody);
+    cardContainer.appendChild(cardFooter);
+    card.appendChild(cardContainer);
+
+    document.getElementById('room-history').prepend(card);
+
+    questionCounter++;
+}
+
+
+/**
+ * @param {JSON} question 
+ * @param {Array<String>} validCategories
+ * @param {Array<String>} validSubcategories
+ * @returns {boolean} Whether or not the question is part of the valid category and subcategory combination.
+ */
+function isValidCategory(question, validCategories, validSubcategories) {
+    if (validCategories.length === 0 && validSubcategories.length === 0) return true;
+
+    // check if the subcategory is explicitly included (overrides missing category)
+    if (question.subcategory && validSubcategories.includes(question.subcategory)) return true;
+
+    // check if category is excluded (and subcategory is excluded)
+    if (!validCategories.includes(question['category'])) return false;
+
+    // at this point, the question category is included in the list of valid categories 
+    // check for the case where none of the subcategories are selected but the category is;
+    // in which case, the question is valid
+    if (!question.subcategory) return true;
+
+    // check to see if the category has no corresponding subcategories
+    let index = CATEGORIES.indexOf(question['category']);
+    if (!(index in SUBCATEGORIES)) return true;
+
+    // check to see if none of the subcategories of the question are selected
+    for (let i = 0; i < SUBCATEGORIES[index].length; i++) {
+        if (validSubcategories.includes(SUBCATEGORIES[index][i])) return false;
+    }
+
+    // if there are no subcategories selected in the field, then it is valid
     return true;
 }
 
+
 /**
- * Increases or decreases a session storage item by a certain amount.
- * @param {String} item - The name of the sessionStorage item.
- * @param {Number} x - The amount to increase/decrease the sessionStorage item.
+ * Updates the category modal to show the given categories and subcategories.
+ * @param {Array<String>} validCategories 
+ * @param {Array<String>} validSubcategories 
+ * @returns {void}
  */
-function shift(item, x) {
-    sessionStorage.setItem(item, parseFloat(sessionStorage.getItem(item)) + x);
+function loadCategoryModal(validCategories, validSubcategories) {
+    document.querySelectorAll('#categories input').forEach(element => element.checked = false);
+    document.querySelectorAll('#subcategories input').forEach(element => element.checked = false);
+    document.querySelectorAll('#subcategories label').forEach(element => element.classList.remove('d-none'));
+
+    if (validCategories.length === 0) {
+        validSubcategories.forEach(subcat => {
+            document.getElementById(subcat).checked = true;
+        });
+        return;
+    }
+
+    CATEGORIES.forEach((cat, index) => {
+        document.getElementById(cat).checked = validCategories.includes(cat);
+        if (validCategories.includes(cat)) {
+            if (index in SUBCATEGORIES) {
+                SUBCATEGORIES[index].forEach(subcat => {
+                    document.getElementById(subcat).checked = validSubcategories && validSubcategories.includes(subcat);
+                });
+            }
+        } else if (index in SUBCATEGORIES) {
+            SUBCATEGORIES[index].forEach(subcat => document.querySelector(`[for="${subcat}"]`).classList.add('d-none'));
+        }
+    });
 }
+
+
+function rangeToArray(string, max = 0) {
+    if (string.length === 0) {
+        string = `1-${max}`;
+    }
+
+    if (string.endsWith('-')) {
+        string = string + max;
+    }
+
+    let tokens = string.split(",");
+    let ranges = [];
+    for (let i = 0; i < tokens.length; i++) {
+        let range = tokens[i].trim().split("-");
+        if (range.length === 1) {
+            ranges.push([parseInt(range[0]), parseInt(range[0])]);
+        } else {
+            ranges.push([parseInt(range[0]), parseInt(range[1])]);
+        }
+    }
+
+    let array = [];
+    for (let i = 0; i < ranges.length; i++) {
+        for (let j = ranges[i][0]; j <= ranges[i][1]; j++) {
+            array.push(j);
+        }
+    }
+
+    return array;
+}
+
 
 /**
  * Adds the given category if it is not in the list of valid categories.
@@ -91,6 +268,7 @@ function updateCategory(category, validCategories, validSubcategories) {
     return [validCategories, validSubcategories];
 }
 
+
 /**
  * Adds the given subcategory if it is not in the list of valid subcategories.
  * Otherwise, the subcategory is removed.
@@ -109,231 +287,22 @@ function updateSubcategory(subcategory, validSubcategories) {
     return validSubcategories;
 }
 
-/**
- * Updates the category modal to show the given categories and subcategories.
- * @param {Array<String>} validCategories 
- * @param {Array<String>} validSubcategories 
- * @returns {void}
- */
-function loadCategoryModal(validCategories, validSubcategories) {
-    document.querySelectorAll('#categories input').forEach(element => element.checked = false);
-    document.querySelectorAll('#subcategories input').forEach(element => element.checked = false);
-    document.querySelectorAll('#subcategories label').forEach(element => element.classList.remove('d-none'));
-
-    if (validCategories.length === 0) {
-        validSubcategories.forEach(subcat => {
-            document.getElementById(subcat).checked = true;
-        });
-        return;
-    }
-
-    CATEGORIES.forEach((cat, index) => {
-        document.getElementById(cat).checked = validCategories.includes(cat);
-        if (validCategories.includes(cat)) {
-            if (index in SUBCATEGORIES) {
-                SUBCATEGORIES[index].forEach(subcat => {
-                    document.getElementById(subcat).checked = validSubcategories && validSubcategories.includes(subcat);
-                });
-            }
-        } else if (index in SUBCATEGORIES) {
-            SUBCATEGORIES[index].forEach(subcat => document.querySelector(`[for="${subcat}"]`).classList.add('d-none'));
-        }
-    });
-}
-
-/**
- * 
- * @param {JSON} question 
- * @param {Array<String>} validCategories
- * @param {Array<String>} validSubcategories
- * @returns {boolean} Whether or not the question is part of the valid category and subcategory combination.
- */
-function isValidCategory(question, validCategories, validSubcategories) {
-    if (validCategories.length === 0 && validSubcategories.length === 0) return true;
-
-    // check if the subcategory is explicitly included (overrides missing category)
-    if (question.subcategory && validSubcategories.includes(question.subcategory)) return true;
-
-    // check if category is excluded (and subcategory is excluded)
-    if (!validCategories.includes(question['category'])) return false;
-
-    // at this point, the question category is included in the list of valid categories 
-    // check for the case where none of the subcategories are selected but the category is;
-    // in which case, the question is valid
-    if (!question.subcategory) return true;
-
-    // check to see if the category has no corresponding subcategories
-    let index = CATEGORIES.indexOf(question['category']);
-    if (!(index in SUBCATEGORIES)) return true;
-
-    // check to see if none of the subcategories of the question are selected
-    for (let i = 0; i < SUBCATEGORIES[index].length; i++) {
-        if (validSubcategories.includes(SUBCATEGORIES[index][i])) return false;
-    }
-
-    // if there are no subcategories selected in the field, then it is valid
-    return true;
-}
-
-/**
- * 
- * @param {String} packetNumberString 
- * @param {Number} maxPacketNumber 
- * @returns {Array<Number>} An array of selected packet numbers.
- */
-function packetNumberStringToArray(packetNumberString, maxPacketNumber = 24) {
-    if (packetNumberString.length === 0 || packetNumberString.toLowerCase() === 'all') {
-        packetNumberString = `1-${maxPacketNumber}`;
-    }
-    var packetNumbers = packetNumberString.split(',');
-    for (let i = 0; i < packetNumbers.length; i++) {
-        packetNumbers[i] = packetNumbers[i].trim();
-    }
-    for (let i = 0; i < packetNumbers.length; i++) {
-        if (packetNumbers[i].toString().includes('-')) {
-            let bounds = packetNumbers[i].split('-');
-            for (let j = parseInt(bounds[0]); j <= parseInt(bounds[1]); j++) {
-                packetNumbers.push(j);
-            }
-            packetNumbers.splice(i, 1);
-            i--;
-        }
-    }
-
-    return packetNumbers;
-}
-
-/**
- * 
- * @param {String} setName 
- * @returns
- */
-async function getNumPackets(setName) {
-    if (setName === undefined) return 0;
-    return await fetch(`/api/num-packets?setName=${encodeURIComponent(setName)}`)
-        .then(response => response.json())
-        .then(data => {
-            return parseInt(data.value);
-        });
-}
-
-/**
- * @param {String} setName - The name of the set (e.g. "2021 PACE").
- * @param {String} packetNumber - The packet number of the set.
- * @return {{tossups: Array<JSON>, bonuses: Array<JSON>}} An array containing the questions.
- */
-async function getPacket(setName, packetNumber) {
-    return await fetch(`/api/packet?&setName=${encodeURIComponent(setName)}&packetNumber=${encodeURIComponent(packetNumber)}`).then(response => response.json());
-}
-
-/**
- * @param {String} setName - The name of the set (e.g. "2021 PACE").
- * @param {String} packetNumber - The packet number of the set.
- * @return {Array<JSON>} An array containing the tossups.
- */
-async function getTossups(setName, packetNumber) {
-    return await fetch(`/api/packet-tossups?&setName=${encodeURIComponent(setName)}&packetNumber=${encodeURIComponent(packetNumber)}`)
-        .then(response => response.json())
-        .then(data => data.tossups);
-}
-
-/**
- * @param {String} setName - The name of the set (e.g. "2021 PACE").
- * @param {String} packetNumber - The packet number of the set.
- * @return {Array<JSON>} An array containing the bonuses.
- */
-async function getBonuses(setName, packetNumber) {
-    return await fetch(`/api/packet-bonuses?&setName=${encodeURIComponent(setName)}&packetNumber=${encodeURIComponent(packetNumber)}`)
-        .then(response => response.json())
-        .then(data => data.bonuses);
-}
-
-/**
- * Initizalizes all variables (called when the user presses the start button).
- * @returns {Promsie<Boolean>} Whether or not the function was successful.
- */
-function initialize(alertOnFailure = true) {
-    setName = document.getElementById('set-title').value.trim();
-    if (setName.length == 0) {
-        if (alertOnFailure) alert('Please enter a set name.');
-        return false;
-    }
-
-    document.getElementById('options').classList.add('d-none');
-    document.getElementById('toggle-options').disabled = false;
-
-    packetNumbers = packetNumberStringToArray(document.getElementById('packet-number').value.trim(), maxPacketNumber);
-    currentPacketNumber = packetNumbers[0];
-
-    currentQuestionNumber = document.getElementById('question-number').value;
-    if (currentQuestionNumber == '') currentQuestionNumber = '1';  // default = 1
-    currentQuestionNumber = parseInt(currentQuestionNumber) - 2;
-
-    document.getElementById('next').disabled = false;
-    document.getElementById('next').innerHTML = 'Skip';
-
-    return true;
-}
-
-function createQuestionCard(question, packetNumber, questionNumber) {
-    if (!question || Object.keys(question).length === 0) return;
-
-    // append a card containing the question to the history element
-    let card = document.createElement('div');
-    card.className = 'card my-2';
-
-    let cardHeader = document.createElement('div');
-    cardHeader.className = 'card-header';
-    cardHeader.setAttribute('data-bs-toggle', 'collapse');
-    cardHeader.setAttribute('data-bs-target', '#question-' + questionCounter);
-    cardHeader.innerHTML = question.answer;
-    card.appendChild(cardHeader);
-
-    let cardContainer = document.createElement('div');
-    cardContainer.id = 'question-' + questionCounter;
-    cardContainer.className = 'card-container collapse';
-
-    let cardBody = document.createElement('div');
-    cardBody.className = 'card-body';
-
-    let cardText = document.createElement('p');
-    cardText.className = 'card-text';
-    cardText.innerHTML = question.question;
-    cardBody.appendChild(cardText);
-
-    let cardFooter = document.createElement('div');
-    cardFooter.className = 'card-footer';
-
-    let cardFooterText = document.createElement('small');
-    cardFooterText.className = 'text-muted';
-    cardFooterText.innerHTML = `${document.getElementById('set-title').value} / ${question.category} / ${question.subcategory}`;
-    cardFooter.appendChild(cardFooterText);
-
-    let cardFooterText2 = document.createElement('small');
-    cardFooterText2.className = 'text-muted float-end';
-    cardFooterText2.innerHTML = `Packet ${packetNumber} / Question ${questionNumber}`;
-    cardFooter.appendChild(cardFooterText2);
-
-    cardContainer.appendChild(cardBody);
-    cardContainer.appendChild(cardFooter);
-    card.appendChild(cardContainer);
-
-    document.getElementById('room-history').prepend(card);
-
-    questionCounter++;
-}
 
 document.getElementById('toggle-options').addEventListener('click', function () {
     this.blur();
     document.getElementById('options').classList.toggle('d-none');
 });
 
-document.getElementById('set-title').addEventListener('change', async function (event) {
+
+document.getElementById('set-name').addEventListener('change', async function (event) {
     maxPacketNumber = await getNumPackets(this.value);
     if (maxPacketNumber > 0) {
         document.getElementById('packet-number').placeholder = `Packet #s (1-${maxPacketNumber})`;
+    } else {
+        document.getElementById('packet-number').placeholder = 'Packet #s';
     }
 });
+
 
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
---
+++ client/singleplayer/singleplayer.js
@@ -0,0 +1,91 @@
+/**
+ * @param {String} setName - The name of the set (e.g. "2021 PACE").
+ * @param {String} packetNumber - The packet number of the set.
+ * @return {Array<JSON>} An array containing the bonuses.
+ */
+ async function getBonuses(setName, packetNumber) {
+    return await fetch(`/api/packet-bonuses?&setName=${encodeURIComponent(setName)}&packetNumber=${encodeURIComponent(packetNumber)}`)
+        .then(response => response.json())
+        .then(data => data.bonuses);
+}
+
+
+/**
+ * @param {String} setName - The name of the set (e.g. "2021 PACE").
+ * @param {String} packetNumber - The packet number of the set.
+ * @return {{tossups: Array<JSON>, bonuses: Array<JSON>}} An array containing the questions.
+ */
+async function getPacket(setName, packetNumber) {
+    return await fetch(`/api/packet?&setName=${encodeURIComponent(setName)}&packetNumber=${encodeURIComponent(packetNumber)}`).then(response => response.json());
+}
+
+
+/**
+ * @param {String} setName - The name of the set (e.g. "2021 PACE").
+ * @param {String} packetNumber - The packet number of the set.
+ * @return {Array<JSON>} An array containing the tossups.
+ */
+async function getTossups(setName, packetNumber) {
+    return await fetch(`/api/packet-tossups?&setName=${encodeURIComponent(setName)}&packetNumber=${encodeURIComponent(packetNumber)}`)
+        .then(response => response.json())
+        .then(data => data.tossups);
+}
+
+
+/**
+ * Initizalizes all variables (called when the user presses the start button).
+ * @returns {Promsie<Boolean>} Whether or not the function was successful.
+ */
+function initialize(alertOnFailure = true) {
+    setName = document.getElementById('set-name').value.trim();
+    if (setName.length == 0) {
+        if (alertOnFailure) alert('Please enter a set name.');
+        return false;
+    }
+
+    document.getElementById('options').classList.add('d-none');
+    document.getElementById('toggle-options').disabled = false;
+
+    packetNumbers = rangeToArray(document.getElementById('packet-number').value.trim(), maxPacketNumber);
+    currentPacketNumber = packetNumbers[0];
+
+    currentQuestionNumber = document.getElementById('question-number').value;
+    if (currentQuestionNumber == '') currentQuestionNumber = '1';  // default = 1
+    currentQuestionNumber = parseInt(currentQuestionNumber) - 2;
+
+    document.getElementById('next').disabled = false;
+    document.getElementById('next').innerHTML = 'Skip';
+
+    return true;
+}
+
+
+/**
+ * Increases or decreases a session storage item by a certain amount.
+ * @param {String} item - The name of the sessionStorage item.
+ * @param {Number} x - The amount to increase/decrease the sessionStorage item.
+ */
+function shift(item, x) {
+    sessionStorage.setItem(item, parseFloat(sessionStorage.getItem(item)) + x);
+}
+
+
+document.querySelectorAll('#categories input').forEach(input => {
+    input.addEventListener('click', function (event) {
+        this.blur();
+        validCategories = JSON.parse(localStorage.getItem('validCategories'));
+        validSubcategories = JSON.parse(localStorage.getItem('validSubcategories'));
+        [validCategories, validSubcategories] = updateCategory(input.id, validCategories, validSubcategories);
+        localStorage.setItem('validCategories', JSON.stringify(validCategories));
+        localStorage.setItem('validSubcategories', JSON.stringify(validSubcategories));
+    });
+});
+
+
+document.querySelectorAll('#subcategories input').forEach(input => {
+    input.addEventListener('click', function (event) {
+        this.blur();
+        validSubcategories = updateSubcategory(input.id, validSubcategories);
+        localStorage.setItem('validSubcategories', JSON.stringify(validSubcategories));
+    });
+});
