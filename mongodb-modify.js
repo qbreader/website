4b9f1f67c92bebe3db1c909c5b60e46a57a1bc8d
Geoffrey Wu <geoffrey.feifei@gmail.com>
2022-07-26 02:35:51 -0400

    Update mongodb-modify.js

--- mongodb-modify.js
+++ mongodb-modify.js
@@ -8,7 +8,7 @@ const { MongoClient, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.MONGODB_USERNAME || 'geoffreywu42'}:${process.env.MONGODB_PASSWORD || 'password'}@qbreader.0i7oej9.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri);
-client.connect().then(() => {
+client.connect().then(async () => {
     console.log('connected to mongodb');
 
     const database = client.db('qbreader');
@@ -17,7 +17,7 @@ client.connect().then(() => {
     const questions = database.collection('questions');
 
     let counter = 0;
-    sets.find({}).forEach(async set => {
+    await sets.find({name: "2013 NASAT"}).forEach(async set => {
         if (counter % 10 === 0) {
             console.log(counter, set.name);
         }
