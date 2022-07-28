d62b9db55a9712056ac87471193e8f729603b59a
Geoffrey Wu <42471355+geoffrey-wu@users.noreply.github.com>
2022-07-28 09:07:34 -0400

    move tossups and bonus files out of unnecessary folder

--- routes/bonuses.js
+++ routes/bonuses.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile('bonuses.html', { root: './client/singleplayer/bonuses' });
+    res.sendFile('bonuses.html', { root: './client/singleplayer' });
 });
 
 module.exports = router;
