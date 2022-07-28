96ced107532071d06e34695e4738714bcd93e0d4
Geoffrey Wu <42471355+geoffrey-wu@users.noreply.github.com>
2022-07-28 08:57:17 -0400

    move keybinds to same file as rest of JS

--- client/singleplayer/tossups/tossups.js
+++ client/singleplayer/tossups/tossups.js
@@ -301,6 +301,28 @@ document.getElementById('toggle-correct').addEventListener('click', function () 
 });
 
 
+document.addEventListener('keydown', (event) => {
+    if (document.activeElement.tagName === 'INPUT') return;
+
+    switch (event.key) {
+        case ' ':
+            document.getElementById('buzz').click();
+            // Prevent spacebar from scrolling the page:
+            if (event.target == document.body) event.preventDefault();
+            break;
+        case 'n':
+            document.getElementById('next').click();
+            break;
+        case 'p':
+            document.getElementById('pause').click();
+            break;
+        case 's':
+            document.getElementById('start').click();
+            break;
+    }
+});
+
+
 window.onload = () => {
     if (localStorage.getItem('setNameTossupSave')) {
         setName = localStorage.getItem('setNameTossupSave');
