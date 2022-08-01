308bdb052e115f562881dbda5c92ee7b5da975f2
Geoffrey Wu <geoffrey.feifei@gmail.com>
2022-08-01 11:13:27 -0400

    fix #39

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const server = require('http').createServer(app);
 const port = process.env.PORT || 3000;
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ server });
-const uuid = require('uuid');
 
 const rooms = require('./rooms');
 
@@ -48,57 +47,22 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/client/singleplayer/tossups.html');
 });
 
-var sockets = {};
 
 wss.on('connection', (ws) => {
     console.log(`Connection in room ${ws.protocol}`);
-    ws.userId = uuid.v4();
-    if (ws.protocol in sockets) {
-        sockets[ws.protocol].push(ws);
-    } else {
-        rooms.createRoom(ws.protocol);
-        sockets[ws.protocol] = [ws];
-    }
 
-    ws.send(JSON.stringify({
-        type: 'user-id',
-        userId: ws.userId
-    }));
+    rooms.createRoom(ws.protocol);
+    rooms.createPlayer(ws.protocol, ws);
 
-    ws.on('message', async (message) => {
+    ws.on('message', (message) => {
         message = JSON.parse(message);
-        message.userId = ws.userId;
-        message = await rooms.parseMessage(ws.protocol, message);
         console.log(message);
-
-        if (message.type === 'join' || message.type === 'change-username') {
-            ws.username = message.username;
-        }
-
-        for (let i = 0; i < sockets[ws.protocol].length; i++) {
-            sockets[ws.protocol][i].send(JSON.stringify(message));
-        }
+        rooms.parseMessage(ws.protocol, message);
     });
 
-    ws.on('close', () => {
-        console.log(`User ${ws.username} closed connection in room ${ws.protocol}`);
-        let message = { type: 'leave', userId: ws.userId, username: ws.username };
-        rooms.parseMessage(ws.protocol, message);
-
-        for (let i = 0; i < sockets[ws.protocol].length; i++) {
-            if (sockets[ws.protocol][i] === ws) continue;
-
-            sockets[ws.protocol][i].send(JSON.stringify(message));
-        }
-
-        sockets[ws.protocol] = sockets[ws.protocol].filter(socket => socket !== ws);
-
-        // Delete stale rooms
-        if (sockets[ws.protocol].length === 0) {
-            console.log(`Deleted room ${ws.protocol}`);
-            rooms.deleteRoom(ws.protocol);
-            delete sockets[ws.protocol];
-        }
+    ws.on('close', async () => {
+        rooms.deletePlayer(ws.protocol, ws);
+        rooms.pruneRoom(ws.protocol);
     });
 });
 
