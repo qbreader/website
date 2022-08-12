d34affa22ae2e9a35b38287f1971fdce99f7faf7
Geoffrey Wu <geoffrey.feifei@gmail.com>
2022-08-11 22:57:20 -0400

    delete unnecessary imports

--- routes/multiplayer.js
+++ routes/multiplayer.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const rooms = require('../rooms');
-
-const roomList = [];
 
 router.get('/', (req, res) => {
     res.sendFile('index.html', { root: './client/multiplayer' });
