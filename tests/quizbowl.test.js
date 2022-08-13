768e031081cfa3c51ae4a3526e9f84fa6e2ed4a8
Geoffrey Wu <geoffrey.feifei@gmail.com>
2022-08-13 14:06:47 -0400

    unit tests for quizbowl.checkAnswer() function

---
+++ tests/quizbowl.test.js
@@ -0,0 +1,48 @@
+const quizbowl = require('../quizbowl.js');
+
+const answerline1 = "<b><u>Manchester</u></b>"
+const answerline2 = "<b><u>damper</u></b>s [or <b><u>dashpot</u></b>s; accept (tuned) mass <b><u>damper</u></b>s]"
+const answerline3 = "Alice <b><u>Munro</u></b> [or Alice Ann <b><u>Munro</u></b> or Alice Ann <b><u>Laidlaw</u></b>] (The first two stories are “The Progress of Love” and “Hateship, Friendship, Courtship, Loveship, Marriage.”)"
+const answerline4 = "<b><u>light pollution</u></b> [accept <b><u>sky glow</u></b> or <b><u>glare</u></b>; prompt on artificial <u>light</u>] (The Znamya project created large mirrors intended to light up the night.)"
+const answerline5 = "<b><u>Sahara</u></b> Desert [or aṣ-<b><u>Ṣaḥrāʾ al-Kubrā</u></b> or <b><u>Aneẓruf</u></b> <b><u>Ameqran</u></b>; accept <b><u>Deshret</u></b>; accept Nilo-<b><u>Saharan</u></b> languages; accept <b><u>Tibesti</u></b> Mountains or <b><u>Tassili</u></b> n’Ajjer or <b><u>Aïr</u></b> Mountains until read; accept the <b><u>Fezzan</u></b> or <b><u>Illizi</u></b> or <b><u>Hoggar</u></b> Mountains or <b><u>Ahaggar</u></b> or <b><u>Tanezrouft</u></b> or <b><u>Grand Erg</u></b> Oriental; accept <b><u>Ténéré</u></b> until “Tenerian”; prompt on <u>Algeria</u> or <u>Chad</u> or <u>Libya</u> or <u>Niger</u> or <u>Egypt</u>; prompt on North <u>Africa</u> or <u>Maghreb</u>]"
+const answerline6 = "<b><u>primatology</u></b> [or word forms; accept any answers about the study of great <b><u>ape</u></b>s, nonhuman <b><u>primate</u></b>s, <b><u>gorilla</u></b>s, <b><u>bonobo</u></b>s, or <b><u>chimp</u></b>anzees; prompt on the study of <u>monkey</u>s or <u>simian</u>s; prompt on word forms of <u>ethology</u>, <u>biology</u>, <u>anthropology</u>, or evolutionary or social <u>psychology</u>; prompt on the study of <u>animal</u>s with “what type of animals?”]"
+const answerline7 = "primatology [or word forms; accept any answers about the study of great apes, nonhuman primates, gorillas, bonobos, or chimpanzees; prompt on the study of monkeys or simians; prompt on word forms of ethology, biology, anthropology, or evolutionary or social psychology; prompt on the study of animals with “what type of animals?”]"
+
+const tests = [
+    // single answerline
+    ['accept', answerline1, 'manchester'],
+    ['accept', answerline1, 'MANCHESTER'],
+    ['reject', answerline1, 'London'],
+    
+    // multiple answerlines
+    ['accept', answerline2, 'dampers'],
+    ['accept', answerline2, 'dashpot'],
+    ['accept', answerline2, 'tuned mass dampers'],
+
+    // authors and proper names
+    ['accept', answerline3, 'Munro'],
+    ['accept', answerline3, 'Alice Munro'],
+
+    // prompts and multiple underlined words
+    ['prompt', answerline4, 'light'],
+    ['accept', answerline4, 'light pollution'],
+    ['reject', answerline4, 'pollution'],
+
+    // partial underlining and words not underlined
+    ['reject', answerline5, 'Desert'],
+    ['accept', answerline6, 'chimpanzee'],
+    ['accept', answerline6, 'chimp'],
+
+    // unformatted answerlines
+    ['reject', answerline7, 'chimp'], // TODO: make this accept
+    ['accept', answerline7, 'chimpanzee'],
+];
+
+console.log('TESTING quizbowl.checkAnswer()');
+tests.forEach(test => {
+    const expected = test[0];
+    const answerline = test[1];
+    const givenAnswer = test[2];
+    const result = quizbowl.checkAnswer(answerline, givenAnswer);
+    console.assert(expected === result, `expected "${expected}" but got "${result}" for given answer "${givenAnswer}"`);
+})
