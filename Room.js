d290a2a27bbb47fd5e381f293b73e931800f1ddd
Geoffrey Wu <geoffrey.feifei@gmail.com>
2022-08-20 18:51:20 -0500

    remove trailing whitespace

--- Room.js
+++ Room.js
@@ -20,10 +20,10 @@ class Room {
         this.wordIndex = 0;
 
         this.query = {
-            difficulties: [4, 5], 
-            packetNumbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24], 
-            setName: '2022 PACE NSC', 
-            categories: [], 
+            difficulties: [4, 5],
+            packetNumbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
+            setName: '2022 PACE NSC',
+            categories: [],
             subcategories: []
         };
 
