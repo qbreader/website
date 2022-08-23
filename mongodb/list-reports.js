5747bd5fcba00b355ca9332cd2a12719bed3ab07
Geoffrey Wu <geoffrey.feifei@gmail.com>
2022-08-22 21:46:49 -0500

    add colors to `list-reports.js`

--- mongodb/list-reports.js
+++ mongodb/list-reports.js
@@ -2,6 +2,7 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
+const Colors = require('../Colors');
 const { MongoClient, ObjectId } = require('mongodb');
 
 const uri = `mongodb+srv://${process.env.MONGODB_USERNAME || 'geoffreywu42'}:${process.env.MONGODB_PASSWORD || 'password'}@qbreader.0i7oej9.mongodb.net/?retryWrites=true&w=majority`;
@@ -17,6 +18,16 @@ const sets = database.collection('sets');
 // list all questions that have a report
 questions.find({ reports: { $exists: true }, type: 'tossup' }, { sort: { reports: -1 } }).forEach(async question => {
     const setName = (await sets.findOne({ _id: question.set }, { projection: { _id: 0, name: 1 } })).name;
-    const string = `Number of reports: ${question.reports}\n${setName} Packet ${question.packetNumber} Question ${question.questionNumber}\n${question.question}\nANSWER: ${question.answer}`;
+    question.answer = question.formatted_answer ?? question.answer;
+    question.answer = question.answer.replace(/<b>/g, Colors.BOLD);
+    question.answer = question.answer.replace(/<\/b>/g, Colors.ENDC);
+    question.answer = question.answer.replace(/<u>/g, Colors.UNDERLINE);
+    question.answer = question.answer.replace(/<\/u>/g, Colors.ENDC);
+    const string = `
+${setName} Packet ${question.packetNumber} Question ${question.questionNumber}
+Set ID: ${Colors.OKBLUE}${question.set}${Colors.ENDC}
+${question.question}
+ANSWER: ${question.answer}
+Number of reports: ${question.reports}`;
     console.log(string);
 });
