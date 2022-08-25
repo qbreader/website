a3d14a9feef1985390ec39f41f2ef8c5af1a79cf
Geoffrey Wu <geoffrey.feifei@gmail.com>
2022-08-25 18:14:09 -0500

    make high contrast question text toggleable

--- client/script.js
+++ client/script.js
@@ -256,13 +256,6 @@ function updateSubcategory(subcategory, validSubcategories) {
     return validSubcategories;
 }
 
-
-document.getElementById('toggle-options').addEventListener('click', function () {
-    this.blur();
-    document.getElementById('options').classList.toggle('d-none');
-});
-
-
 document.getElementById('set-name').addEventListener('change', async function (event) {
     maxPacketNumber = await getNumPackets(this.value);
     if (maxPacketNumber > 0) {
@@ -272,6 +265,19 @@ document.getElementById('set-name').addEventListener('change', async function (e
     }
 });
 
+document.getElementById('toggle-high-contrast-question-text').addEventListener('click', function () {
+    this.blur();
+    if (this.checked) {
+        document.getElementById('question').classList.add('high-contrast-question-text');
+    } else {
+        document.getElementById('question').classList.remove('high-contrast-question-text');
+    }
+});
+
+document.getElementById('toggle-options').addEventListener('click', function () {
+    this.blur();
+    document.getElementById('options').classList.toggle('d-none');
+});
 
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
