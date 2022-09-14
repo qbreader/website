17f84bdeafa984bb63b393b4ff9b89e5edab8add
Geoffrey Wu <geoffrey.feifei@gmail.com>
2022-09-14 14:21:08 -0400

    close #68

--- client/quizbowl.js
+++ client/quizbowl.js
@@ -261,6 +261,12 @@ document.getElementById('set-name').addEventListener('change', async function (e
     }
 });
 
+document.getElementById('font-size').addEventListener('input', function () {
+    localStorage.setItem('font-size', this.value);
+    document.getElementById('font-size-display').innerHTML = this.value;
+    document.getElementById('question').style.setProperty('font-size', `${this.value}px`);
+});
+
 document.getElementById('toggle-high-contrast-question-text').addEventListener('click', function () {
     this.blur();
     if (this.checked) {
@@ -276,6 +282,12 @@ document.getElementById('toggle-options').addEventListener('click', function () 
     this.blur();
     document.getElementById('options').classList.toggle('d-none');
 });
+
+if (localStorage.getItem('font-size')) {
+    document.getElementById('font-size').value = localStorage.getItem('font-size');
+    document.getElementById('font-size-display').innerHTML = localStorage.getItem('font-size');
+    document.getElementById('question').style.setProperty('font-size', `${localStorage.getItem('font-size')}px`);
+}
 
 if (localStorage.getItem('high-contrast-question-text') === 'true') {
     document.getElementById('toggle-high-contrast-question-text').checked = true;
