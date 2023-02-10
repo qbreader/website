4571f57c4dd317bbaec9e13510bc07a256530548
Geoffrey Wu <geoffrey.feifei@gmail.com>
2023-02-10 14:14:17 -0500

    log ips

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const ipFilter = require('express-ipfilter').IpFilter;
 const IpDeniedError = require('express-ipfilter').IpDeniedError;
 const ips = require('../BLOCKED_IPS');
 console.log(`Blocked IPs: ${ips}`);
-app.use(ipFilter(ips, { mode: 'deny', log: false }));
+app.use(ipFilter(ips, { mode: 'deny', log: true }));
 
 app.use((err, req, res, _next) => {
     if (err instanceof IpDeniedError) {
