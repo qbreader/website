2dc554bb225b5bab3c16b29219c77ed5e1836dae
Sky "g3ner1c" Hong <g3ner1c-sky@pm.me>
2023-03-29 20:09:20 -0400

    split api docs into individual pages

--- routes/api-docs.js
+++ routes/api-docs.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const fs = require('fs');
 const router = express.Router();
+const docsDir = './client/api-docs';
 
 router.get('/', (req, res) => {
     res.sendFile('index.html', { root: './client/api-docs' });
 });
 
+// routes every html doc in /client/api-docs
+fs.readdirSync(docsDir).forEach(file => {
+    console.log(file);
+    if (file.endsWith('.html') && file !== 'index.html') {
+        const route = file.substring(0, file.length - 5);
+        router.get(`/${route}`, (req, res) => {
+            res.sendFile(file, { root: './client/api-docs' });
+        });
+    }
+});
+
+fs.readdirSync(`${docsDir}/multiplayer`).forEach(file => {
+    console.log(file);
+    if (file.endsWith('.html') && file !== 'index.html') {
+        const route = file.substring(0, file.length - 5);
+        router.get(`/multiplayer/${route}`, (req, res) => {
+            res.sendFile(file, { root: './client/api-docs/multiplayer' });
+        });
+    }
+});
 module.exports = router;
