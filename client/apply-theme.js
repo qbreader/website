f6aa276902d9d7caef73e28df3f72f78414e9124
alopezlago <alopezlago@cs.hmc.edu>
2023-04-26 13:22:55 -0700

    Fix double quote issue

--- client/apply-theme.js
+++ client/apply-theme.js
@@ -1,7 +1,7 @@
 if (localStorage.getItem('color-theme') === 'dark') {
     document.querySelector('#custom-css').setAttribute('href', '/bootstrap/dark.css');
 } else if (localStorage.getItem('color-theme') === 'light') {
-    document.querySelector('#custom-css').setAttribute('href', "/bootstrap/light.css");
+    document.querySelector('#custom-css').setAttribute('href', '/bootstrap/light.css');
 } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     // Get OS preferred color scheme
     document.querySelector('#custom-css').setAttribute('href', '/bootstrap/dark.css');
