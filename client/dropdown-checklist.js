e95d528505725a4a9a8cb984547c95b675d41583
Geoffrey Wu <geoffrey.feifei@gmail.com>
2023-05-08 21:37:55 -0400

    close #98

---
+++ client/dropdown-checklist.js
@@ -0,0 +1,26 @@
+function getDropdownValues(id) {
+    const values = [];
+    Array.from(document.getElementById(id).children).forEach(li => {
+        const input = li.querySelector('input');
+        if (input.checked) {
+            values.push(parseInt(input.value));
+        }
+    });
+    return values;
+}
+
+
+Array.from(document.querySelectorAll('.checkbox-menu input[type=\'checkbox\']')).forEach(input => {
+    input.addEventListener('change', function () {
+        if (input.checked)
+            input.closest('li').classList.add('active');
+        else
+            input.closest('li').classList.remove('active');
+    });
+});
+
+Array.from(document.querySelectorAll('.allow-focus')).forEach(dropdown => {
+    dropdown.addEventListener('click', function (e) {
+        e.stopPropagation();
+    });
+});
