6f8e2b01301bc9a4cfae46816e0fb44dacab3e56
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-05-20 15:00:19 -0500

    show bonus values and difficulties, if possible

--- client/database/index.js
+++ client/database/index.js
@@ -344,7 +344,7 @@ function BonusCard({
     }
   }), indices.map(i => /*#__PURE__*/React.createElement("div", {
     key: `${bonus._id}-${i}`
-  }, /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement("p", null, "[10]\xA0", /*#__PURE__*/React.createElement("span", {
+  }, /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement("p", null, "[", bonus.values ? bonus.values[i] : 10, bonus.difficulties ? bonus.difficulties[i] : '', "]\xA0", /*#__PURE__*/React.createElement("span", {
     dangerouslySetInnerHTML: {
       __html: highlightedBonus.parts[i]
     }
