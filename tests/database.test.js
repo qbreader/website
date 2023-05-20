1826b6475de3add1f3f1c9e442fee2c0266c994f
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-05-20 14:52:01 -0500

    update database import

--- tests/database.test.js
+++ tests/database.test.js
@@ -1,4 +1,4 @@
-const { getQuery, getPacket, getSet, getRandomQuestions, getNumPackets, reportQuestion } = require('../server/database');
+const { getQuery, getPacket, getSet, getRandomQuestions, getNumPackets, reportQuestion } = require('../database/questions');
 
 async function testTiming(count) {
     const packetNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
