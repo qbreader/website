2feaee0dc36360a7d71a3aa0658675a202c51644
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-06-18 18:24:06 -0400

    add geoword leaderboard

---
+++ client/geoword/leaderboard.js
@@ -0,0 +1,33 @@
+const division = decodeURIComponent(window.location.pathname.split('/')[4]);
+const packetName = window.location.pathname.split('/')[3];
+const packetTitle = titleCase(packetName);
+
+document.getElementById('packet-name').textContent = packetTitle;
+document.getElementById('division').textContent = division;
+
+fetch('/geoword/api/leaderboard?' + new URLSearchParams({ packetName, division }))
+    .then(response => response.json())
+    .then(data => {
+        const { leaderboard } = data;
+
+        let innerHTML = '';
+        for (const index in leaderboard) {
+            const { username, numberCorrect, points, pointsPerTossup, averageCorrectCelerity } = leaderboard[index];
+
+            innerHTML += `
+                <tr>
+                    <td>${parseInt(index) + 1}</td>
+                    <th scope="row">${escapeHTML(username)}</th>
+                    <td>${(averageCorrectCelerity ?? 0.0).toFixed(3)}</td>
+                    <td>${numberCorrect}</td>
+                    <td>${points}</td>
+                    <td>${(pointsPerTossup ?? 0.0).toFixed(2)}</td>
+                </tr>
+            `;
+        }
+        document.getElementById('leaderboard-body').innerHTML = innerHTML;
+    });
+
+function escapeHTML(unsafe) {
+    return unsafe.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll('\'', '&#039;');
+}
