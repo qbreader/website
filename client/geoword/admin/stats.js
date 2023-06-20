ad5e978a7a8396ddf90121065c4ff6d3b0362a91
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-06-20 00:29:54 -0400

    add admin stats

---
+++ client/geoword/admin/stats.js
@@ -0,0 +1,38 @@
+const division = decodeURIComponent(window.location.pathname.split('/')[5]);
+const packetName = window.location.pathname.split('/')[4];
+const packetTitle = titleCase(packetName);
+
+document.getElementById('packet-name').textContent = packetTitle;
+document.getElementById('division').textContent = division;
+
+fetch('/api/geoword/admin/stats?' + new URLSearchParams({ packetName, division }))
+    .then(response => response.json())
+    .then(data => {
+        const { stats } = data;
+
+        console.log(stats);
+
+        let innerHTML = '<hr>';
+
+        for (const i in stats) {
+            innerHTML += `
+            <div class="row mb-3">
+                <div class="col-6">
+                    <div><b>#${stats[i].tossup.questionNumber}</b></div>
+                    <div><b>Times heard:</b> ${stats[i].timesHeard}</div>
+                    <div><b>Number correct:</b> ${stats[i].numberCorrect}</div>
+                    <div><b>Answer:</b> ${stats[i].tossup.formatted_answer}</div>
+                </div>
+                <div class="col-6">
+                    <div><b>Best buzz:</b> ${stats[i].bestUsername}</div>
+                    <div><b>Best celerity:</b> ${(stats[i].bestCelerity ?? 0.0).toFixed(3)}</div>
+                    <div><b>Average correct celerity:</b> ${(stats[i].averageCorrectCelerity ?? 0).toFixed(3)}</div>
+                    <div><b>Average points:</b> ${(stats[i].averagePoints ?? 0.0).toFixed(2)}</div>
+                </div>
+            </div>
+            <hr>
+            `;
+        }
+
+        document.getElementById('stats').innerHTML = innerHTML;
+    });
