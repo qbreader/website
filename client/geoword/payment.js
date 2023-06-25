78ea41fd4f85012956235b863e1c609205eb1824
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-06-25 01:12:45 -0400

    set up geoword payment

---
+++ client/geoword/payment.js
@@ -0,0 +1,127 @@
+const packetName = window.location.pathname.split('/').pop();
+document.getElementById('packet-name').textContent = titleCase(packetName);
+
+// This is your test publishable API key.
+const STRIPE_PUBLISHABLE_KEY = 'pk_test_51NManVKG9mAb0mOpXFfZQf7OW5szwT4uPjxyzQYETfd866Ht5EQEWT1BeXoIotloxdPxfpijP5lQRJtc2Xh6tEfL00OqWKGvM3';
+const stripe = Stripe(STRIPE_PUBLISHABLE_KEY);
+
+let elements;
+
+initialize();
+checkStatus();
+
+document.querySelector('#payment-form').addEventListener('submit', handleSubmit);
+
+let emailAddress = '';
+// Fetches a payment intent and captures the client secret
+async function initialize() {
+    const response = await fetch('/api/geoword/create-payment-intent', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ packetName: packetName }),
+    });
+    const { clientSecret } = await response.json();
+
+    const isDarkTheme = (localStorage.getItem('color-theme') === 'dark') || (!localStorage.getItem('color-theme') && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
+    const appearance = {
+        theme: isDarkTheme ? 'night' : 'stripe'
+    };
+    elements = stripe.elements({ appearance, clientSecret });
+
+    const linkAuthenticationElement = elements.create('linkAuthentication');
+    linkAuthenticationElement.mount('#link-authentication-element');
+
+    linkAuthenticationElement.on('change', (event) => {
+        emailAddress = event.value.email;
+    });
+
+    const paymentElementOptions = {
+        layout: 'tabs',
+    };
+
+    const paymentElement = elements.create('payment', paymentElementOptions);
+    paymentElement.mount('#payment-element');
+}
+
+async function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+
+    const { error } = await stripe.confirmPayment({
+        elements,
+        confirmParams: {
+            // Make sure to change this to your payment completion page
+            return_url: 'http://localhost:4242/geoword/confirmation',
+            receipt_email: emailAddress,
+        },
+    });
+
+    // This point will only be reached if there is an immediate error when
+    // confirming the payment. Otherwise, your customer will be redirected to
+    // your `return_url`. For some payment methods like iDEAL, your customer will
+    // be redirected to an intermediate site first to authorize the payment, then
+    // redirected to the `return_url`.
+    if (error.type === 'card_error' || error.type === 'validation_error') {
+        showMessage(error.message);
+    } else {
+        showMessage('An unexpected error occurred.');
+    }
+
+    setLoading(false);
+}
+
+// Fetches the payment intent status after payment submission
+async function checkStatus() {
+    const clientSecret = new URLSearchParams(window.location.search).get(
+        'payment_intent_client_secret'
+    );
+
+    if (!clientSecret) {
+        return;
+    }
+
+    const { paymentIntent } = await stripe.retrievePaymentIntent(clientSecret);
+
+    switch (paymentIntent.status) {
+    case 'succeeded':
+        showMessage('Payment succeeded!');
+        break;
+    case 'processing':
+        showMessage('Your payment is processing.');
+        break;
+    case 'requires_payment_method':
+        showMessage('Your payment was not successful, please try again.');
+        break;
+    default:
+        showMessage('Something went wrong.');
+        break;
+    }
+}
+
+// ------- UI helpers -------
+
+function showMessage(messageText) {
+    const messageContainer = document.querySelector('#payment-message');
+
+    messageContainer.classList.remove('hidden');
+    messageContainer.textContent = messageText;
+
+    setTimeout(function () {
+        messageContainer.classList.add('hidden');
+        messageContainer.textContent = '';
+    }, 4000);
+}
+
+// Show a spinner on payment submission
+function setLoading(isLoading) {
+    if (isLoading) {
+    // Disable the button and show a spinner
+        document.querySelector('#submit').disabled = true;
+        document.querySelector('#spinner').classList.remove('hidden');
+        document.querySelector('#button-text').classList.add('hidden');
+    } else {
+        document.querySelector('#submit').disabled = false;
+        document.querySelector('#spinner').classList.add('hidden');
+        document.querySelector('#button-text').classList.remove('hidden');
+    }
+}
---
+++ routes/api/webhook.js
@@ -0,0 +1,54 @@
+import * as geoword from '../../database/geoword.js';
+
+import { Router } from 'express';
+import { ObjectId } from 'mongodb';
+import stripeClass from 'stripe';
+
+const router = Router();
+const stripe = new stripeClass(process.env.STRIPE_SECRET_KEY);
+
+// This is your test secret API key.
+// Replace this endpoint secret with your endpoint's unique secret
+// If you are testing with the CLI, find the secret by running 'stripe listen'
+// If you are using an endpoint defined with the API or dashboard, look in your webhook settings
+// at https://dashboard.stripe.com/webhooks
+const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
+
+router.post('/', (req, res) => {
+    const sig = req.headers['stripe-signature'];
+
+    let event;
+
+    try {
+        event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
+    } catch (err) {
+        res.status(400).send(`Webhook Error: ${err.message}`);
+        return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+    case 'payment_intent.succeeded': {
+        const paymentIntentSucceeded = event.data.object;
+        // Then define and call a function to handle the event payment_intent.succeeded
+        const { user_id, packetName } = paymentIntentSucceeded.metadata;
+        geoword.recordPayment({ packetName, user_id: new ObjectId(user_id) });
+        break;
+    }
+
+    case 'charge.succeeded':
+        // We can safely ignore this
+        res.send();
+        return;
+
+    default:
+        // ... handle other event types
+        // console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    console.log('Received event:', event.type);
+    res.send();
+});
+
+export default router;
