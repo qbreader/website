0e8bfaa9e41bc736f09010defbc18ad1ef6593c8
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-07-03 22:39:01 -0400

    fix category reports

--- constants.js
+++ constants.js
@@ -7,21 +7,40 @@ const DEFAULT_MAX_YEAR = 2023;
 const DIFFICULTIES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const CATEGORIES = ['Literature', 'History', 'Science', 'Fine Arts', 'Religion', 'Mythology', 'Philosophy', 'Social Science', 'Current Events', 'Geography', 'Other Academic', 'Trash'];
-// const SUBCATEGORIES = [
-//     ['American Literature', 'British Literature', 'Classical Literature', 'European Literature', 'World Literature', 'Other Literature'],
-//     ['American History', 'Ancient History', 'European History', 'World History', 'Other History'],
-//     ['Biology', 'Chemistry', 'Physics', 'Math', 'Other Science'],
-//     ['Visual Fine Arts', 'Auditory Fine Arts', 'Other Fine Arts'],
-//     ['Religion'],
-//     ['Mythology'],
-//     ['Philosophy'],
-//     ['Social Science'],
-//     ['Current Events'],
-//     ['Geography'],
-//     ['Other Academic'],
-//     ['Trash']
-// ];
 const SUBCATEGORIES_FLATTENED = ['American Literature', 'British Literature', 'Classical Literature', 'European Literature', 'World Literature', 'Other Literature', 'American History', 'Ancient History', 'European History', 'World History', 'Other History', 'Biology', 'Chemistry', 'Physics', 'Math', 'Other Science', 'Visual Fine Arts', 'Auditory Fine Arts', 'Other Fine Arts', 'Religion', 'Mythology', 'Philosophy', 'Social Science', 'Current Events', 'Geography', 'Other Academic', 'Trash'];
+const SUBCATEGORY_TO_CATEGORY = {
+    'American Literature': 'Literature',
+    'British Literature': 'Literature',
+    'Classical Literature': 'Literature',
+    'European Literature': 'Literature',
+    'World Literature': 'Literature',
+    'Other Literature': 'Literature',
+    'Drama': 'Literature',
+    'Poetry': 'Literature',
+    'Long Fiction': 'Literature',
+    'Short Fiction': 'Literature',
+    'American History': 'History',
+    'Ancient History': 'History',
+    'European History': 'History',
+    'World History': 'History',
+    'Other History': 'History',
+    'Biology': 'Science',
+    'Chemistry': 'Science',
+    'Physics': 'Science',
+    'Math': 'Science',
+    'Other Science': 'Science',
+    'Visual Fine Arts': 'Fine Arts',
+    'Auditory Fine Arts': 'Fine Arts',
+    'Other Fine Arts': 'Fine Arts',
+    'Religion': 'Religion',
+    'Mythology': 'Mythology',
+    'Philosophy': 'Philosophy',
+    'Social Science': 'Social Science',
+    'Current Events': 'Current Events',
+    'Geography': 'Geography',
+    'Other Academic': 'Other Academic',
+    'Trash': 'Trash',
+};
 
 /**
  * List of multiplayer permanent room names.
@@ -42,7 +61,7 @@ export {
     DEFAULT_MAX_YEAR,
     DIFFICULTIES,
     CATEGORIES,
-    // SUBCATEGORIES, // not used
+    SUBCATEGORY_TO_CATEGORY,
     SUBCATEGORIES_FLATTENED,
     PERMANENT_ROOMS,
     COOKIE_MAX_AGE,
