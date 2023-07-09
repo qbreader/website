967583dd072fe64accbf13a2e064b086fd724053
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-07-09 00:25:44 -0400

    `/geoword` endpoint backend reorganization

---
+++ routes/geoword/packet.js
@@ -0,0 +1,20 @@
+import * as geoword from '../../database/geoword.js';
+
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+    const { username } = req.session;
+    const packetName = req.params.packetName;
+    const paid = await geoword.checkPayment({ packetName, username });
+
+    if (paid) {
+        res.sendFile('packet.html', { root: './client/geoword' });
+        return;
+    }
+
+    res.redirect('/geoword/payment/' + packetName);
+});
+
+export default router;
