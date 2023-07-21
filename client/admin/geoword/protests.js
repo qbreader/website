4526dbc645b1667fdfa2cd956a49653e58ced827
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-07-21 19:45:11 -0400

    [geoword] record prompt trail

--- client/admin/geoword/protests.js
+++ client/admin/geoword/protests.js
@@ -30,6 +30,7 @@ fetch('/api/admin/geoword/protests?' + new URLSearchParams({ packetName, divisio
                     <span>
                         <b>${escapeHTML(protest.username + ': ' || '')}</b>
                         <span id="given-answer-${protest._id}">${protest.givenAnswer}</span>
+                        ${getPromptString(protest)}
                         (celerity: ${protest.celerity.toFixed(3)}) - pending review
                     </span>
                     <a id="${protest._id}" question="${questionNumber}" href="#" data-bs-toggle="modal" data-bs-target="#resolve-protest-modal">Resolve protest</a>
@@ -37,10 +38,10 @@ fetch('/api/admin/geoword/protests?' + new URLSearchParams({ packetName, divisio
             }
 
             for (const protest of protests.filter(protest => protest.questionNumber === questionNumber && !protest.pendingProtest)) {
-                const resolution = `resolved: ${protest.decision} (reason: ${protest.reason || 'none'})`;
+                const resolution = `resolution: ${protest.decision} ${getProtestReasonString(protest)}`;
 
                 innerHTML += `<li class="list-group-item">
-                    <span><b>${escapeHTML(protest.username + ': ' || '')}</b><span>${protest.givenAnswer}</span> - ${resolution}</span>
+                    <span><b>${escapeHTML(protest.username + ': ' || '')}</b><span>${protest.givenAnswer} ${getPromptString(protest)}</span> - ${resolution}</span>
                 </li>`;
             }
 
@@ -76,3 +77,25 @@ document.getElementById('resolve-protest-submit').addEventListener('click', () =
         }
     });
 });
+
+function getPromptString(protest) {
+    if (!protest?.prompts) {
+        return '';
+    }
+
+    let string = '(prompted on: ';
+    for (const answer of protest.prompts) {
+        string += answer + ', ';
+    }
+    string = string.slice(0, -2);
+    string = string + ')';
+    return string;
+}
+
+function getProtestReasonString(protest) {
+    if (!protest?.reason || protest.reason === '') {
+        return '';
+    }
+
+    return `(reason: ${protest.reason})`;
+}
