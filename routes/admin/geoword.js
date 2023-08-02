c9ed7d10a13cd1eef2cb2078955493dd85d57e73
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-08-01 23:36:15 -0400

    let admins compare players

--- routes/admin/geoword.js
+++ routes/admin/geoword.js
@@ -6,6 +6,10 @@ router.get('/', async (req, res) => {
     res.sendFile('index.html', { root: './client/admin/geoword' });
 });
 
+router.get('/compare', async (req, res) => {
+    res.sendFile('compare.html', { root: './client/admin/geoword' });
+});
+
 router.get('/protests/:packetName/:division', async (req, res) => {
     res.sendFile('protests.html', { root: './client/admin/geoword' });
 });
