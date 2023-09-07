84ff13b603f5c9b11c87fa78e8e0acd411538724
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-09-07 17:34:31 -0400

    toggle stars from star view

--- client/user/stars/bonuses.js
+++ client/user/stars/bonuses.js
@@ -3,44 +3,70 @@ fetch('/auth/stars/')
     .then(response => response.bonuses)
     .then(bonuses => {
         const bonusList = document.getElementById('bonus-list');
-        bonuses.forEach(bonus => {
+
+        for (const index in bonuses) {
+            const bonus = bonuses[index];
+
+            let cardBody = '';
+            for (let i = 0; i < bonus.parts.length; i++) {
+                cardBody += `
+                    <hr></hr>
+                    <p>
+                        ${getBonusPartLabel(bonus, i)} ${bonus.parts[i]}
+                    </p>
+                    <div>ANSWER: ${(bonus?.formatted_answers ?? bonus.answers)[i]}</div>
+                `;
+            }
+
             bonusList.innerHTML += `
-                <div className="card my-2">
-                <div className="card-header d-flex justify-content-between">
-                    <b className="clickable">
-                        ${bonus.set.name} | ${bonus.category} | ${bonus.subcategory} ${bonus.alternate_subcategory ? ' (' + bonus.alternate_subcategory + ')' : ''} | ${bonus.difficulty}
-                    </b>
-                    <b className="clickable" data-bs-toggle="collapse" data-bs-target=${`#question-${_id}`}>
-                        Packet {bonus.packet.number} | Question {bonus.questionNumber}
-                    </b>
-                </div>
-                <div className="card-container collapse show" id=${`question-${_id}`}>
-                    <div className="card-body">
-                        <p dangerouslySetInnerHTML={{ __html: highlightedBonus.leadin }}></p>
-                        {indices.map((i) =>
-                            <div key=${`${bonus._id}-${i}`}>
-                                <hr></hr>
-                                <p>
-                                    <span>{getBonusPartLabel(bonus, i)} </span>
-                                    <span dangerouslySetInnerHTML={{ __html: highlightedBonus.parts[i] }}></span>
-                                </p>
-                                <div>
-                                    <b>ANSWER: </b>
-                                    <span dangerouslySetInnerHTML={{ __html: (highlightedBonus?.formatted_answers ?? highlightedBonus.answers)[i] }}></span>
-                                </div>
-                            </div>,
-                        )}
+                <div class="card my-2">
+                    <div class="card-header d-flex justify-content-between">
+                        <b class="clickable">
+                            ${bonus.set.name} | ${bonus.category} | ${bonus.subcategory} ${bonus.alternate_subcategory ? ' (' + bonus.alternate_subcategory + ')' : ''} | ${bonus.difficulty}
+                        </b>
+                        <a href="#" class="selected" id="star-bonus-${index}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                        </svg>
+                        </a>
                     </div>
-                    <div className="card-footer clickable" data-bs-toggle="modal" data-bs-target="#bonus-stats-modal">
-                        <small className="text-muted">${packetName ? 'Packet ' + packetName : <span>&nbsp;</span>}</small>
-                        <small className="text-muted float-end">
-                            <a href="#" onClick={onClick} id=report-question-${_id} data-bs-toggle="modal" data-bs-target="#report-question-modal">
-                                Report Question
-                            </a>
-                        </small>
+                    <div class="card-container collapse show" id=question-${bonus._id}>
+                        <div class="card-body">
+                            <p>${bonus.leadin}</p>
+                            ${cardBody}
+                        </div>
+                        <div class="card-footer clickable" data-bs-toggle="modal" data-bs-target="#bonus-stats-modal">
+                            <small class="text-muted">${bonus.packet.name ? 'Packet ' + bonus.packet.name : '&nbsp;'}</small>
+                            <small class="text-muted float-end">
+                                Packet ${bonus.packet.number} / Question ${bonus.questionNumber}
+                            </small>
+                        </div>
                     </div>
                 </div>
-            </div>
             `;
-        });
+
+            document.getElementById(`star-bonus-${index}`).addEventListener('click', function () {
+                if (this.classList.contains('selected')) {
+                    this.innerHTML = `
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+                        <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                    </svg>`;
+                    unstarBonus(bonus._id);
+                } else {
+                    this.innerHTML = `
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                    </svg>`;
+                    starBonus(bonus._id);
+                }
+
+                this.classList.toggle('selected');
+            });
+        }
     });
+
+function getBonusPartLabel(bonus, index, defaultValue = 10, defaultDifficulty = '') {
+    const value = bonus.values ? (bonus.values[index] ?? defaultValue) : defaultValue;
+    const difficulty = bonus.difficulties ? (bonus.difficulties[index] ?? defaultDifficulty) : defaultDifficulty;
+    return `[${value}${difficulty}]`;
+}
--- client/user/stars/tossups.js
+++ client/user/stars/tossups.js
@@ -3,14 +3,20 @@ fetch('/auth/stars/')
     .then(response => response.tossups)
     .then(tossups => {
         const tossupList = document.getElementById('tossup-list');
-        tossups.forEach(tossup => {
+
+        for (const index in tossups) {
+            const tossup = tossups[index];
             tossupList.innerHTML += `
                 <div class="card mb-2">
-                    <div class="card-header">
+                    <div class="card-header d-flex justify-content-between">
                         <b>${tossup.set.name} | ${tossup.category} | ${tossup.subcategory} ${tossup.alternate_subcategory ? ' (' + tossup.alternate_subcategory + ')' : ''} | ${tossup.difficulty}</b>
-                        <b class="float-end">Packet ${tossup.packet.number} | Question ${tossup.questionNumber}</b>
+                        <a href="#" class="selected" id="star-tossup-${index}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                        </svg>
+                        </a>
                     </div>
-                    <div class="card-container" id="question-${tossup._id}">
+                    <div class="card-container collapse show" id="question-${tossup._id}">
                         <div class="card-body">
                             <span>${tossup.question}</span>&nbsp;
                             <hr></hr>
@@ -19,13 +25,29 @@ fetch('/auth/stars/')
                         <div class="card-footer">
                             <small class="text-muted">${tossup.packet.name ? 'Packet ' + tossup.packet.name : '&nbsp;'}</small>
                             <small class="text-muted float-end">
-                                <a href="#" onClick={onClick} id="report-question-${tossup._id}" data-bs-toggle="modal" data-bs-target="#report-question-modal">
-                                    Report Question
-                                </a>
+                                Packet ${tossup.packet.number} / Question ${tossup.questionNumber}
                             </small>
                         </div>
                     </div>
                 </div>
             `;
-        });
+
+            document.getElementById(`star-tossup-${index}`).addEventListener('click', function () {
+                if (this.classList.contains('selected')) {
+                    this.innerHTML = `
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+                        <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                    </svg>`;
+                    unstarTossup(tossup._id);
+                } else {
+                    this.innerHTML = `
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                    </svg>`;
+                    starTossup(tossup._id);
+                }
+
+                this.classList.toggle('selected');
+            });
+        }
     });
