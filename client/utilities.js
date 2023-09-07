0f7d2816bf8ffb9dbed5266fd7e8b4ab6c5cdf25
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-09-07 17:34:31 -0400

    toggle stars from star view

--- client/utilities.js
+++ client/utilities.js
@@ -348,66 +348,6 @@ function reportQuestion() {
 }
 
 
-function starBonus(bonus_id) {
-    fetch('/auth/stars/star-bonus', {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ bonus_id }),
-    }).then(response => {
-        if (!response.ok) {
-            alert('There was an error starring the bonus.');
-        }
-    }).catch(_error => {
-        alert('There was an error starring the bonus.');
-    });
-}
-
-
-function starTossup(tossup_id) {
-    fetch('/auth/stars/star-tossup', {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ tossup_id }),
-    }).then(response => {
-        if (!response.ok) {
-            alert('There was an error starring the bonus.');
-        }
-    }).catch(_error => {
-        alert('There was an error starring the bonus.');
-    });
-}
-
-
-function unstarBonus(bonus_id) {
-    fetch('/auth/stars/unstar-bonus', {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ bonus_id }),
-    }).then(response => {
-        if (!response.ok) {
-            alert('There was an error unstarring the bonus.');
-        }
-    }).catch(_error => {
-        alert('There was an error unstarring the bonus.');
-    });
-}
-
-
-function unstarTossup(tossup_id) {
-    fetch('/auth/stars/unstar-tossup', {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ tossup_id }),
-    }).then(response => {
-        if (!response.ok) {
-            alert('There was an error unstarring the bonus.');
-        }
-    }).catch(_error => {
-        alert('There was an error unstarring the bonus.');
-    });
-}
-
-
 /**
  * Adds the given category if it is not in the list of valid categories.
  * Otherwise, the category is removed.
