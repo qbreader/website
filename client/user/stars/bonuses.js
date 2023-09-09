223a58bc622dfd61f01ba8c4e65af4ba223ef58b
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-09-09 00:35:54 -0400

    fix eventListener bug on star list page

--- client/user/stars/bonuses.js
+++ client/user/stars/bonuses.js
@@ -4,9 +4,7 @@ fetch('/auth/stars/')
     .then(bonuses => {
         const bonusList = document.getElementById('bonus-list');
 
-        for (const index in bonuses) {
-            const bonus = bonuses[index];
-
+        for (const bonus of bonuses) {
             let cardBody = '';
             for (let i = 0; i < bonus.parts.length; i++) {
                 cardBody += `
@@ -24,7 +22,7 @@ fetch('/auth/stars/')
                         <b class="clickable" data-bs-toggle="collapse" data-bs-target="#question-${bonus._id}" aria-expanded="true">
                             ${bonus.set.name} | ${bonus.category} | ${bonus.subcategory} ${bonus.alternate_subcategory ? ' (' + bonus.alternate_subcategory + ')' : ''} | ${bonus.difficulty}
                         </b>
-                        <a href="#" class="selected" id="star-bonus-${index}">
+                        <a class="clickable selected" id="star-bonus-${bonus._id}">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
                             <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                         </svg>
@@ -44,8 +42,10 @@ fetch('/auth/stars/')
                     </div>
                 </div>
             `;
+        }
 
-            document.getElementById(`star-bonus-${index}`).addEventListener('click', async function () {
+        for (const bonus of bonuses) {
+            document.getElementById(`star-bonus-${bonus._id}`).addEventListener('click', async function () {
                 if (this.classList.contains('selected')) {
                     this.innerHTML = `
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
