e9cdfbf132b24357e41fa7b2c9217fb720f6ced6
suhas <hi@suhas.one>
2023-11-22 12:58:14 -0600

    tossup by id

---
+++ routes/api/tossup-by-id.js
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import { getTossupById } from '../../database/questions.js';
+import { ObjectId } from 'mongodb';
+// import { BSONTypeError } from 'bson'
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+    if (!req.query.id) {
+        res.status(400).send('Tossup ID not specified');
+        return;
+    }
+    let oid;
+    try {
+        oid = ObjectId(req.query.id);
+    } catch (b) {
+        res.status(400).send('Invalid Tossup ID');
+        return;
+    }
+    const tossup = await getTossupById(oid);
+    console.log(tossup);
+    if (tossup === null) {
+        res.status(404).send(`Tossup with ID ${req.query.id} was not found`);
+        return;
+    }
+
+    res.header('Access-Control-Allow-Origin', '*');
+    res.json({ tossup });
+
+});
+
+export default router;
