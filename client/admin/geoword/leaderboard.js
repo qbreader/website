fe4ee4affe14c4a77065d1c130689cb18689226e
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-11-23 13:30:47 -0600

    skip numbering for inactive geoword buzzes

--- client/admin/geoword/leaderboard.js
+++ client/admin/geoword/leaderboard.js
@@ -10,13 +10,14 @@ fetch('/api/admin/geoword/leaderboard?' + new URLSearchParams({ packetName, divi
     .then(data => {
         const { leaderboard } = data;
 
+        let numberSkipped = 0;
         let innerHTML = '';
         for (const index in leaderboard) {
             const { username, numberCorrect, points, pointsPerTossup, averageCorrectCelerity, active } = leaderboard[index];
 
             innerHTML += `
                 <tr ${!active && 'class="table-info"'}>
-                    <td>${parseInt(index) + 1}</td>
+                    <td>${active ? parseInt(index) + 1 - numberSkipped : ''}</td>
                     <th scope="row">${escapeHTML(username)}</th>
                     <td>${(averageCorrectCelerity ?? 0.0).toFixed(3)}</td>
                     <td>${numberCorrect}</td>
@@ -24,6 +25,10 @@ fetch('/api/admin/geoword/leaderboard?' + new URLSearchParams({ packetName, divi
                     <td>${(pointsPerTossup ?? 0.0).toFixed(2)}</td>
                 </tr>
             `;
+
+            if (!active) {
+                numberSkipped++;
+            }
         }
         document.getElementById('leaderboard-body').innerHTML = innerHTML;
     });
