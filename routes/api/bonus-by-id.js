c94d8fecd64a10f41dfcaa1bdcc207f8d9822151
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-11-23 10:12:49 -0600

    add get bonus by id

---
+++ routes/api/bonus-by-id.js
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+import { getBonusById } from '../../database/questions.js';
+import { ObjectId } from 'mongodb';
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+    if (!req.query.id) {
+        res.status(400).send('Bonus ID not specified');
+        return;
+    }
+    let oid;
+    try {
+        oid = ObjectId(req.query.id);
+    } catch (b) {
+        res.status(400).send('Invalid Bonus ID');
+        return;
+    }
+    const bonus = await getBonusById(oid);
+    if (bonus === null) {
+        res.status(404).send(`Bonus with ID ${req.query.id} was not found`);
+        return;
+    }
+
+    res.header('Access-Control-Allow-Origin', '*');
+    res.json({ bonus });
+
+});
+
+export default router;
