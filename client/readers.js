dd7dd58e4b96649fb1cb92c73cb0cceddf432d4b
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-11-23 10:57:30 -0600

    add settings page

--- client/readers.js
+++ client/readers.js
@@ -3,36 +3,16 @@
  * and retrieval of `SET_LIST`.
  */
 
-document.getElementById('font-size').addEventListener('input', function () {
-    localStorage.setItem('font-size', this.value);
-    document.getElementById('font-size-display').textContent = this.value;
-    document.getElementById('question').style.setProperty('font-size', `${this.value}px`);
-});
-
-document.getElementById('toggle-high-contrast-question-text').addEventListener('click', function () {
-    this.blur();
-    if (this.checked) {
-        document.getElementById('question').classList.add('high-contrast-question-text');
-        localStorage.setItem('high-contrast-question-text', 'true');
-    } else {
-        document.getElementById('question').classList.remove('high-contrast-question-text');
-        localStorage.removeItem('high-contrast-question-text');
-    }
-});
-
 document.getElementById('toggle-options').addEventListener('click', function () {
     this.blur();
     document.getElementById('options').classList.toggle('d-none');
 });
 
 if (localStorage.getItem('font-size')) {
-    document.getElementById('font-size').value = localStorage.getItem('font-size');
-    document.getElementById('font-size-display').textContent = localStorage.getItem('font-size');
     document.getElementById('question').style.setProperty('font-size', `${localStorage.getItem('font-size')}px`);
 }
 
 if (localStorage.getItem('high-contrast-question-text') === 'true') {
-    document.getElementById('toggle-high-contrast-question-text').checked = true;
     document.getElementById('question').classList.add('high-contrast-question-text');
 }
 
