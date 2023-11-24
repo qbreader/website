38070006f8c089c9ba910959a330f05f29d81cd3
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-11-24 13:47:23 -0600

    reorganize routing

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,18 @@
+import aboutRouter from './about.js';
+import adminRouter from './admin/index.js';
+import apiRouter from './api/index.js';
+import apiDocsRouter from './api-docs.js';
+import authRouter from './auth/index.js';
+import backupsRouter from './backups.js';
+import bonusesRouter from './bonuses.js';
+import databaseRouter from './database.js';
+import geowordRouter from './geoword/index.js';
+import multiplayerRouter from './multiplayer.js';
+import settingsRouter from './settings.js';
+import tossupsRouter from './tossups.js';
+import userRouter from './user.js';
+import webhookRouter from './api/webhook.js';
+
 import { Router } from 'express';
 const router = Router();
 
@@ -62,6 +77,29 @@ router.get('/index', (req, res) => {
 
 router.get('/users', (req, res) => {
     res.redirect(`/user${req.url}`);
+});
+
+/**
+ * Routes:
+ */
+
+router.use('/about', aboutRouter);
+router.use('/admin', adminRouter);
+router.use('/api', apiRouter);
+router.use('/api-docs', apiDocsRouter);
+router.use('/auth', authRouter);
+router.use('/backups', backupsRouter);
+router.use('/bonuses', bonusesRouter);
+router.use('/db', databaseRouter);
+router.use('/geoword', geowordRouter);
+router.use('/multiplayer', multiplayerRouter);
+router.use('/settings', settingsRouter);
+router.use('/tossups', tossupsRouter);
+router.use('/user', userRouter);
+router.use('/webhook', webhookRouter);
+
+router.use((req, res) => {
+    res.sendFile(req.url, { root: './client' });
 });
 
 export default router;
