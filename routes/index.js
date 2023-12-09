2fde3160f47ccc14578d8491dbf137c1b3823d8e
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-12-09 17:22:48 -0500

    add singleplayer landing page

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,11 @@ import apiRouter from './api/index.js';
 import apiDocsRouter from './api-docs.js';
 import authRouter from './auth/index.js';
 import backupsRouter from './backups.js';
-import bonusesRouter from './bonuses.js';
 import databaseRouter from './database.js';
 import geowordRouter from './geoword/index.js';
 import multiplayerRouter from './multiplayer.js';
 import settingsRouter from './settings.js';
-import tossupsRouter from './tossups.js';
+import singleplayerRouter from './singleplayer/index.js';
 import userRouter from './user.js';
 import webhookRouter from './api/webhook.js';
 
@@ -67,12 +66,20 @@ router.get('/api-info', (req, res) => {
     res.redirect('/api-docs');
 });
 
+router.get('/bonuses', (_req, res) => {
+    res.redirect('/singleplayer/bonuses');
+});
+
 router.get('/database', (_req, res) => {
     res.redirect('/db');
 });
 
 router.get('/index', (req, res) => {
     res.redirect('/');
+});
+
+router.get('/tossups', (req, res) => {
+    res.redirect('/singleplayer/tossups');
 });
 
 router.get('/users', (req, res) => {
@@ -89,12 +96,11 @@ router.use('/api', apiRouter);
 router.use('/api-docs', apiDocsRouter);
 router.use('/auth', authRouter);
 router.use('/backups', backupsRouter);
-router.use('/bonuses', bonusesRouter);
 router.use('/db', databaseRouter);
 router.use('/geoword', geowordRouter);
 router.use('/multiplayer', multiplayerRouter);
 router.use('/settings', settingsRouter);
-router.use('/tossups', tossupsRouter);
+router.use('/singleplayer', singleplayerRouter);
 router.use('/user', userRouter);
 router.use('/webhook', webhookRouter);
 
