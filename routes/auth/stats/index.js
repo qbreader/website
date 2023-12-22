4a41f229b3b07b6b945b0467a20a31089f195b79
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-12-22 17:33:54 -0600

    reorganize /auth/stats endpoints

---
+++ routes/auth/stats/index.js
@@ -0,0 +1,29 @@
+import singleBonusRouter from './single-bonus.js';
+import singleTossupRouter from './single-tossup.js';
+
+import { checkToken } from '../../../server/authentication.js';
+
+import { Router } from 'express';
+
+const router = Router();
+
+router.use((req, res, next) => {
+    const { username, token } = req.session;
+    if (!checkToken(username, token)) {
+        delete req.session;
+        res.sendStatus(401);
+        return;
+    }
+
+    if (!checkToken(username, token, true)) {
+        res.sendStatus(403);
+        return;
+    }
+
+    next();
+});
+
+router.use('/single-bonus', singleBonusRouter);
+router.use('/single-tossup', singleTossupRouter);
+
+export default router;
