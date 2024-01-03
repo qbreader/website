3d58bd0ac53773c050cf595b786b47119bea751e
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-01-02 22:16:38 -0600

    remove jazz for now

--- client/database/index.js
+++ client/database/index.js
@@ -1,7 +1,10 @@
 const paginationShiftLength = screen.width > 992 ? 10 : 5;
 const CATEGORY_BUTTONS = [['Literature', 'primary'], ['History', 'success'], ['Science', 'danger'], ['Fine Arts', 'warning'], ['Religion', 'secondary'], ['Mythology', 'secondary'], ['Philosophy', 'secondary'], ['Social Science', 'secondary'], ['Current Events', 'secondary'], ['Geography', 'secondary'], ['Other Academic', 'secondary'], ['Trash', 'secondary']];
 const SUBCATEGORY_BUTTONS = [['American Literature', 'primary'], ['British Literature', 'primary'], ['Classical Literature', 'primary'], ['European Literature', 'primary'], ['World Literature', 'primary'], ['Other Literature', 'primary'], ['American History', 'success'], ['Ancient History', 'success'], ['European History', 'success'], ['World History', 'success'], ['Other History', 'success'], ['Biology', 'danger'], ['Chemistry', 'danger'], ['Physics', 'danger'], ['Other Science', 'danger'], ['Visual Fine Arts', 'warning'], ['Auditory Fine Arts', 'warning'], ['Other Fine Arts', 'warning']];
-const ALTERNATE_SUBCATEGORY_BUTTONS = [['Drama', 'primary'], ['Poetry', 'primary'], ['Long Fiction', 'primary'], ['Short Fiction', 'primary'], ['Math', 'danger'], ['Astronomy', 'danger'], ['Computer Science', 'danger'], ['Earth Science', 'danger'], ['Engineering', 'danger'], ['Jazz', 'warning']];
+const ALTERNATE_SUBCATEGORY_BUTTONS = [['Drama', 'primary'], ['Poetry', 'primary'], ['Long Fiction', 'primary'], ['Short Fiction', 'primary'], ['Math', 'danger'], ['Astronomy', 'danger'], ['Computer Science', 'danger'], ['Earth Science', 'danger'], ['Engineering', 'danger']
+// ['Jazz', 'warning'],
+];
+
 let validCategories = [];
 let validSubcategories = [];
 let validAlternateSubcategories = [];
