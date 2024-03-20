388ee0d7d5c686bed79103e47ce06cdf37e77b2a
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-03-20 00:12:33 -0400

    close #245: alternate_subcategory in personal stats

--- routes/auth/user-stats/bonus.js
+++ routes/auth/user-stats/bonus.js
@@ -1,3 +1,4 @@
+import getAlternateSubcategoryStats from '../../../database/account-info/user-stats/get-alternate-subcategory-stats.js';
 import getCategoryStats from '../../../database/account-info/user-stats/get-category-stats.js';
 import getSubcategoryStats from '../../../database/account-info/user-stats/get-subcategory-stats.js';
 
@@ -9,11 +10,17 @@ router.get('/', async (req, res) => {
     const { username } = req.session;
     const { difficulties, setName, includeMultiplayer, includeSingleplayer, startDate, endDate } = req.query;
 
-    const [categoryStats, subcategoryStats] = await Promise.all([
+    const [categoryStats, subcategoryStats, alternateSubcategoryStats] = await Promise.all([
         await getCategoryStats({ username, questionType: 'bonus', difficulties, setName, includeMultiplayer, includeSingleplayer, startDate, endDate }),
         await getSubcategoryStats({ username, questionType: 'bonus', difficulties, setName, includeMultiplayer, includeSingleplayer, startDate, endDate }),
+        await getAlternateSubcategoryStats({ username, questionType: 'bonus', difficulties, setName, includeMultiplayer, includeSingleplayer, startDate, endDate }),
     ]);
-    res.json({ categoryStats, subcategoryStats });
+
+    res.json({
+        'category-stats': categoryStats,
+        'subcategory-stats': subcategoryStats,
+        'alternate-subcategory-stats': alternateSubcategoryStats,
+    });
 });
 
 export default router;
--- routes/auth/user-stats/tossup.js
+++ routes/auth/user-stats/tossup.js
@@ -1,3 +1,4 @@
+import getAlternateSubcategoryStats from '../../../database/account-info/user-stats/get-alternate-subcategory-stats.js';
 import getBestBuzz from '../../../database/account-info/user-stats/get-best-buzz.js';
 import getCategoryStats from '../../../database/account-info/user-stats/get-category-stats.js';
 import getSubcategoryStats from '../../../database/account-info/user-stats/get-subcategory-stats.js';
@@ -10,12 +11,19 @@ router.get('/', async (req, res) => {
     const { username } = req.session;
     const { difficulties, setName, includeMultiplayer, includeSingleplayer, startDate, endDate } = req.query;
 
-    const [bestBuzz, categoryStats, subcategoryStats] = await Promise.all([
+    const [bestBuzz, categoryStats, subcategoryStats, alternateSubcategoryStats] = await Promise.all([
         await getBestBuzz({ username, difficulties, setName, includeMultiplayer, includeSingleplayer, startDate, endDate }),
         await getCategoryStats({ username, questionType: 'tossup', difficulties, setName, includeMultiplayer, includeSingleplayer, startDate, endDate }),
         await getSubcategoryStats({ username, questionType: 'tossup', difficulties, setName, includeMultiplayer, includeSingleplayer, startDate, endDate }),
+        await getAlternateSubcategoryStats({ username, questionType: 'tossup', difficulties, setName, includeMultiplayer, includeSingleplayer, startDate, endDate }),
     ]);
-    res.json({ bestBuzz, categoryStats, subcategoryStats });
+
+    res.json({
+        'best-buzz': bestBuzz,
+        'category-stats': categoryStats,
+        'subcategory-stats': subcategoryStats,
+        'alternate-subcategory-stats': alternateSubcategoryStats,
+    });
 });
 
 export default router;
