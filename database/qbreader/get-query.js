68f5453e15407640c2c83fee319d98e80c1bc322
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-04-16 14:34:21 -0400

    fix interpuncts and ł in search

--- database/qbreader/get-query.js
+++ database/qbreader/get-query.js
@@ -20,7 +20,9 @@ function unformatString(string) {
         .replace(/[\u2018-\u201B]/g, '\'')
         .replace(/[\u201C-\u201F]/g, '"')
         .replace(/[\u2026]/g, '...')
-        .replace(/[\u2032-\u2037]/g, '\'');
+        .replace(/[\u2032-\u2037]/g, '\'')
+        .replace(/[\u00B7\u22C5\u2027]/g, '') // interpuncts
+        .replace(/\u0142/g, 'l'); // ł -> l
 }
 
 function getQuerySummary(options) {
