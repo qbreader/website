19935bc56f5f3250ddd92a57933a1ce0aa07c91c
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-04-16 20:24:50 -0400

    fix `api.getSetList()`

--- client/user/stats/script.js
+++ client/user/stats/script.js
@@ -1,6 +1,6 @@
 import api from '../../api/index.js';
 
-const SET_LIST = await api.getSetList();
+const SET_LIST = api.getSetList();
 document.getElementById('set-list').innerHTML = SET_LIST.map(setName => `<option>${setName}</option>`).join('');
 
 function fillSetName(event) {
