71310f79772631b8a3ac9434d3bbd4d251a15b67
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-05-14 17:11:11 -0500

    make escapeHTML a function in utilities.js

--- client/multiplayer/room.js
+++ client/multiplayer/room.js
@@ -2,7 +2,7 @@ import account from '../accounts.js';
 import questionStats from '../auth/question-stats.js';
 import api from '../api/index.js';
 import audio from '../audio/index.js';
-import { arrayToRange, createTossupCard, rangeToArray } from '../utilities/index.js';
+import { arrayToRange, createTossupCard, escapeHTML, rangeToArray } from '../utilities/index.js';
 import CategoryManager from '../utilities/category-manager.js';
 import { attachDropdownChecklist, getDropdownValues } from '../utilities/dropdown-checklist.js';
 
