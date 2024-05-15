6c08407df7cc0084e6542c1de0d058a96ebd0bc0
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-05-15 14:23:24 -0500

    close #251: different sound for power/10 on correct answer

--- client/audio/index.js
+++ client/audio/index.js
@@ -3,4 +3,5 @@ export default class audio {
     static buzz = new Audio('/audio/buzz.mp3');
     static correct = new Audio('/audio/correct.mp3');
     static incorrect = new Audio('/audio/incorrect.mp3');
+    static power = new Audio('/audio/power.mp3');
 }
