7d7e379e6386e0c121b4366b667f293ba38fbf27
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-05-23 23:08:49 -0500

    shuffle around multiplayer constants

--- server/multiplayer/TossupRoom.js
+++ server/multiplayer/TossupRoom.js
@@ -1,8 +1,10 @@
 import Player from './Player.js';
+import { PERMANENT_ROOMS, ROOM_NAME_MAX_LENGTH } from './constants.js';
+
 import RateLimit from '../RateLimit.js';
 
 import { HEADER, ENDC, OKBLUE, OKGREEN } from '../../bcolors.js';
-import { DEFAULT_MIN_YEAR, DEFAULT_MAX_YEAR, PERMANENT_ROOMS, ROOM_NAME_MAX_LENGTH, CATEGORIES, SUBCATEGORIES_FLATTENED, ALTERNATE_SUBCATEGORIES_FLATTENED, SUBCATEGORY_TO_CATEGORY, ALTERNATE_SUBCATEGORY_TO_CATEGORY } from '../../constants.js';
+import { DEFAULT_MIN_YEAR, DEFAULT_MAX_YEAR, CATEGORIES, SUBCATEGORIES_FLATTENED, ALTERNATE_SUBCATEGORIES_FLATTENED, SUBCATEGORY_TO_CATEGORY, ALTERNATE_SUBCATEGORY_TO_CATEGORY } from '../../constants.js';
 import getRandomTossups from '../../database/qbreader/get-random-tossups.js';
 import getSet from '../../database/qbreader/get-set.js';
 import getSetList from '../../database/qbreader/get-set-list.js';
