97404110350450e70cbe07108a359f7875c5d839
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-08-14 12:48:47 -0500

    move backups to root of client/

--- routes/backups.js
+++ routes/backups.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (_req, res) => {
-  res.sendFile('index.html', { root: './client/backups' });
+  res.sendFile('backups.html', { root: './client' });
 });
 
 export default router;
