1d240154defc97ec01a5864b699a449540d76569
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-08-17 13:34:50 -0500

    add s3 export

--- database/databases.js
+++ database/databases.js
@@ -1,30 +1,21 @@
+import * as AWS from '@aws-sdk/client-s3';
 import { MongoClient } from 'mongodb';
 
 const uri = `mongodb+srv://${process.env.MONGODB_USERNAME || 'geoffreywu42'}:${process.env.MONGODB_PASSWORD || 'password'}@qbreader.0i7oej9.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri);
+export const mongoClient = new MongoClient(uri);
 
-async function connectToDatabase (log = false) {
-  await client.connect();
+await mongoClient.connect();
+console.log('connected to mongodb');
 
-  if (log) {
-    console.log('connected to mongodb');
+export const qbreader = mongoClient.db('qbreader');
+export const accountInfo = mongoClient.db('account-info');
+export const geoword = mongoClient.db('geoword');
+
+export const bucketName = process.env.BUCKETEER_BUCKET_NAME;
+export const s3 = new AWS.S3({
+  region: 'us-east-1',
+  credentials: {
+    accessKeyId: process.env.BUCKETEER_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.BUCKETEER_AWS_SECRET_ACCESS_KEY
   }
-}
-
-async function closeDatabase () {
-  await client.close();
-}
-
-await connectToDatabase(true);
-
-const qbreader = client.db('qbreader');
-const accountInfo = client.db('account-info');
-const geoword = client.db('geoword');
-
-export {
-  connectToDatabase,
-  closeDatabase,
-  qbreader,
-  accountInfo,
-  geoword
-};
+});
