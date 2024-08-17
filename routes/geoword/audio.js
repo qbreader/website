300b4ca6abb99be115832a62962902ac23aa82ed
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-08-17 14:50:14 -0500

    catch missing file errors

--- routes/geoword/audio.js
+++ routes/geoword/audio.js
@@ -6,14 +6,22 @@ const router = Router();
 
 router.get('/:packetName/sample.mp3', async (req, res) => {
   const { packetName } = req.params;
-  const audio = await getAudio({ packetName, sample: true });
-  res.send(audio);
+  try {
+    const audio = await getAudio({ packetName, sample: true });
+    res.send(audio);
+  } catch (error) {
+    res.status(404).send('Audio not found');
+  }
 });
 
 router.get('/game/:packetName/:division/:questionNumber.mp3', async (req, res) => {
   const { packetName, division, questionNumber } = req.params;
-  const audio = await getAudio({ packetName, division, questionNumber: parseInt(questionNumber) });
-  res.send(audio);
+  try {
+    const audio = await getAudio({ packetName, division, questionNumber: parseInt(questionNumber) });
+    res.send(audio);
+  } catch (error) {
+    res.status(404).send('Audio not found');
+  }
 });
 
 router.get('/*.mp3', (req, res) => {
