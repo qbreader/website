94f777b09844eb7250819d422e83f407ef5cee37
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-08-17 14:10:44 -0500

    use aws s3 to get files

--- database/geoword/get-audio.js
+++ database/geoword/get-audio.js
@@ -1,4 +1,5 @@
-import { audio, tossups } from './collections.js';
+import { bucketName, s3 } from '../databases.js';
+import * as AWS from '@aws-sdk/client-s3';
 
 /**
  *
@@ -7,18 +8,12 @@ import { audio, tossups } from './collections.js';
  * @param {Integer} questionNumber
  * @returns {Promise<Buffer>}
  */
-async function getAudio (packetName, division, questionNumber) {
-  const tossup = await tossups.findOne({ 'packet.name': packetName, division, questionNumber });
-  if (!tossup) {
-    return null;
-  }
-
-  const audioFile = await audio.findOne({ _id: tossup.audio_id });
-  if (!audioFile) {
-    return null;
-  }
-
-  return audioFile.audio.buffer;
+async function getAudio ({ packetName, division, sample = false, questionNumber }) {
+  const filepath = sample ? `${packetName}/sample.mp3` : `${packetName}/${division}/${questionNumber}.mp3`;
+  const command = new AWS.GetObjectCommand({ Bucket: bucketName, Key: 'geoword/audio/' + filepath });
+  const { Body } = await s3.send(command);
+  const byteArray = await Body.transformToByteArray();
+  return Buffer.from(byteArray);
 }
 
 export default getAudio;
--- routes/geoword/audio.js
+++ routes/geoword/audio.js
@@ -4,9 +4,15 @@ import { Router } from 'express';
 
 const router = Router();
 
+router.get('/:packetName/sample.mp3', async (req, res) => {
+  const { packetName } = req.params;
+  const audio = await getAudio({ packetName, sample: true });
+  res.send(audio);
+});
+
 router.get('/game/:packetName/:division/:questionNumber.mp3', async (req, res) => {
   const { packetName, division, questionNumber } = req.params;
-  const audio = await getAudio(packetName, division, parseInt(questionNumber));
+  const audio = await getAudio({ packetName, division, questionNumber: parseInt(questionNumber) });
   res.send(audio);
 });
 
