f7fb1c0dd809fee09c582e451c6c636c72ba56b0
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-09-18 12:05:19 -0400

    simplify routing

--- routes/admin/leaderboard.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const router = Router();
-
-router.get('/', async (req, res) => {
-  res.sendFile('index.html', { root: './client/admin/leaderboard' });
-});
-
-export default router;
--- routes/database.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-
-router.get('/', (req, res) => {
-  res.sendFile('index.html', { root: './client/database' });
-});
-
-export default router;
