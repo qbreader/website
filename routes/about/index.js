cae5c691648b9bd8d6fe617ded5bfd0633c5775d
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-09-18 02:07:00 -0400

    add privacy policy and terms of service

--- routes/about/index.js
+++ routes/about/index.js
@@ -5,4 +5,12 @@ router.get('/', (req, res) => {
   res.sendFile('index.html', { root: './client/about' });
 });
 
+router.get('/privacy-policy', (req, res) => {
+  res.sendFile('privacy-policy.html', { root: './client/about' });
+});
+
+router.get('/terms-of-service', (req, res) => {
+  res.sendFile('terms-of-service.html', { root: './client/about' });
+});
+
 export default router;
