5268ea0a05793a5646d636b529581b17c4ad8fd5
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-10-12 16:20:12 -0400

    fix redirect loop

--- server/https-enforcement.js
+++ server/https-enforcement.js
@@ -2,7 +2,7 @@ export default function httpsEnforcement (req, res, next) {
   const hostname = req.hostname;
 
   // Use HTTPS if not on localhost
-  if (req.protocol !== 'https' && !['localhost', '127.0.0.1'].includes(hostname)) {
+  if (!req.secure && !['localhost', '127.0.0.1'].includes(hostname)) {
     return res.redirect(301, `https://${hostname}${req.originalUrl}`);
   }
 
