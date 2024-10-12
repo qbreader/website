811a375e3bac9e967b9699b435511790e29d5294
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-10-12 15:25:49 -0400

    move redirection to server-side

---
+++ server/https-enforcement.js
@@ -0,0 +1,10 @@
+export default function httpsEnforcement (req, res, next) {
+  const hostname = req.hostname;
+
+  // Use HTTPS if not on localhost
+  if (req.protocol !== 'https' && !['localhost', '127.0.0.1'].includes(hostname)) {
+    return res.redirect(301, `https://${hostname}${req.originalUrl}`);
+  }
+
+  next();
+}
