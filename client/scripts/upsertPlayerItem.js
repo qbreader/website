44d5422e0bcc15fd0c98ed585ee930a5318e1102
Your Name <you@example.com>
2024-11-04 18:08:36 -0600

    linted according to semistandard

--- client/scripts/upsertPlayerItem.js
+++ client/scripts/upsertPlayerItem.js
@@ -6,51 +6,49 @@ import { escapeHTML } from './utilities/strings.js';
  * @param {string} USER_ID - The item is highlighted blue if `USER_ID === player.userId`.
  * @param {string} ownerId - ID of the room owner
  */
-export default function upsertPlayerItem(player, USER_ID, ownerId, socket) {
-    if (!player || !player.userId) {
-        console.error("Player or player.userId is undefined", { player });
-        return;
-    }
+export default function upsertPlayerItem (player, USER_ID, ownerId, socket) {
+  if (!player || !player.userId) {
+    console.error('Player or player.userId is undefined', { player });
+    return;
+  }
 
-    const { userId, username, powers = 0, tens = 0, negs = 0, tuh = 0, points = 0, online } = player;
-    const celerity = player?.celerity?.correct?.average ?? player?.celerity ?? 0;
-    
-    const playerIsOwner = ownerId === userId;
-    const iAmOwner = ownerId === USER_ID;
-    const res = playerIsOwner ? "Yes" : "No";
+  const { userId, username, powers = 0, tens = 0, negs = 0, tuh = 0, points = 0, online } = player;
+  const celerity = player?.celerity?.correct?.average ?? player?.celerity ?? 0;
 
-    // Remove the existing player item if it exists
-    if (document.getElementById('list-group-' + userId)) {
-        document.getElementById('list-group-' + userId).remove();
-    }
+  const playerIsOwner = ownerId === userId;
+  const iAmOwner = ownerId === USER_ID;
+  const res = playerIsOwner ? 'Yes' : 'No';
 
-    
-    const playerItem = document.createElement('a');
-    playerItem.className = `list-group-item ${userId === USER_ID ? 'user-score' : ''} clickable`;
-    playerItem.id = `list-group-${userId}`;
-    
-    
-    const displayUsername = playerIsOwner ? `👑 ${escapeHTML(username)}` : escapeHTML(username);
+  // Remove the existing player item if it exists
+  if (document.getElementById('list-group-' + userId)) {
+    document.getElementById('list-group-' + userId).remove();
+  }
 
-    playerItem.innerHTML = `
+  const playerItem = document.createElement('a');
+  playerItem.className = `list-group-item ${userId === USER_ID ? 'user-score' : ''} clickable`;
+  playerItem.id = `list-group-${userId}`;
+
+  const displayUsername = playerIsOwner ? `👑 ${escapeHTML(username)}` : escapeHTML(username);
+
+  playerItem.innerHTML = `
       <div class="d-flex justify-content-between">
           <span id="username-${userId}">${displayUsername}</span>
           <span><span id="points-${userId}" class="badge rounded-pill ${online ? 'bg-success' : 'bg-secondary'}">${points}</span></span>
       </div>
     `;
 
-    // Set attributes for the popover
-    playerItem.setAttribute('data-bs-container', 'body');
-    playerItem.setAttribute('data-bs-custom-class', 'custom-popover');
-    playerItem.setAttribute('data-bs-html', 'true');
-    playerItem.setAttribute('data-bs-placement', 'left');
-    playerItem.setAttribute('data-bs-toggle', 'popover');
-    playerItem.setAttribute('data-bs-trigger', 'focus');
-    playerItem.setAttribute('tabindex', '0');
+  // Set attributes for the popover
+  playerItem.setAttribute('data-bs-container', 'body');
+  playerItem.setAttribute('data-bs-custom-class', 'custom-popover');
+  playerItem.setAttribute('data-bs-html', 'true');
+  playerItem.setAttribute('data-bs-placement', 'left');
+  playerItem.setAttribute('data-bs-toggle', 'popover');
+  playerItem.setAttribute('data-bs-trigger', 'focus');
+  playerItem.setAttribute('tabindex', '0');
 
-    // Popover content
-    playerItem.setAttribute('data-bs-title', username);
-    playerItem.setAttribute('data-bs-content', `
+  // Popover content
+  playerItem.setAttribute('data-bs-title', username);
+  playerItem.setAttribute('data-bs-content', `
         <ul class="list-group list-group-flush">
             <li class="list-group-item"><span>Powers</span><span id="powers-${userId}" class="float-end badge rounded-pill bg-secondary stats-${userId}">${powers}</span></li>
             <li class="list-group-item"><span>Tens</span><span id="tens-${userId}" class="float-end badge rounded-pill bg-secondary stats-${userId}">${tens}</span></li>
@@ -61,24 +59,22 @@ export default function upsertPlayerItem(player, USER_ID, ownerId, socket) {
         </ul>
     `);
 
-    
-    document.getElementById('player-list-group').appendChild(playerItem);
+  document.getElementById('player-list-group').appendChild(playerItem);
 
-    //ban button if the viewer is the owner and the player is not
-    if (iAmOwner && userId !== ownerId) {
-        const banButton = document.createElement('button');
-        banButton.className = 'btn btn-danger btn-sm mt-2';
-        banButton.innerText = 'Ban';
-        
-        
-        playerItem.appendChild(banButton);
+  // ban button if the viewer is the owner and the player is not
+  if (iAmOwner && userId !== ownerId) {
+    const banButton = document.createElement('button');
+    banButton.className = 'btn btn-danger btn-sm mt-2';
+    banButton.innerText = 'Ban';
 
-        
-        banButton.addEventListener('click', () => {
-            socket.send(JSON.stringify({ type: 'ban', ownerId: ownerId, target_user: userId, targ_name: username }));
-        });
-    }
+    playerItem.appendChild(banButton);
 
-    // popover
-    new bootstrap.Popover(playerItem);
+    banButton.addEventListener('click', () => {
+      socket.send(JSON.stringify({ type: 'ban', ownerId, target_user: userId, targ_name: username }));
+    });
+  }
+
+  // bootstrap requires "new" to be called on each popover
+  // eslint-disable-next-line no-new
+  new bootstrap.Popover(playerItem);
 }
