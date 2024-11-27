2b5d44fb6fcb5edd79817be07b1eb3a765fbd69a
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-11-27 15:14:03 -0600

    fix #258: view frequency lists by `alternate_subcategory`

--- database/qbreader/get-frequency-list.js
+++ database/qbreader/get-frequency-list.js
@@ -1,17 +1,24 @@
 import { bonuses, tossups } from './collections.js';
-import { DIFFICULTIES } from '../../quizbowl/constants.js';
 import mergeTwoSortedArrays from '../../server/merge-two-sorted-arrays.js';
 
 /**
- * Get a frequency list of answers for a given subcategory and difficulty.
- * @param {string} subcategory
- * @param {number[]} [difficulties] An array of difficulties to include. Defaults to all difficulties.
- * @param {number} [limit=50] The maximum number of answers to return. Defaults to 50.
+ * Get a frequency list of answers for a given subcategory, alternateSubcategory, and difficulty.
+ * If both a `subcategory` and `alternateSubcategory` are provided, the frequency list will filter over questions that match both fields.
+ * @param {object} params
+ * @param {string} params.subcategory The subcategory to get the frequency list for.
+ * @param {string} params.alternateSubcategory The alternate subcategory to get the frequency list for.
+ * @param {number[]} [params.difficulties] An array of difficulties to include.
+ * @param {number} [params.limit=50] The maximum number of answers to return.
+ * @param {'tossup' | 'bonus' | 'all'} [params.questionType] The type of question to include.
  * @returns {Promise<{ answer: string, count: number }[]>} The frequency list.
  */
-async function getFrequencyList (subcategory, difficulties = DIFFICULTIES, limit = 50, questionType = 'all') {
+export default async function getFrequencyList ({ subcategory, alternateSubcategory, difficulties, limit, questionType }) {
+  const matchDocument = { difficulty: { $in: difficulties } };
+  if (subcategory) { matchDocument.subcategory = subcategory; }
+  if (alternateSubcategory) { matchDocument.alternate_subcategory = alternateSubcategory; }
+
   const tossupAggregation = [
-    { $match: { subcategory, difficulty: { $in: difficulties } } },
+    { $match: matchDocument },
     {
       $addFields: {
         // This is a regex that matches everything before the first open parenthesis or bracket.
@@ -24,15 +31,11 @@ async function getFrequencyList (subcategory, difficulties = DIFFICULTIES, limit
         regex: { $regexFind: { input: '$regex.match', regex: /[^()]+(?![^(]*\))/ } }
       }
     },
-    {
-      $addFields: {
-        sanitized_answer: { $trim: { input: '$regex.match' } }
-      }
-    },
-    { $group: { _id: '$sanitized_answer', count: { $sum: 1 } } },
+    { $addFields: { answer_normalized: { $trim: { input: '$regex.match' } } } },
+    { $group: { _id: '$answer_normalized', count: { $sum: 1 } } },
     { $match: { _id: { $ne: null } } },
-    { $addFields: { answer_sanitized: '$_id' } },
-    { $sort: { answer_sanitized: 1 } }
+    { $addFields: { answer: '$_id' } },
+    { $sort: { answer: 1 } }
   ];
 
   const bonusAggregation = [
@@ -44,17 +47,13 @@ async function getFrequencyList (subcategory, difficulties = DIFFICULTIES, limit
     case 'tossup': {
       const tossupList = await tossups.aggregate(tossupAggregation).toArray();
       tossupList.sort((a, b) => b.count - a.count);
-      if (limit) {
-        tossupList.length = limit;
-      }
+      if (limit) { tossupList.length = Math.min(limit, tossupList.length); }
       return tossupList;
     }
     case 'bonus': {
       const bonusList = await bonuses.aggregate(bonusAggregation).toArray();
       bonusList.sort((a, b) => b.count - a.count);
-      if (limit) {
-        bonusList.length = limit;
-      }
+      if (limit) { bonusList.length = Math.min(limit, bonusList.length); }
       return bonusList;
     }
     case 'all':
@@ -71,17 +70,15 @@ async function getFrequencyList (subcategory, difficulties = DIFFICULTIES, limit
   const frequencyList = mergeTwoSortedArrays(
     tossupList,
     bonusList,
-    (a) => a.answer_sanitized,
-    (a, b) => ({ answer_sanitized: a.answer_sanitized, count: a.count + b.count })
+    (a) => a.answer,
+    (a, b) => ({ answer: a.answer, count: a.count + b.count })
   );
 
   frequencyList.sort((a, b) => b.count - a.count);
 
   if (limit) {
-    frequencyList.length = limit;
+    frequencyList.length = Math.min(limit, frequencyList.length);
   }
 
   return frequencyList;
 }
-
-export default getFrequencyList;
