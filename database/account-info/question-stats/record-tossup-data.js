f0825cc7feb3c2635a21013ae1bc9a3b9ac97cda
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-12-14 16:10:07 -0500

    validate parameters when recording tossup/bonus stats

--- database/account-info/question-stats/record-tossup-data.js
+++ database/account-info/question-stats/record-tossup-data.js
@@ -3,35 +3,26 @@ import getUserId from '../get-user-id.js';
 
 import { ObjectId } from 'mongodb';
 
-async function recordTossupData (username, data) {
+async function recordTossupData (username, { tossup, celerity, isCorrect, pointValue, multiplayer }) {
   const userId = await getUserId(username);
-  const { tossup } = data;
-  const newData = {};
-  for (const field of ['celerity', 'isCorrect', 'pointValue', 'multiplayer']) {
-    if (Object.prototype.hasOwnProperty.call(data, field)) {
-      newData[field] = data[field];
-    } else {
-      return false;
-    }
-  }
-
-  if (!Object.prototype.hasOwnProperty.call(data, 'tossup')) {
-    return false;
-  }
-
-  for (const field of ['category', 'subcategory', 'alternate_subcategory', 'difficulty']) {
-    if (Object.prototype.hasOwnProperty.call(tossup, field)) {
-      newData[field] = tossup[field];
-    }
-  }
+  const newData = {
+    user_id: userId,
+    celerity,
+    isCorrect,
+    pointValue,
+    multiplayer,
+    category: tossup.category,
+    subcategory: tossup.subcategory,
+    alternate_subcategory: tossup.alternate_subcategory,
+    difficulty: tossup.difficulty
+  };
 
   try {
     newData.tossup_id = new ObjectId(tossup._id);
     newData.set_id = new ObjectId(tossup.set._id);
   } catch (e) {
-    return false;
+    return null;
   }
-  newData.user_id = userId;
   return await tossupData.insertOne(newData);
 }
 
--- routes/auth/question-stats/record-bonus.js
+++ routes/auth/question-stats/record-bonus.js
@@ -5,6 +5,35 @@ import { Router } from 'express';
 
 const router = Router();
 
+/**
+ * Validates the parameters for recording the stats of a played bonus.
+ *
+ * @param {object} params - The parameters to validate.
+ * @param {object} params.tossup - The bonus object.
+ * @param {number[]} params.pointsPerPart - The points per part of the bonus.
+ * @returns {{
+ *  tossup: object,
+ *  pointsPerPart: number[]
+ * } | null} The validated parameters or null if validation fails.
+ */
+function validateParams ({ bonus, pointsPerPart }) {
+  if (bonus?.constructor !== Object) { return null; }
+
+  for (const key of ['category', 'subcategory', '_id']) {
+    if (typeof bonus[key] !== 'string') {
+      return null;
+    }
+  }
+  if (isNaN(parseInt(bonus.difficulty)) || bonus.difficulty < 0 || bonus.difficulty > 10) { return null; }
+  if (bonus.alternate_subcategory !== undefined && typeof bonus.alternate_subcategory !== 'string') { return null; }
+  if (typeof bonus?.set?._id !== 'string') { return null; }
+
+  if (!Array.isArray(pointsPerPart)) { return null; }
+  if (pointsPerPart.some(points => typeof points !== 'number')) { return null; }
+
+  return { bonus, pointsPerPart };
+}
+
 router.post('/', async (req, res) => {
   const { username, token } = req.session;
   if (!checkToken(username, token)) {
@@ -18,7 +47,11 @@ router.post('/', async (req, res) => {
     return;
   }
 
-  const results = await recordBonusData(username, req.body);
+  const params = validateParams(req.body);
+  if (params === null) {
+    return res.status(400).send('Invalid parameters');
+  }
+  const results = await recordBonusData(username, params);
   res.json(results);
 });
 
