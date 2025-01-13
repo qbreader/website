599421056823e3f82d0c4d447d22bba0b87c6a94
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-01-13 14:32:04 -0600

    fix #330: difficulty and year range not appearing

--- client/scripts/dual-slider.js
+++ client/scripts/dual-slider.js
@@ -9,27 +9,25 @@ document.body.onmouseup = function () {
   slidersToUpdate.length = 0;
 };
 
-$(document).ready(function () {
-  $('#slider').slider({
-    min: 2000,
-    max: 2024,
-    step: 1,
-    values: [2010, 2024],
-    slide: function (event, ui) {
-      for (let i = 0; i < ui.values.length; ++i) {
-        $(`span.sliderValue${i}`)[0].textContent = ui.values[i];
-        if (!slidersToUpdate.includes($(`span.sliderValue${i}`)[0])) {
-          slidersToUpdate.push($(`span.sliderValue${i}`)[0]);
-        }
+$('#slider').slider({
+  min: 2000,
+  max: 2024,
+  step: 1,
+  values: [2010, 2024],
+  slide: function (event, ui) {
+    for (let i = 0; i < ui.values.length; ++i) {
+      $(`span.sliderValue${i}`)[0].textContent = ui.values[i];
+      if (!slidersToUpdate.includes($(`span.sliderValue${i}`)[0])) {
+        slidersToUpdate.push($(`span.sliderValue${i}`)[0]);
       }
     }
-  });
+  }
+});
 
-  document.getElementById('slider').classList.remove('ui-widget-content');
-  document.getElementById('slider').classList.remove('ui-widget');
+document.getElementById('slider').classList.remove('ui-widget-content');
+document.getElementById('slider').classList.remove('ui-widget');
 
-  $('input.sliderValue').change(function () {
-    const $this = $(this);
-    $('#slider').slider('values', $this.data('index'), $this.val());
-  });
+$('input.sliderValue').change(function () {
+  const $this = $(this);
+  $('#slider').slider('values', $this.data('index'), $this.val());
 });
