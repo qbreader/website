d409d88371528bd02235dadba23be20b560b1783
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-03-29 19:51:38 -0400

    update record-tossup

--- routes/auth/question-stats/record-tossup.js
+++ routes/auth/question-stats/record-tossup.js
@@ -1,7 +1,7 @@
 import recordTossupData from '../../../database/account-info/question-stats/record-tossup-data.js';
-import { checkToken } from '../../../server/authentication.js';
 
 import { Router } from 'express';
+import { ObjectId } from 'mongodb';
 
 const router = Router();
 
@@ -9,30 +9,26 @@ const router = Router();
  * Validates the parameters for recording the stats of a played tossup.
  *
  * @param {object} params - The parameters to validate.
- * @param {object} params.tossup - The tossup object.
+ * @param {ObjectId | string} params._id - The _id of the tossup.
  * @param {string | number} params.celerity - The celerity value, should be a number between 0 and 1.
  * @param {string | boolean} params.isCorrect - Indicates if the answer is correct, should be 'true' or 'false'.
  * @param {string | number} params.pointValue - The point value of the tossup.
  * @param {string | boolean} params.multiplayer - Indicates if the game is multiplayer, should be 'true' or 'false'.
  * @returns {{
- *  tossup: object,
+ *  _id: ObjectId,
  *  celerity: number,
  *  isCorrect: boolean,
  *  pointValue: number,
  *  multiplayer: boolean
  * } | null} The validated parameters or null if validation fails.
  */
-function validateParams ({ tossup, celerity, isCorrect, pointValue, multiplayer }) {
-  if (tossup?.constructor !== Object) { return null; }
-
-  for (const key of ['category', 'subcategory', '_id']) {
-    if (typeof tossup[key] !== 'string') {
-      return null;
-    }
+function validateParams ({ _id, celerity, isCorrect, multiplayer, pointValue }) {
+  if (!_id) { return null; }
+  try {
+    _id = new ObjectId(_id);
+  } catch (e) {
+    return null;
   }
-  if (isNaN(parseInt(tossup.difficulty)) || tossup.difficulty < 0 || tossup.difficulty > 10) { return null; }
-  if (tossup.alternate_subcategory !== undefined && typeof tossup.alternate_subcategory !== 'string') { return null; }
-  if (typeof tossup?.set?._id !== 'string') { return null; }
 
   if (isNaN(parseFloat(celerity)) || celerity < 0 || celerity > 1) { return null; }
 
@@ -44,22 +40,11 @@ function validateParams ({ tossup, celerity, isCorrect, pointValue, multiplayer 
   if (multiplayer === 'true') { multiplayer = true; } else if (multiplayer === 'false') { multiplayer = false; }
   if (typeof multiplayer !== 'boolean') { return null; }
 
-  return { tossup, celerity, isCorrect, pointValue, multiplayer };
+  return { _id, celerity, isCorrect, pointValue, multiplayer };
 }
 
 router.post('/', async (req, res) => {
-  const { username, token } = req.session;
-  if (!checkToken(username, token)) {
-    delete req.session;
-    res.sendStatus(401);
-    return;
-  }
-
-  if (!checkToken(username, token, true)) {
-    res.sendStatus(403);
-    return;
-  }
-
+  const { username } = req.session;
   const params = validateParams(req.body);
   if (params === null) {
     return res.status(400).send('Invalid parameters');
