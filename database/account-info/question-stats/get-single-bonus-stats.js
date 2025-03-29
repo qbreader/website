f56a38c76e9a9ff36e4a59a0da0a8231b8f62ff2
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-03-29 01:56:04 -0400

    update get single question stats functions

--- database/account-info/question-stats/get-single-bonus-stats.js
+++ database/account-info/question-stats/get-single-bonus-stats.js
@@ -1,51 +1,27 @@
 import { perBonusData } from '../collections.js';
 
-import getBonus from '../../qbreader/get-bonus.js';
-
 /**
  * Get the stats for a single bonus.
  * @param {ObjectId} bonusId the bonus id
  * @returns {Promise<Document>} the bonus stats
  */
 async function getSingleBonusStats (bonusId) {
-  const bonus = await getBonus(bonusId);
-
-  if (!bonus) {
-    return null;
-  }
-
-  const result = await perBonusData.aggregate([
-    { $match: { bonus_id: bonusId, pointsPerPart: { $size: bonus.parts.length } } },
-    { $addFields: { pointValue: { $sum: '$pointsPerPart' } } },
-    {
-      $addFields: {
-        convertedPart1: { $ne: [{ $arrayElemAt: ['$pointsPerPart', 0] }, 0] },
-        convertedPart2: { $ne: [{ $arrayElemAt: ['$pointsPerPart', 1] }, 0] },
-        convertedPart3: { $ne: [{ $arrayElemAt: ['$pointsPerPart', 2] }, 0] },
-        is30: { $eq: ['$pointValue', 30] },
-        is20: { $eq: ['$pointValue', 20] },
-        is10: { $eq: ['$pointValue', 10] },
-        is0: { $eq: ['$pointValue', 0] }
-      }
-    },
-    {
-      $group: {
-        _id: bonusId,
-        count: { $sum: 1 },
-        '30s': { $sum: { $cond: ['$is30', 1, 0] } },
-        '20s': { $sum: { $cond: ['$is20', 1, 0] } },
-        '10s': { $sum: { $cond: ['$is10', 1, 0] } },
-        '0s': { $sum: { $cond: ['$is0', 1, 0] } },
-        part1: { $avg: { $cond: ['$convertedPart1', 1, 0] } },
-        part2: { $avg: { $cond: ['$convertedPart2', 1, 0] } },
-        part3: { $avg: { $cond: ['$convertedPart3', 1, 0] } },
-        totalPoints: { $sum: '$pointValue' },
-        ppb: { $avg: '$pointValue' }
-      }
-    }
-  ]).toArray();
-
-  return result[0];
+  const document = await perBonusData.findOne({ _id: bonusId });
+  if (!document) { return null; }
+  document.data.forEach(d => { d.pointValue = d.pointsPerPart.reduce((a, b) => a + b, 0); });
+  return {
+    _id: bonusId,
+    '30s': document.data.filter(d => d.pointValue === 30).length,
+    '20s': document.data.filter(d => d.pointValue === 20).length,
+    '10s': document.data.filter(d => d.pointValue === 10).length,
+    '0s': document.data.filter(d => d.pointValue === 0).length,
+    count: document.data.length,
+    part1: document.data.reduce((a, b) => a + (b.pointsPerPart[0] > 0 ? 1 : 0), 0) / document.data.length,
+    part2: document.data.reduce((a, b) => a + (b.pointsPerPart[1] > 0 ? 1 : 0), 0) / document.data.length,
+    part3: document.data.reduce((a, b) => a + (b.pointsPerPart[2] > 0 ? 1 : 0), 0) / document.data.length,
+    ppb: document.data.reduce((a, b) => a + b.pointValue, 0) / document.data.length,
+    totalPoints: document.data.reduce((a, b) => a + b.pointValue, 0)
+  };
 }
 
 export default getSingleBonusStats;
