ac4ea47f0568c686d3878314746f6f198e49754f
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-03-29 21:24:29 -0400

    update user-stats functions

--- database/account-info/user-stats/get-category-stats.js
+++
@@ -1,9 +0,0 @@
-import getStatsHelper from './get-stats-helper.js';
-import getUserId from '../get-user-id.js';
-
-async function getCategoryStats ({ username, questionType, difficulties, setName, includeMultiplayer, includeSingleplayer, startDate, endDate }) {
-  const userId = await getUserId(username);
-  return await getStatsHelper({ userId, questionType, groupByField: 'category', difficulties, setName, includeMultiplayer, includeSingleplayer, startDate, endDate });
-}
-
-export default getCategoryStats;
--- routes/auth/user-stats/tossup.js
+++ routes/auth/user-stats/tossup.js
@@ -1,7 +1,6 @@
-import getAlternateSubcategoryStats from '../../../database/account-info/user-stats/get-alternate-subcategory-stats.js';
+import getUserId from '../../../database/account-info/get-user-id.js';
 import getBestBuzz from '../../../database/account-info/user-stats/get-best-buzz.js';
-import getCategoryStats from '../../../database/account-info/user-stats/get-category-stats.js';
-import getSubcategoryStats from '../../../database/account-info/user-stats/get-subcategory-stats.js';
+import getSummaryTossupStats from '../../../database/account-info/user-stats/get-summary-tossup-stats.js';
 
 import { Router } from 'express';
 
@@ -9,13 +8,15 @@ const router = Router();
 
 router.get('/', async (req, res) => {
   const { username } = req.session;
+  const userId = await getUserId(username);
   const { difficulties, setName, includeMultiplayer, includeSingleplayer, startDate, endDate } = req.query;
+  const query = { difficulties, setName, includeMultiplayer, includeSingleplayer, startDate, endDate };
 
   const [bestBuzz, categoryStats, subcategoryStats, alternateSubcategoryStats] = await Promise.all([
-    await getBestBuzz({ username, difficulties, setName, includeMultiplayer, includeSingleplayer, startDate, endDate }),
-    await getCategoryStats({ username, questionType: 'tossup', difficulties, setName, includeMultiplayer, includeSingleplayer, startDate, endDate }),
-    await getSubcategoryStats({ username, questionType: 'tossup', difficulties, setName, includeMultiplayer, includeSingleplayer, startDate, endDate }),
-    await getAlternateSubcategoryStats({ username, questionType: 'tossup', difficulties, setName, includeMultiplayer, includeSingleplayer, startDate, endDate })
+    await getBestBuzz(userId, query),
+    await getSummaryTossupStats(userId, 'category', query),
+    await getSummaryTossupStats(userId, 'subcategory', query),
+    await getSummaryTossupStats(userId, 'alternate_subcategory', query)
   ]);
 
   res.json({
