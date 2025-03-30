687e977fb057ffb4c64f0813a21a9efec9ab1463
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-03-29 21:24:29 -0400

    update user-stats functions

--- database/account-info/user-stats/get-best-buzz.js
+++ database/account-info/user-stats/get-best-buzz.js
@@ -1,37 +1,38 @@
 import { perTossupData } from '../collections.js';
 import generateMatchDocument from './generate-match-document.js';
-import getUserId from '../get-user-id.js';
 
 import getTossup from '../../qbreader/get-tossup.js';
 
 /**
  * Generates a match document for a given set of match options.
- * @param {Object} options - The options for the match generation.
- * @param {string} options.user_id - The ID of the user generating the match.
- * @param {Array<Number>} options.difficulties - An array of difficulties to filter by.
- * @param {string} options.setName - The name of the set to search in.
- * @param {boolean} options.includeMultiplayer - Whether to include multiplayer questions.
- * @param {boolean} options.includeSingleplayer - Whether to include singleplayer questions.
- * @param {Date} [options.startDate] - The start date of the match.
- * @param {Date} [options.endDate] - The end date of the match.
+ * @param {ObjectId} userId - The _id of the user generating the match.
+ * @param {Object} query - The options for the match generation.
+ * @param {Array<Number>} query.difficulties - An array of difficulties to filter by.
+ * @param {string} query.setName - The name of the set to search in.
+ * @param {boolean} query.includeMultiplayer - Whether to include multiplayer questions.
+ * @param {boolean} query.includeSingleplayer - Whether to include singleplayer questions.
+ * @param {Date} [query.startDate] - The start date of the match.
+ * @param {Date} [query.endDate] - The end date of the match.
  * @returns {Promise<Object>} The generated match document.
  */
-async function getBestBuzz ({ username, difficulties, setName, includeMultiplayer, includeSingleplayer, startDate, endDate }) {
-  const userId = await getUserId(username);
-  const matchDocument = await generateMatchDocument({ userId, difficulties, setName, includeMultiplayer, includeSingleplayer, startDate, endDate });
-  matchDocument.isCorrect = true;
+export default async function getBestBuzz (userId, query) {
+  const matchDocument = await generateMatchDocument({ userId, ...query });
+  matchDocument['data.isCorrect'] = true;
 
   const data = (await perTossupData.aggregate([
-    { $addFields: { createdAt: { $toDate: '$_id' } } },
     { $match: matchDocument },
-    { $sort: { celerity: -1 } },
+    { $unwind: '$data' },
+    { $match: { 'data.user_id': userId } },
+    { $match: { 'data.user_id': { $ne: null } } },
+    { $sort: { 'data.celerity': -1 } },
     { $limit: 1 }
   ]).toArray())[0];
 
   if (!data) { return null; }
 
-  data.tossup = await getTossup(data.tossup_id);
-  return data;
+  data.tossup = await getTossup(data._id);
+  return {
+    ...data.data,
+    ...data
+  };
 }
-
-export default getBestBuzz;
---
+++ database/account-info/user-stats/get-summary-bonus-stats.js
@@ -0,0 +1,47 @@
+import { perBonusData } from '../collections.js';
+import generateMatchDocument from './generate-match-document.js';
+
+/**
+ * @param {ObjectId} userId
+ * @param {'category' | 'subcategory' | 'alternate_subcategory'} groupByField
+ * @param {Object} query
+ * @param {Number[]} query.difficulties
+ * @param {String} query.setName
+ * @param {Boolean} query.includeMultiplayer
+ * @param {Boolean} query.includeSingleplayer
+ * @param {Date} query.startDate
+ * @param {Date} query.endDate
+ * @returns
+ */
+export default async function getSummaryBonusStats (userId, groupByField, query) {
+  const matchDocument = await generateMatchDocument({ userId, ...query });
+  return await perBonusData.aggregate([
+    { $match: matchDocument },
+    { $match: { [groupByField]: { $exists: true } } },
+    { $unwind: '$data' },
+    { $match: { 'data.user_id': userId } },
+    { $match: { 'data.user_id': { $ne: null } } },
+    { $addFields: { pointValue: { $sum: '$data.pointsPerPart' } } },
+    {
+      $addFields: {
+        is30: { $eq: ['$pointValue', 30] },
+        is20: { $eq: ['$pointValue', 20] },
+        is10: { $eq: ['$pointValue', 10] },
+        is0: { $eq: ['$pointValue', 0] }
+      }
+    },
+    {
+      $group: {
+        _id: '$' + groupByField,
+        count: { $sum: 1 },
+        '30s': { $sum: { $cond: ['$is30', 1, 0] } },
+        '20s': { $sum: { $cond: ['$is20', 1, 0] } },
+        '10s': { $sum: { $cond: ['$is10', 1, 0] } },
+        '0s': { $sum: { $cond: ['$is0', 1, 0] } },
+        totalPoints: { $sum: '$pointValue' },
+        ppb: { $avg: '$pointValue' }
+      }
+    },
+    { $sort: { ppb: -1, totalPoints: -1 } }
+  ]).toArray();
+}
