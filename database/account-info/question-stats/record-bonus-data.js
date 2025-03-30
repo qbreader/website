9d21fb6b3b769120730f61d50f1f23450f3875dc
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-03-29 21:39:15 -0400

    fix record data functions

--- database/account-info/question-stats/record-bonus-data.js
+++ database/account-info/question-stats/record-bonus-data.js
@@ -1,12 +1,8 @@
 import { perBonusData } from '../collections.js';
-import getUserId from '../get-user-id.js';
 
-async function recordBonusData (username, { _id, pointsPerPart }) {
-  const userId = await getUserId(username);
+export default async function recordBonusData (userId, { _id, pointsPerPart }) {
   return await perBonusData.updateOne(
     { _id },
-    { $push: { data: { user_id: userId, pointsPerPart } } }
+    { $push: { data: { user_id: userId, created: new Date(), pointsPerPart } } }
   );
 }
-
-export default recordBonusData;
--- database/account-info/question-stats/record-tossup-data.js
+++ database/account-info/question-stats/record-tossup-data.js
@@ -1,12 +1,8 @@
 import { perTossupData } from '../collections.js';
-import getUserId from '../get-user-id.js';
 
-async function recordTossupData (username, { _id, celerity, isCorrect, multiplayer, pointValue }) {
-  const userId = await getUserId(username);
+export default async function recordTossupData (userId, { _id, celerity, isCorrect, multiplayer, pointValue }) {
   return await perTossupData.updateOne(
     { _id },
-    { $push: { data: { user_id: userId, celerity, isCorrect, multiplayer, pointValue } } }
+    { $push: { data: { user_id: userId, created: new Date(), celerity, isCorrect, multiplayer, pointValue } } }
   );
 }
-
-export default recordTossupData;
