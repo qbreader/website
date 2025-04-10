bfe5e2a8fc02967d4e38b101cb472d0f63ff9cca
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-04-09 23:11:52 -0400

    fix issue where database stats don't update if empty

--- database/account-info/question-stats/get-single-bonus-stats.js
+++ database/account-info/question-stats/get-single-bonus-stats.js
@@ -8,19 +8,22 @@ import { perBonusData } from '../collections.js';
 async function getSingleBonusStats (bonusId) {
   const document = await perBonusData.findOne({ _id: bonusId });
   if (!document) { return null; }
-  document.data.forEach(d => { d.pointValue = d.pointsPerPart.reduce((a, b) => a + b, 0); });
+  const data = document.data;
+  // data should always be an array
+  if (data.length === 0) { return null; }
+  data.forEach(d => { d.pointValue = d.pointsPerPart.reduce((a, b) => a + b, 0); });
   return {
     _id: bonusId,
-    '30s': document.data.filter(d => d.pointValue === 30).length,
-    '20s': document.data.filter(d => d.pointValue === 20).length,
-    '10s': document.data.filter(d => d.pointValue === 10).length,
-    '0s': document.data.filter(d => d.pointValue === 0).length,
-    count: document.data.length,
-    part1: document.data.reduce((a, b) => a + (b.pointsPerPart[0] > 0 ? 1 : 0), 0) / document.data.length,
-    part2: document.data.reduce((a, b) => a + (b.pointsPerPart[1] > 0 ? 1 : 0), 0) / document.data.length,
-    part3: document.data.reduce((a, b) => a + (b.pointsPerPart[2] > 0 ? 1 : 0), 0) / document.data.length,
-    ppb: document.data.reduce((a, b) => a + b.pointValue, 0) / document.data.length,
-    totalPoints: document.data.reduce((a, b) => a + b.pointValue, 0)
+    '30s': data.filter(d => d.pointValue === 30).length,
+    '20s': data.filter(d => d.pointValue === 20).length,
+    '10s': data.filter(d => d.pointValue === 10).length,
+    '0s': data.filter(d => d.pointValue === 0).length,
+    count: data.length,
+    part1: data.reduce((a, b) => a + (b.pointsPerPart[0] > 0 ? 1 : 0), 0) / data.length,
+    part2: data.reduce((a, b) => a + (b.pointsPerPart[1] > 0 ? 1 : 0), 0) / data.length,
+    part3: data.reduce((a, b) => a + (b.pointsPerPart[2] > 0 ? 1 : 0), 0) / data.length,
+    ppb: data.reduce((a, b) => a + b.pointValue, 0) / data.length,
+    totalPoints: data.reduce((a, b) => a + b.pointValue, 0)
   };
 }
 
