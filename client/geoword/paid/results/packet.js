cf97b9249614e6703a21958b4fa35c7200869f1f
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-04-12 19:44:07 -0400

    fix #291: use bootstrap navs to organize geoword

--- client/geoword/paid/results/packet.js
+++ client/geoword/paid/results/packet.js
@@ -1,30 +1,29 @@
+import createTabs from '../../../scripts/utilities/create-tabs.js';
 import { titleCase } from '../../../scripts/utilities/strings.js';
 
 const search = new URLSearchParams(window.location.search);
-const division = search.get('division');
 const packetName = search.get('packetName');
 const packetTitle = titleCase(packetName);
 
 document.getElementById('packet-name').textContent = packetTitle;
 
-fetch('/api/geoword/paid/results/packet?' + new URLSearchParams({ packetName, division }))
-  .then(response => {
-    if (response.status === 403) {
-      document.getElementById('packet').textContent = 'You do not have permission to view this packet. Either you have not paid for it, or you have not finished playing it. Please contact the administrator if you believe this is an error.';
-    } else {
-      return response.json();
-    }
-  })
+fetch('/api/geoword/paid/results/packet?' + new URLSearchParams({ packetName }))
+  .then(response => response.json())
   .then(data => {
-    const { packet } = data;
-    let innerHTML = '';
+    const { packets } = data;
 
-    for (const tossup of packet) {
-      innerHTML += `<div>${tossup.questionNumber}. ${tossup.question}</div>`;
-      innerHTML += `<div>ANSWER: ${tossup.answer}</div>`;
-      innerHTML += `<p>&lt;${tossup.category} / ${tossup.subcategory}&gt;</p>`;
-      innerHTML += '<hr class="my-3"></hr>';
+    const divs = createTabs({ tabNames: Object.keys(packets) });
+
+    for (const division of Object.keys(packets)) {
+      const packet = packets[division];
+      const div = divs[division];
+      let innerHTML = '';
+      for (const tossup of packet) {
+        innerHTML += `<div>${tossup.questionNumber}. ${tossup.question}</div>`;
+        innerHTML += `<div>ANSWER: ${tossup.answer}</div>`;
+        innerHTML += `<p>&lt;${tossup.category} / ${tossup.subcategory}&gt;</p>`;
+        innerHTML += '<hr class="my-3"></hr>';
+      }
+      div.innerHTML += innerHTML;
     }
-
-    document.getElementById('packet').innerHTML += innerHTML;
   });
