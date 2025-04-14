00ea81dbb44b739a4035cfa8c7c86c071c762a29
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-04-13 22:25:28 -0400

    nav tabs for protests

--- database/geoword/get-protests.js
+++ database/geoword/get-protests.js
@@ -1,8 +1,9 @@
 import { buzzes, tossups } from './collections.js';
 
 import getUsername from '../account-info/get-username.js';
+import getDivisions from './get-divisions.js';
 
-async function getProtests (packetName, division) {
+async function getDivisionProtests ({ division, packetName }) {
   const protests = await buzzes.find(
     { 'packet.name': packetName, division, pendingProtest: { $exists: true } },
     { sort: { questionNumber: 1 } }
@@ -19,6 +20,28 @@ async function getProtests (packetName, division) {
   }
 
   return { protests, packet };
+}
+
+/**
+ *
+ * @returns {Promise<{
+ *   [division: string]: {
+ *     protests: Array<buzzes>,
+ *     packet: Array<tossups>
+ * }>}
+ */
+async function getProtests ({ packetName }) {
+  const divisions = await getDivisions(packetName);
+  if (!divisions || divisions.length === 0) {
+    return {};
+  }
+
+  const queries = divisions.map(division => getDivisionProtests({ division, packetName }));
+  const results = await Promise.all(queries);
+
+  return Object.fromEntries(
+    divisions.map((division, index) => [division, results[index]])
+  );
 }
 
 export default getProtests;
