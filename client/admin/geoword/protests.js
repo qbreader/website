b469e99ef9bb5dc6b3d5f015f3e43d371ac892e4
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-04-13 22:25:28 -0400

    nav tabs for protests

--- client/admin/geoword/protests.js
+++ client/admin/geoword/protests.js
@@ -1,66 +1,74 @@
+import createTabs from '../../scripts/utilities/create-tabs.js';
 import { escapeHTML, titleCase } from '../../scripts/utilities/strings.js';
 
 const search = new URLSearchParams(window.location.search);
-const division = search.get('division');
 const packetName = search.get('packetName');
 const packetTitle = titleCase(packetName);
 
 document.getElementById('packet-name').textContent = packetTitle;
-document.getElementById('division').textContent = division;
 
-fetch('/api/admin/geoword/protests?' + new URLSearchParams({ packetName, division }))
+fetch('/api/admin/geoword/protests?' + new URLSearchParams({ packetName }))
   .then(response => response.json())
   .then(data => {
-    const { protests, packet } = data;
+    const { protests: allProtests } = data;
 
-    let innerHTML = '';
-    for (const tossup of packet) {
-      const { questionNumber } = tossup;
-      innerHTML += `<div>${tossup.questionNumber}. ${tossup.question}</div>`;
-      // innerHTML += '<hr class="my-3"></hr>';
-      innerHTML += `<div>ANSWER: ${tossup.answer}</div>`;
-      innerHTML += `<p>&lt;${tossup.category} / ${tossup.subcategory}&gt;</p>`;
+    const divs = createTabs({ tabNames: Object.keys(allProtests) });
 
-      if (protests.filter(protest => protest.questionNumber === questionNumber).length === 0) {
+    for (const division of Object.keys(allProtests)) {
+      const { packet, protests } = allProtests[division];
+
+      let innerHTML = '';
+      for (const tossup of packet) {
+        const { questionNumber } = tossup;
+        innerHTML += `<div>${tossup.questionNumber}. ${tossup.question}</div>`;
+        innerHTML += `<div>ANSWER: ${tossup.answer}</div>`;
+        innerHTML += `<p>&lt;${tossup.category} / ${tossup.subcategory}&gt;</p>`;
+
+        if (protests.filter(protest => protest.questionNumber === questionNumber).length === 0) {
+          innerHTML += '<hr>';
+          continue;
+        }
+
+        innerHTML += '<h5>Protests:</h5>';
+        innerHTML += '<ul class="list-group mb-3">';
+        for (const protest of protests.filter(protest => protest.questionNumber === questionNumber && protest.pendingProtest)) {
+          innerHTML += `
+            <li class="list-group-item d-flex justify-content-between">
+              <span>
+                <b>${escapeHTML(protest.username + ': ' || '')}</b>
+                <span id="given-answer-${protest._id}">${protest.givenAnswer}</span>
+                ${getPromptString(protest)}
+                (celerity: ${protest.celerity.toFixed(3)}) - pending review
+              </span>
+              <a id="${protest._id}" question="${questionNumber}" href="#" data-bs-toggle="modal" data-bs-target="#resolve-protest-modal">Resolve protest</a>
+            </li>
+          `;
+        }
+
+        for (const protest of protests.filter(protest => protest.questionNumber === questionNumber && !protest.pendingProtest)) {
+          const resolution = `resolution: ${protest.decision} ${getProtestReasonString(protest)}`;
+
+          innerHTML += `
+            <li class="list-group-item">
+              <span><b>${escapeHTML(protest.username + ': ' || '')}</b><span>${protest.givenAnswer} ${getPromptString(protest)}</span> - ${resolution}</span>
+            </li>
+          `;
+        }
+
+        innerHTML += '</ul>';
         innerHTML += '<hr>';
-        continue;
       }
 
-      innerHTML += '<h5>Protests:</h5>';
-      innerHTML += '<ul class="list-group mb-3">';
-      for (const protest of protests.filter(protest => protest.questionNumber === questionNumber && protest.pendingProtest)) {
-        innerHTML += `<li class="list-group-item d-flex justify-content-between">
-                    <span>
-                        <b>${escapeHTML(protest.username + ': ' || '')}</b>
-                        <span id="given-answer-${protest._id}">${protest.givenAnswer}</span>
-                        ${getPromptString(protest)}
-                        (celerity: ${protest.celerity.toFixed(3)}) - pending review
-                    </span>
-                    <a id="${protest._id}" question="${questionNumber}" href="#" data-bs-toggle="modal" data-bs-target="#resolve-protest-modal">Resolve protest</a>
-                </li>`;
-      }
+      divs[division].innerHTML += innerHTML;
 
-      for (const protest of protests.filter(protest => protest.questionNumber === questionNumber && !protest.pendingProtest)) {
-        const resolution = `resolution: ${protest.decision} ${getProtestReasonString(protest)}`;
-
-        innerHTML += `<li class="list-group-item">
-                    <span><b>${escapeHTML(protest.username + ': ' || '')}</b><span>${protest.givenAnswer} ${getPromptString(protest)}</span> - ${resolution}</span>
-                </li>`;
-      }
-
-      innerHTML += '</ul>';
-      innerHTML += '<hr>';
-    }
-
-    document.getElementById('protests').innerHTML += innerHTML;
-
-    document.querySelectorAll('a[data-bs-toggle="modal"]').forEach(a => {
-      a.addEventListener('click', () => {
-        document.getElementById('resolve-protest-id').value = a.id;
-        document.getElementById('resolve-protest-given-answer').value = document.getElementById(`given-answer-${a.id}`).textContent;
-        document.getElementById('resolve-protest-actual-answer').innerHTML = packet[parseInt(a.attributes.question.value) - 1].answer;
+      document.querySelectorAll('a[data-bs-toggle="modal"]').forEach(a => {
+        a.addEventListener('click', () => {
+          document.getElementById('resolve-protest-id').value = a.id;
+          document.getElementById('resolve-protest-given-answer').value = document.getElementById(`given-answer-${a.id}`).textContent;
+          document.getElementById('resolve-protest-actual-answer').innerHTML = packet[parseInt(a.attributes.question.value) - 1].answer;
+        });
       });
-    });
+    }
   });
 
 document.getElementById('resolve-protest-submit').addEventListener('click', () => {
