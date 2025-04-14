07d83e1d927ed1eb80ca52326ee9bf8b27f461af
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-04-13 23:17:54 -0400

    revert back to mongodb for geoword audio storage

--- database/databases.js
+++ database/databases.js
@@ -1,4 +1,3 @@
-import * as AWS from '@aws-sdk/client-s3';
 import { MongoClient } from 'mongodb';
 
 const uri = `mongodb+srv://${process.env.MONGODB_USERNAME || 'geoffreywu42'}:${process.env.MONGODB_PASSWORD || 'password'}@qbreader.0i7oej9.mongodb.net/?retryWrites=true&w=majority`;
@@ -10,12 +9,3 @@ console.log('connected to mongodb');
 export const qbreader = mongoClient.db('qbreader');
 export const accountInfo = mongoClient.db('account-info');
 export const geoword = mongoClient.db('geoword');
-
-export const bucketName = process.env.BUCKETEER_BUCKET_NAME;
-export const s3 = new AWS.S3({
-  region: 'us-east-1',
-  credentials: {
-    accessKeyId: process.env.BUCKETEER_AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.BUCKETEER_AWS_SECRET_ACCESS_KEY
-  }
-});
