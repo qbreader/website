68e0b74c33fe848148b75db7714e1a62a785870c
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-05-01 15:44:02 -0400

    add pop culture subcategories

--- quizbowl/categories.js
+++ quizbowl/categories.js
@@ -10,7 +10,7 @@ export const CATEGORY_TO_SUBCATEGORY = {
   'Current Events': ['Current Events'],
   Geography: ['Geography'],
   'Other Academic': ['Other Academic'],
-  'Pop Culture': ['Pop Culture']
+  'Pop Culture': ['Movies', 'Music', 'Sports', 'Television', 'Video Games', 'Other Pop Culture']
 };
 export const CATEGORIES = Object.keys(CATEGORY_TO_SUBCATEGORY);
 
@@ -55,7 +55,12 @@ export const SUBCATEGORY_TO_CATEGORY = {
   'Current Events': 'Current Events',
   Geography: 'Geography',
   'Other Academic': 'Other Academic',
-  'Pop Culture': 'Pop Culture'
+  Movies: 'Pop Culture',
+  Music: 'Pop Culture',
+  Sports: 'Pop Culture',
+  Television: 'Pop Culture',
+  'Video Games': 'Pop Culture',
+  'Other Pop Culture': 'Pop Culture'
 };
 export const SUBCATEGORIES = Object.keys(SUBCATEGORY_TO_CATEGORY);
 
