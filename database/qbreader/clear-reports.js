839f7d45ade359adcd9c000733c2473cb45ca47b
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-06-19 14:51:45 -0400

    /admin: add clear reports button

---
+++ database/qbreader/clear-reports.js
@@ -0,0 +1,22 @@
+import { bonuses, tossups } from './collections.js';
+
+/**
+ * Deletes the reports from a single tossup or bonus.
+ * @param {ObjectId} _id - The unique identifier of the document to update.
+ * @param {'tossup' | 'bonus'} type - The type of document to update ('tossup' or 'bonus').
+ * @returns The result of the update operation.
+ */
+export default async function clearReports (_id, type) {
+  switch (type) {
+    case 'tossup':
+      return await tossups.updateOne(
+        { _id },
+        { $unset: { reports: 1 } }
+      );
+    case 'bonus':
+      return await bonuses.updateOne(
+        { _id },
+        { $unset: { reports: 1 } }
+      );
+  }
+}
---
+++ routes/api/admin/clear-reports.js
@@ -0,0 +1,15 @@
+import clearReports from '../../../database/qbreader/clear-reports.js';
+
+import { Router } from 'express';
+import { ObjectId } from 'mongodb';
+
+const router = Router();
+
+router.put('/', async (req, res) => {
+  let { _id, type } = req.body;
+  try { _id = new ObjectId(_id); } catch (e) { return res.status(400).send('Invalid ID'); }
+  const result = await clearReports(_id, type);
+  res.sendStatus(result.modifiedCount > 0 ? 200 : 500);
+});
+
+export default router;
