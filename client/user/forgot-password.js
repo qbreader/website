b839b5b4d202c8edda5145e78e8439e46e2e3e03
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-06-20 11:49:14 -0400

    migrate email from sendgrid to mailersend

--- client/user/forgot-password.js
+++ client/user/forgot-password.js
@@ -3,7 +3,7 @@ import account from '../scripts/accounts.js';
 account.deleteUsername();
 
 const form = document.getElementById('forgot-password-form');
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', async (event) => {
   event.preventDefault();
   event.stopPropagation();
   form.classList.add('was-validated');
@@ -15,6 +15,6 @@ form.addEventListener('submit', (event) => {
   document.getElementById('submission').textContent = 'Submitting...';
 
   const username = document.getElementById('username').value;
-  fetch('/auth/send-password-reset-email?' + new URLSearchParams({ username }));
+  await fetch('/auth/send-password-reset-email?' + new URLSearchParams({ username }));
   window.location.href = '/';
 }, false);
