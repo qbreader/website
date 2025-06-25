6032c6e5c782da84954fed54ef9fdb12a2db767d
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-06-25 12:48:02 -0400

    /admin add ability to change set name

--- routes/api/admin/question-management/set.js
+++ routes/api/admin/question-management/set.js
@@ -1,3 +1,4 @@
+import renameSet from '../../../../database/qbreader/admin/rename-set.js';
 import updateSetDifficulty from '../../../../database/qbreader/admin/update-set-difficulty.js';
 import updateSetStandardness from '../../../../database/qbreader/admin/update-set-standardness.js';
 import { DIFFICULTIES } from '../../../../quizbowl/constants.js';
@@ -5,6 +6,22 @@ import { DIFFICULTIES } from '../../../../quizbowl/constants.js';
 import { Router } from 'express';
 
 const router = Router();
+
+router.put('/rename-set', async (req, res) => {
+  const { oldName, newName } = req.body;
+
+  if (typeof oldName !== 'string' || typeof newName !== 'string') {
+    return res.sendStatus(400);
+  }
+
+  const result = await renameSet(oldName, newName);
+
+  if (result) {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(400);
+  }
+});
 
 router.put('/update-difficulty', async (req, res) => {
   const { setName, difficulty } = req.body;
