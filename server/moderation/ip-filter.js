07f939213e0e2b3365850d0438e17c29eb47bcd3
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-08-07 09:03:02 -0500

    remove dependency on ip package

--- server/moderation/ip-filter.js
+++ server/moderation/ip-filter.js
@@ -1,6 +1,3 @@
-import expressIpfilter from 'express-ipfilter';
-const { IpFilter, IpDeniedError } = expressIpfilter;
-
 const ips = [
   '3.236.192.58',
   '18.206.238.89',
@@ -12,14 +9,11 @@ export const clientIp = (req, _res) => {
   return req.headers['x-forwarded-for'] ? (req.headers['x-forwarded-for']).split(',')[0] : req.ip;
 };
 
-export const ipFilterMiddleware = IpFilter(ips, { mode: 'deny', log: false, detectIp: clientIp });
-
-export const ipFilterError = (err, req, res, _next) => {
-  if (err instanceof IpDeniedError) {
+export const ipFilterMiddleware = (req, res, next) => {
+  const ip = clientIp(req);
+  if (ips.includes(ip)) {
     console.log(`Blocked IP: ${req.ip}`);
-    res.status(403);
-    res.end();
-  } else {
-    res.status(err.status || 500);
+    return res.status(403).end();
   }
+  next();
 };
