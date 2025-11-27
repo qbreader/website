61965ae6498e2997db88f86eec6a72550d6d79d9
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-11-27 12:50:38 -0600

    let's try that again

--- server/multiplayer/handle-wss-connection.js
+++ server/multiplayer/handle-wss-connection.js
@@ -90,10 +90,10 @@ export default function handleWssConnection (ws, req) {
   }
 
   if (room.settings.loginRequired === true) {
-    const cookieString = (req?.headers?.cookie ?? 'session=;').split(';').find(token => token.trim().startsWith('session=')) ?? '';
-    const cookieBuffer = Buffer.from(cookieString.split('=')[1], 'base64');
     let valid = true;
     try {
+      const cookieString = (req?.headers?.cookie ?? 'session=;').split(';').find(token => token.trim().startsWith('session='));
+      const cookieBuffer = Buffer.from(cookieString.split('=')[1], 'base64');
       const cookies = JSON.parse(cookieBuffer.toString('utf-8'));
       valid = checkToken(cookies.username, cookies.token, true);
     } catch (e) { valid = false; }
