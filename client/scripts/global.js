7b328d5659a3306a93271749d72ffa2f9dac3f4a
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-12-29 22:13:00 -0500

    combine `/scripts/apply-theme.js` and `/script.js` into one file

--- client/scripts/global.js
+++ client/scripts/global.js
@@ -1,3 +1,31 @@
+/**
+ * This file is run at the start of every page
+ */
+
+import account from './accounts.js';
+
+function isTouchDevice () {
+  if ('ontouchstart' in window) return true;
+
+  // eslint-disable-next-line no-undef
+  return window.DocumentTouch && document instanceof DocumentTouch;
+}
+
+const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+for (const tooltipTriggerEl of tooltipTriggerList) {
+  if (isTouchDevice()) continue;
+
+  // eslint-disable-next-line no-new
+  new bootstrap.Tooltip(tooltipTriggerEl);
+}
+
+account.getUsername().then(username => {
+  if (username) {
+    document.getElementById('login-link').textContent = username;
+    document.getElementById('login-link').href = '/user/my-profile';
+  }
+});
+
 // mostly copied from https://getbootstrap.com/docs/5.3/customize/color-modes/#javascript
 
 const getStoredTheme = () => window.localStorage.getItem('color-theme');
