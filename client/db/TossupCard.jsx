0ae8ae4e21dab5fc28a5ba1240ac4aab0582be87
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-12-31 20:17:11 -0500

    fix individual question link in db search results

--- client/db/TossupCard.jsx
+++ client/db/TossupCard.jsx
@@ -75,7 +75,7 @@ export default function TossupCard ({ tossup, highlightedTossup, hideAnswerline,
         </div>
         <div>
           <small className='text-muted'>
-            <a href={`/db/explorer/tossup?_id=${_id}`} onClick={e => e.stopPropagation()}>
+            <a href={`/db/tossup/?_id=${_id}`} onClick={e => e.stopPropagation()}>
               Link to tossup
             </a>
             <span> | </span>
