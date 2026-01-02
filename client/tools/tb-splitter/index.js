1a520b0d53a091e19968705896edb6ef56b70466
Geoffrey Wu <geoffreywu1000@gmail.com>
2026-01-02 11:52:01 -0600

    delete unnecessary comments

--- client/tools/tb-splitter/index.js
+++ client/tools/tb-splitter/index.js
@@ -2,7 +2,6 @@
 
 function parseDoNotReadToDirective (text) {
   const regex = /.*Do not read to ([^.]*).*/;
-  // return the capture group if it exists and the original text
   const match = text.match(regex);
   return match?.at(1)?.replace(/ /, '-');
 }
@@ -39,46 +38,3 @@ document.getElementById('submit').addEventListener('click', function (e) {
   };
   reader.readAsText(file);
 });
-
-// document.getElementById('form').addEventListener('submit', async function (e) {
-//   e.preventDefault();
-//   e.stopPropagation();
-
-//   const files = document.getElementById('file-input').files;
-//   if (files.length === 0) { return; }
-
-//   const parser = new Parser({
-//     hasCategoryTags: document.getElementById('has-category-tags').checked,
-//     hasQuestionNumbers: document.getElementById('has-question-numbers').checked,
-//     buzzpoints: document.getElementById('buzzpoint-format').checked,
-//     modaq: document.getElementById('modaq-format').checked
-//   });
-//   zip = new JSZip();
-
-//   const fileOutput = document.getElementById('file-output');
-//   fileOutput.innerHTML = '';
-//   document.getElementById('warning-list').innerHTML = '';
-
-//   for (const file of files) {
-//     const arrayBuffer = await file.arrayBuffer();
-//     try {
-//       const { data, warnings } = await parser.parseDocxPacket(arrayBuffer, file.name);
-//       const newFileName = file.name.replace(/\.docx$/, '.json');
-//       fileOutput.appendChild(createDownloadGroupItem(data, newFileName));
-//       zip.file(newFileName, JSON.stringify(data, null, 4));
-//       if (warnings.length > 0) {
-//         fillWarnings(warnings, newFileName);
-//       }
-//     } catch (e) {
-//       window.alert(`Error parsing ${file.name}: ${e.message}`);
-//     }
-//   }
-
-//   document.getElementById('download-all').classList.remove('d-none');
-// });
-
-// document.getElementById('download-all').addEventListener('click', function () {
-//   zip.generateAsync({ type: 'blob' }).then(function (content) {
-//     saveAs(content, 'output.zip');
-//   });
-// });
