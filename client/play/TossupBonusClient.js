0bbbb0ca6c407fa451948ec89120660a382d1ee7
Theodore Chen <littletheo@Theodores-MacBook-Air.local>
2026-02-16 17:01:59 -0500

    chore: cleanup unnecessary comments

--- client/play/TossupBonusClient.js
+++ client/play/TossupBonusClient.js
@@ -2,7 +2,9 @@ import { BonusClientMixin } from './BonusClient.js';
 import { TossupClientMixin } from './TossupClient.js';
 import QuestionClient from './QuestionClient.js';
 
-export default class TossupBonusClient extends BonusClientMixin(TossupClientMixin(QuestionClient)) {
+export default class TossupBonusClient extends BonusClientMixin(
+  TossupClientMixin(QuestionClient)
+) {
   constructor (room, userId, socket) {
     super(room, userId, socket);
     attachEventListeners(room, socket);
@@ -11,9 +13,10 @@ export default class TossupBonusClient extends BonusClientMixin(TossupClientMixi
   onmessage (message) {
     const data = JSON.parse(message);
     switch (data.type) {
-      case 'toggle-enable-bonuses': return this.toggleEnableBonuses(data);
-        // case 'toggle-stop-on-power': return this.toggleStopOnPower(data);
-      default: return super.onmessage(message);
+      case 'toggle-enable-bonuses':
+        return this.toggleEnableBonuses(data);
+      default:
+        return super.onmessage(message);
     }
   }
 
@@ -25,20 +28,16 @@ export default class TossupBonusClient extends BonusClientMixin(TossupClientMixi
   toggleEnableBonuses ({ enableBonuses }) {
     document.getElementById('toggle-enable-bonuses').checked = enableBonuses;
   }
-
-  // toggleStopOnPower({ stopOnPower }) {
-  // document.getElementById('toggle-stop-on-power').checked = stopOnPower;
-  // }
 }
 
 function attachEventListeners (room, socket) {
-  document.getElementById('toggle-enable-bonuses').addEventListener('click', function () {
-    this.blur();
-    socket.sendToServer({ type: 'toggle-enable-bonuses', enableBonuses: this.checked });
-  });
-
-  // document.getElementById('toggle-stop-on-power').addEventListener('click', function() {
-  // this.blur();
-  // socket.sendToServer({ type: 'toggle-stop-on-power', stopOnPower: this.checked });
-  // });
+  document
+    .getElementById('toggle-enable-bonuses')
+    .addEventListener('click', function () {
+      this.blur();
+      socket.sendToServer({
+        type: 'toggle-enable-bonuses',
+        enableBonuses: this.checked
+      });
+    });
 }
