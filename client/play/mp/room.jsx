79b63c395671ff7ba79c993994cf18e42eac9016
Theodore Chen <littletheo@Theodores-MacBook-Air.local>
2026-02-16 10:33:17 -0500

    added stop on power to mp

--- client/play/mp/room.jsx
+++ client/play/mp/room.jsx
@@ -1,11 +1,11 @@
-import MultiplayerTossupBonusClient from './MultiplayerTossupBonusClient.js';
+import MultiplayerTossupBonusClient from "./MultiplayerTossupBonusClient.js";
 
-import CategoryManager from '../../../quizbowl/category-manager.js';
-import { getDropdownValues } from '../../scripts/utilities/dropdown-checklist.js';
-import CategoryModal from '../../scripts/components/CategoryModal.jsx';
-import DifficultyDropdown from '../../scripts/components/DifficultyDropdown.jsx';
-import { MODE_ENUM } from '../../../quizbowl/constants.js';
-import getRandomName from '../../../quizbowl/get-random-name.js';
+import CategoryManager from "../../../quizbowl/category-manager.js";
+import { getDropdownValues } from "../../scripts/utilities/dropdown-checklist.js";
+import CategoryModal from "../../scripts/components/CategoryModal.jsx";
+import DifficultyDropdown from "../../scripts/components/DifficultyDropdown.jsx";
+import { MODE_ENUM } from "../../../quizbowl/constants.js";
+import getRandomName from "../../../quizbowl/get-random-name.js";
 
 const room = {
   bonus: {},
@@ -13,7 +13,7 @@ const room = {
   difficulties: [],
   mode: MODE_ENUM.RANDOM,
   muteList: [],
-  ownerId: '',
+  ownerId: "",
   /**
    * userId to player object
    */
@@ -23,177 +23,248 @@ const room = {
   showingOffline: false,
   teams: {},
   tossup: {},
-  username: window.localStorage.getItem('multiplayer-username') || getRandomName()
+  username:
+    window.localStorage.getItem("multiplayer-username") || getRandomName(),
 };
 
 let oldCategories = JSON.stringify(room.categoryManager.export());
 
-const ROOM_NAME = decodeURIComponent(window.location.pathname.split('/').at(-1));
-const USER_ID = window.localStorage.getItem('USER_ID') || 'unknown';
+const ROOM_NAME = decodeURIComponent(
+  window.location.pathname.split("/").at(-1),
+);
+const USER_ID = window.localStorage.getItem("USER_ID") || "unknown";
 
 const socket = new window.WebSocket(
-  window.location.href.replace('http', 'ws').split('?')[0] + '?' +
+  window.location.href.replace("http", "ws").split("?")[0] +
+    "?" +
     new URLSearchParams({
       ...Object.fromEntries(new URLSearchParams(window.location.search)),
       roomName: ROOM_NAME,
       userId: USER_ID,
-      username: room.username
-    }).toString()
+      username: room.username,
+    }).toString(),
 );
-window.history.pushState({}, '', './' + encodeURIComponent(ROOM_NAME));
+window.history.pushState({}, "", "./" + encodeURIComponent(ROOM_NAME));
 
 // Ping server every 30 seconds to prevent socket disconnection
 const PING_INTERVAL_ID = setInterval(
-  () => socket.send(JSON.stringify({ type: 'ping' })),
-  30000
+  () => socket.send(JSON.stringify({ type: "ping" })),
+  30000,
 );
 
 socket.sendToServer = (data) => socket.send(JSON.stringify(data));
 
 socket.onclose = function (event) {
   const { code } = event;
-  if (code !== 3000) { window.alert('Disconnected from server'); }
+  if (code !== 3000) {
+    window.alert("Disconnected from server");
+  }
   clearInterval(PING_INTERVAL_ID);
 };
 
 const client = new MultiplayerTossupBonusClient(room, USER_ID, socket);
 socket.onmessage = (message) => client.onmessage(message);
 
-document.getElementById('answer-input').addEventListener('input', function () {
-  socket.send(JSON.stringify({ type: 'give-answer-live-update', givenAnswer: this.value }));
+document.getElementById("answer-input").addEventListener("input", function () {
+  socket.send(
+    JSON.stringify({
+      type: "give-answer-live-update",
+      givenAnswer: this.value,
+    }),
+  );
 });
 
-document.getElementById('chat').addEventListener('click', function () {
+document.getElementById("chat").addEventListener("click", function () {
   this.blur();
-  document.getElementById('chat-input-group').classList.remove('d-none');
-  document.getElementById('chat-input').focus();
-  socket.send(JSON.stringify({ type: 'chat-live-update', message: '' }));
+  document.getElementById("chat-input-group").classList.remove("d-none");
+  document.getElementById("chat-input").focus();
+  socket.send(JSON.stringify({ type: "chat-live-update", message: "" }));
 });
 
-document.getElementById('chat-form').addEventListener('submit', function (event) {
-  event.preventDefault();
-  event.stopPropagation();
+document
+  .getElementById("chat-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    event.stopPropagation();
 
-  const message = document.getElementById('chat-input').value;
-  document.getElementById('chat-input').value = '';
-  document.getElementById('chat-input-group').classList.add('d-none');
-  document.getElementById('chat-input').blur();
+    const message = document.getElementById("chat-input").value;
+    document.getElementById("chat-input").value = "";
+    document.getElementById("chat-input-group").classList.add("d-none");
+    document.getElementById("chat-input").blur();
 
-  socket.send(JSON.stringify({ type: 'chat', message }));
+    socket.send(JSON.stringify({ type: "chat", message }));
+  });
+
+document.getElementById("chat-input").addEventListener("input", function () {
+  socket.send(
+    JSON.stringify({ type: "chat-live-update", message: this.value }),
+  );
 });
 
-document.getElementById('chat-input').addEventListener('input', function () {
-  socket.send(JSON.stringify({ type: 'chat-live-update', message: this.value }));
-});
-
-const styleSheet = document.createElement('style');
-styleSheet.textContent = room.showingOffline ? '' : '.offline { display: none; }';
+const styleSheet = document.createElement("style");
+styleSheet.textContent = room.showingOffline
+  ? ""
+  : ".offline { display: none; }";
 document.head.appendChild(styleSheet);
-document.getElementById('toggle-offline-players').addEventListener('click', function () {
-  room.showingOffline = this.checked;
+document
+  .getElementById("toggle-offline-players")
+  .addEventListener("click", function () {
+    room.showingOffline = this.checked;
+    this.blur();
+    if (room.showingOffline) {
+      styleSheet.textContent = "";
+    } else {
+      styleSheet.textContent = ".offline { display: none; }";
+    }
+  });
+
+document
+  .getElementById("toggle-controlled")
+  .addEventListener("click", function () {
+    this.blur();
+    socket.send(
+      JSON.stringify({ type: "toggle-controlled", controlled: this.checked }),
+    );
+  });
+
+document.getElementById("toggle-lock").addEventListener("click", function () {
   this.blur();
-  if (room.showingOffline) {
-    styleSheet.textContent = '';
-  } else {
-    styleSheet.textContent = '.offline { display: none; }';
-  }
+  socket.send(JSON.stringify({ type: "toggle-lock", lock: this.checked }));
 });
 
-document.getElementById('toggle-controlled').addEventListener('click', function () {
+document
+  .getElementById("toggle-login-required")
+  .addEventListener("click", function () {
+    this.blur();
+    socket.send(
+      JSON.stringify({
+        type: "toggle-login-required",
+        loginRequired: this.checked,
+      }),
+    );
+  });
+
+document.getElementById("toggle-skip").addEventListener("click", function () {
   this.blur();
-  socket.send(JSON.stringify({ type: 'toggle-controlled', controlled: this.checked }));
+  socket.send(JSON.stringify({ type: "toggle-skip", skip: this.checked }));
 });
 
-document.getElementById('toggle-lock').addEventListener('click', function () {
+document
+  .getElementById("toggle-stop-on-power")
+  .addEventListener("click", function () {
+    this.blur();
+    socket.send(
+      JSON.stringify({
+        type: "toggle-stop-on-power",
+        stopOnPower: this.checked,
+      }),
+    );
+  });
+
+document.getElementById("toggle-public").addEventListener("click", function () {
   this.blur();
-  socket.send(JSON.stringify({ type: 'toggle-lock', lock: this.checked }));
+  socket.send(JSON.stringify({ type: "toggle-public", public: this.checked }));
 });
 
-document.getElementById('toggle-login-required').addEventListener('click', function () {
+document.getElementById("reveal").addEventListener("click", function () {
   this.blur();
-  socket.send(JSON.stringify({ type: 'toggle-login-required', loginRequired: this.checked }));
+  socket.send(JSON.stringify({ type: "start-bonus-answer" }));
 });
 
-document.getElementById('toggle-skip').addEventListener('click', function () {
-  this.blur();
-  socket.send(JSON.stringify({ type: 'toggle-skip', skip: this.checked }));
-});
-
-document.getElementById('toggle-public').addEventListener('click', function () {
-  this.blur();
-  socket.send(JSON.stringify({ type: 'toggle-public', public: this.checked }));
-});
-
-document.getElementById('reveal').addEventListener('click', function () {
-  this.blur();
-  socket.send(JSON.stringify({ type: 'start-bonus-answer' }));
-});
-
-document.getElementById('username').addEventListener('change', function () {
-  socket.send(JSON.stringify({ type: 'set-username', userId: USER_ID, username: this.value }));
+document.getElementById("username").addEventListener("change", function () {
+  socket.send(
+    JSON.stringify({
+      type: "set-username",
+      userId: USER_ID,
+      username: this.value,
+    }),
+  );
   room.username = this.value;
-  window.localStorage.setItem('multiplayer-username', room.username);
+  window.localStorage.setItem("multiplayer-username", room.username);
 });
 
-document.addEventListener('keydown', (event) => {
+document.addEventListener("keydown", (event) => {
   // press escape to close chat
-  if (event.key === 'Escape' && document.activeElement.id === 'chat-input') {
-    document.getElementById('chat-input').value = '';
-    document.getElementById('chat-input-group').classList.add('d-none');
-    document.getElementById('chat-input').blur();
-    socket.send(JSON.stringify({ type: 'chat', message: '' }));
+  if (event.key === "Escape" && document.activeElement.id === "chat-input") {
+    document.getElementById("chat-input").value = "";
+    document.getElementById("chat-input-group").classList.add("d-none");
+    document.getElementById("chat-input").blur();
+    socket.send(JSON.stringify({ type: "chat", message: "" }));
   }
 
-  if (['INPUT', 'TEXTAREA', 'SELECT'].includes(document.activeElement.tagName)) return;
+  if (["INPUT", "TEXTAREA", "SELECT"].includes(document.activeElement.tagName))
+    return;
 
   switch (event.key?.toLowerCase()) {
-    case ' ':
-      if (!document.getElementById('reveal').disabled) {
-        document.getElementById('reveal').click();
+    case " ":
+      if (!document.getElementById("reveal").disabled) {
+        document.getElementById("reveal").click();
       } else {
-        document.getElementById('buzz').click();
+        document.getElementById("buzz").click();
       }
       // Prevent spacebar from scrolling the page
-      if (event.target === document.body) { event.preventDefault(); }
+      if (event.target === document.body) {
+        event.preventDefault();
+      }
       break;
 
-    case 'e': return document.getElementById('toggle-settings').click();
-    case 'k': return document.getElementsByClassName('card-header-clickable')[0].click();
-    case 'p': return document.getElementById('pause').click();
-    case 't': return document.getElementsByClassName('star-tossup')[0].click();
-    case 'y': return navigator.clipboard.writeText(room.tossup._id ?? '');
+    case "e":
+      return document.getElementById("toggle-settings").click();
+    case "k":
+      return document
+        .getElementsByClassName("card-header-clickable")[0]
+        .click();
+    case "p":
+      return document.getElementById("pause").click();
+    case "t":
+      return document.getElementsByClassName("star-tossup")[0].click();
+    case "y":
+      return navigator.clipboard.writeText(room.tossup._id ?? "");
 
-    case 'n':
-    case 's': return document.getElementById('next').click();
+    case "n":
+    case "s":
+      return document.getElementById("next").click();
   }
 });
 
-document.addEventListener('keypress', function (event) {
+document.addEventListener("keypress", function (event) {
   // needs to be keypress
   // keydown immediately hides the input group
   // keyup shows the input group again after submission
-  if (event.key === 'Enter' && event.target === document.body) {
-    document.getElementById('chat').click();
+  if (event.key === "Enter" && event.target === document.body) {
+    document.getElementById("chat").click();
   }
 });
 
-document.getElementById('username').value = room.username;
+document.getElementById("username").value = room.username;
 
-ReactDOM.createRoot(document.getElementById('category-modal-root')).render(
+ReactDOM.createRoot(document.getElementById("category-modal-root")).render(
   <CategoryModal
     categoryManager={room.categoryManager}
     onClose={() => {
       if (oldCategories !== JSON.stringify(room.categoryManager.export())) {
-        socket.send(JSON.stringify({ type: 'set-categories', ...room.categoryManager.export() }));
+        socket.send(
+          JSON.stringify({
+            type: "set-categories",
+            ...room.categoryManager.export(),
+          }),
+        );
       }
       oldCategories = JSON.stringify(room.categoryManager.export());
     }}
-  />
+  />,
 );
 
-ReactDOM.createRoot(document.getElementById('difficulty-dropdown-root')).render(
+ReactDOM.createRoot(document.getElementById("difficulty-dropdown-root")).render(
   <DifficultyDropdown
     startingDifficulties={room.difficulties}
-    onChange={() => socket.send(JSON.stringify({ type: 'set-difficulties', difficulties: getDropdownValues('difficulties') }))}
-  />
+    onChange={() =>
+      socket.send(
+        JSON.stringify({
+          type: "set-difficulties",
+          difficulties: getDropdownValues("difficulties"),
+        }),
+      )
+    }
+  />,
 );
