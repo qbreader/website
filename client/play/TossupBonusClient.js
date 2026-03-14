62029c37af2a08b44df257feb3069e24e1659312
ANewProfile <theochen16@gmail.com>
2026-03-14 08:00:28 -0400

    bulk of TossupBonusClient.js

--- client/play/TossupBonusClient.js
+++ client/play/TossupBonusClient.js
@@ -2,10 +2,8 @@ import { BonusClientMixin } from './BonusClient.js';
 import { TossupClientMixin } from './TossupClient.js';
 import QuestionClient from './QuestionClient.js';
 
-export default class TossupBonusClient extends BonusClientMixin(
-  TossupClientMixin(QuestionClient)
-) {
-  constructor (room, userId, socket) {
+export default class TossupBonusClient extends BonusClientMixin(TossupClientMixin(QuestionClient)) {
+  constructor(room, userId, socket) {
     super(room, userId, socket);
     attachEventListeners(room, socket);
   }
@@ -13,10 +11,8 @@ export default class TossupBonusClient extends BonusClientMixin(
   onmessage (message) {
     const data = JSON.parse(message);
     switch (data.type) {
-      case 'toggle-enable-bonuses':
-        return this.toggleEnableBonuses(data);
-      default:
-        return super.onmessage(message);
+      case 'toggle-enable-bonuses': return this.toggleEnableBonuses(data);
+      default: return super.onmessage(message);
     }
   }
 
@@ -31,13 +27,8 @@ export default class TossupBonusClient extends BonusClientMixin(
 }
 
 function attachEventListeners (room, socket) {
-  document
-    .getElementById('toggle-enable-bonuses')
-    .addEventListener('click', function () {
+  document.getElementById('toggle-enable-bonuses').addEventListener('click', function () {
       this.blur();
-      socket.sendToServer({
-        type: 'toggle-enable-bonuses',
-        enableBonuses: this.checked
-      });
-    });
+      socket.sendToServer({ type: 'toggle-enable-bonuses', enableBonuses: this.checked });
+  });
 }
