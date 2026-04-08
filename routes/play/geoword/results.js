540b8b41a6cd8d86ec179ac6b075b9337ea8fefb
Geoffrey Wu <geoffreywu1000@gmail.com>
2026-04-07 23:18:47 -0400

    correctly check user status for geoword results

--- routes/play/geoword/results.js
+++ routes/play/geoword/results.js
@@ -1,9 +1,12 @@
+import paidMiddleware from './paid-middleware.js';
 import getUser from '../../../database/account-info/get-user.js';
 import getBuzzCount from '../../../database/geoword/get-buzz-count.js';
 import getQuestionCount from '../../../database/geoword/get-question-count.js';
 
 import { Router } from 'express';
 const router = Router();
+
+router.use(/^\/[^.]*$/, paidMiddleware);
 
 // only match routes that do not have a '.' in the URL
 router.use(/^\/[^.]*$/, async (req, res, next) => {
@@ -12,13 +15,12 @@ router.use(/^\/[^.]*$/, async (req, res, next) => {
   const user = await getUser(username);
 
   const [buzzCount, questionCount] = await Promise.all([
-    getBuzzCount(packetName, user._id),
+    getBuzzCount(packetName, user?._id),
     getQuestionCount(packetName)
   ]);
 
   if (!user.admin && buzzCount < questionCount) {
-    res.status(403).send('You must finish the game before viewing results.');
-    return;
+    return res.status(403).send('You must finish the game before viewing results.');
   }
 
   next();
