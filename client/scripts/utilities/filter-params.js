5abd395ad3f4b364c9d48c30562b21c80577b910
Geoffrey Wu <geoffreywu1000@gmail.com>
2026-04-24 19:02:24 -0500

    remove redundant categories in api requests

--- client/scripts/utilities/filter-params.js
+++ client/scripts/utilities/filter-params.js
@@ -1,4 +1,36 @@
 import { DEFAULT_MAX_YEAR, DEFAULT_MIN_YEAR, DIFFICULTIES } from '../../../quizbowl/constants.js';
+import { CATEGORY_TO_SUBCATEGORY, CATEGORY_TO_ALTERNATE_SUBCATEGORIES } from '../../../quizbowl/categories.js';
+
+function removeRedundantCategories (params) {
+  let { categories, subcategories, alternateSubcategories } = params;
+  if (!categories || !subcategories || !alternateSubcategories) { return params; }
+
+  for (const category of categories) {
+    let containsEverySubcategory = true;
+    for (const subcategory of CATEGORY_TO_SUBCATEGORY[category]) {
+      if (!subcategories || !subcategories.includes(subcategory)) {
+        containsEverySubcategory = false;
+        break;
+      }
+    }
+    if (containsEverySubcategory) {
+      subcategories = subcategories.filter(subcategory => !CATEGORY_TO_SUBCATEGORY[category].includes(subcategory));
+    }
+
+    let containsEveryAlternateSubcategory = true;
+    for (const alternateSubcategory of CATEGORY_TO_ALTERNATE_SUBCATEGORIES[category]) {
+      if (!alternateSubcategories || !alternateSubcategories.includes(alternateSubcategory)) {
+        containsEveryAlternateSubcategory = false;
+        break;
+      }
+    }
+    if (containsEveryAlternateSubcategory) {
+      alternateSubcategories = alternateSubcategories.filter(alternateSubcategory => !CATEGORY_TO_ALTERNATE_SUBCATEGORIES[category].includes(alternateSubcategory));
+    }
+  }
+
+  return { ...params, subcategories, alternateSubcategories };
+}
 
 /**
  * Filters out unwanted key-value pairs from the given parameters object.
@@ -13,6 +45,7 @@ import { DEFAULT_MAX_YEAR, DEFAULT_MIN_YEAR, DIFFICULTIES } from '../../../quizb
  * @returns {Object} A new object containing only the key-value pairs that pass the filtering criteria.
  */
 export default function filterParams (params) {
+  params = removeRedundantCategories(params);
   return Object.fromEntries(
     Object.entries(params).filter(([key, value]) => {
       if (value === '' || value === null || value === undefined) { return false; }
