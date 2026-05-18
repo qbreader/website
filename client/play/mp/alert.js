87be5d33f6f97e356f3957a225bc6561f56a4cfb
Lawrence Tong <lawrencetong1030@gmail.com>
2026-05-17 20:38:34 -0500

    feat: bootstrap architecture

--- client/play/mp/alert.js
+++ client/play/mp/alert.js
@@ -1,15 +1,14 @@
 export function showAlert (text, onClose) {
-  const alertBox = document.getElementById('alert-box');
-  const closeButton = alertBox.querySelector('.close-button');
-  const alertBoxText = alertBox.querySelector('.alert-text');
-  alertBoxText.textContent = text;
-  alertBox.style.display = 'flex';
-  alertBox.style.opacity = '1';
-  closeButton.onclick = function () {
-    alertBox.style.opacity = '0';
-    alertBox.addEventListener('transitionend', () => {
-      alertBox.style.display = 'none';
-      onClose?.();
-    }, { once: true });
+  const alertModalElement = document.getElementById('alert-modal');
+  const alertModalText = document.getElementById('alert-modal-text');
+  const alertModal = bootstrap.Modal.getOrCreateInstance(alertModalElement);
+
+  alertModalText.textContent = text;
+
+  const hiddenHandler = () => {
+    onClose?.();
   };
+
+  alertModalElement.addEventListener('hidden.bs.modal', hiddenHandler, { once: true });
+  alertModal.show();
 }
