61081f0ef9a55f824b03914fd9bc62e93a5a8e2e
xpoes123 <logiccubing@gmail.com>
2026-06-29 23:32:36 -0400

    perf: cache rendered SSI HTML instead of re-reading and re-substituting per request

--- routes/ssi-middleware.js
+++ routes/ssi-middleware.js
@@ -4,16 +4,23 @@ import path from 'path';
 const head = fs.readFileSync('./client/ssi/head.html', 'utf8');
 const nav = fs.readFileSync('./client/ssi/nav.html', 'utf8');
 
+// cache rendered HTML per file; files are static between deploys
+const cache = new Map();
+
 export default function (req, res, next) {
   if (path.extname(req.path) !== '') { return next(); }
 
   const filePath = path.resolve('./client' + req.path + (req.path.slice(-1) === '/' ? 'index.html' : '.html'));
   // prevent directory traversal attack
   if (!filePath.startsWith(path.resolve('./client'))) { return next(); }
+
+  if (cache.has(filePath)) { return res.send(cache.get(filePath)); }
+
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) return next();
     data = data.replace('<!--#include virtual="/ssi/head.html" -->', head);
     data = data.replace('<!--#include virtual="/ssi/nav.html" -->', nav);
+    cache.set(filePath, data);
     res.send(data);
   });
 }
