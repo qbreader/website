ed7379a251a968c229755243fc854a8270048058
xpoes123 <logiccubing@gmail.com>
2026-06-29 23:41:14 -0400

    security: rate-limit the report-question endpoint

--- routes/api/report-question.js
+++ routes/api/report-question.js
@@ -2,8 +2,15 @@ import { ObjectId } from 'mongodb';
 import reportQuestion from '../../database/qbreader/report-question.js';
 
 import { Router } from 'express';
+import rateLimit from 'express-rate-limit';
 
 const router = Router();
+router.use(rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour
+  max: 30, // Limit each IP to 30 requests per window
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false // Disable the `X-RateLimit-*` headers
+}));
 
 router.post('/', async (req, res) => {
   let _id;
