d8977ec63b0599e31313d2551576d87e5ff8017e
David Jiang <logiccubing@gmail.com>
2026-07-03 19:27:43 -0500

    feat: add lightweight /health endpoint (#542)

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,8 @@ router.get('/*.scss', (req, res) => res.sendFile(req.url, { root: './scss' }));
 
 router.use(redirectsRouter);
 
+router.get('/health', (req, res) => res.sendStatus(200));
+
 /**
  * Routes:
  */
