ea5ee8db1861a2a46dbeb1dd0e1e9f64712f7bac
dn285 <dn285@cornell.edu>
2026-08-28 00:37:04 -0500

    Add team leaderboard

--- client/play/geoword/results/index.js
+++ client/play/geoword/results/index.js
@@ -57,6 +57,7 @@ fetch('/api/geoword/paid/results/stats?' + new URLSearchParams({ packetName }))
   });
 
 document.getElementById('leaderboard-link').href = `./leaderboard?packetName=${packetName}`;
+document.getElementById('team-leaderboard-link').href = `./team-leaderboard?packetName=${packetName}`;
 document.getElementById('packet-link').href = `./packet?packetName=${packetName}`;
 
 function getPromptString (buzz) {
